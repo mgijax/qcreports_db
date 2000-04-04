d8f2cdeca0db92576c80efd332279d3bba6a0da9
Lori Corbani <Lori.Corbani@jax.org>
2000-04-04 18:19:45 +0000

    improve efficiency

--- nomen/NOMEN_Broadcast.sql
+++ nomen/NOMEN_Broadcast.sql
@@ -1,11 +1,23 @@
+set nocount on
+go
+
+select _Nomen_key
+into #nomen
+from MRK_Nomen
+where broadcast_date between dateadd(day, -3, getdate()) and dateadd(day, -0, getdate())
+go
+
+set nocount off
+go
+
 print ""
 print "Nomenclature Symbols Broadcast Within Last 3 Days"
 print ""
 
-select n.symbol, n.chromosome, n.submittedBy, n.broadcastBy, r.jnumID
-from MRK_Nomen_View n, MRK_Nomen_Reference_View r
-where n.broadcast_date between dateadd(day, -3, getdate()) and dateadd(day, -0, getdate())
-and n._Nomen_key = r._Nomen_key
+select v.symbol, v.chromosome, v.submittedBy, v.broadcastBy, r.jnumID
+from #nomen n, MRK_Nomen_View v, MRK_Nomen_Reference_View r
+where n._Nomen_key = v._Nomen_key
+and v._Nomen_key = r._Nomen_key
 and r.isPrimary = 1
-order by n.broadcast_date, n.broadcastBy, n.submittedBy
+order by v.broadcast_date, v.broadcastBy, v.submittedBy
 go
