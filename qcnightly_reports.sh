043d65b0de73c5fea64c7fd1556052099c38222d
Lori Corbani <Lori.Corbani@jax.org>
2000-04-04 14:49:21 +0000

    QCMGD and QCNOMEN

--- qcnightly_reports.sh
+++ qcnightly_reports.sh
@@ -12,10 +12,16 @@ cd `dirname $0` && source Configuration
 
 umask 002
 
+cd $QCMGD
 foreach i (`ls *.sql`)
 sql.sh $MGD $i
 end
-
 foreach i (`ls *.py`)
 $i
 end
+
+cd $QCNOMEN
+foreach i (`ls *.sql`)
+sql.sh $NOMEN $i
+end
+
