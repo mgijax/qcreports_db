9c6ebf5d441a8dda8e3e55383e116abb7ba770e3
Lori Corbani <Lori.Corbani@jax.org>
2000-06-27 17:57:10 +0000

    revised

--- mgd/MRK_Duplicates.sql
+++ mgd/MRK_Duplicates.sql
@@ -1,5 +1,21 @@
 print ""
-print "Duplicate Pending Markers"
+print "MGD Duplicate Symbols"
+print ""
+
+select _Marker_key, _Species_key, symbol, chromosome, cytogeneticOffset
+into #dup
+from MRK_Marker
+where _Marker_Status_key = 1
+group by _Species_key, symbol, chromosome, cytogeneticOffset having count(*) > 1
+go
+
+select d._Marker_key, s.species, d.symbol, d.chromosome, d.cytogeneticOffset 
+from #dup d, MRK_Species s
+where d._Species_key = s._Species_key
+go
+
+print ""
+print "MGD Duplicate Pending Markers"
 print ""
 
 select m1._Marker_key, m1.symbol, m2._Marker_key, m2.symbol
@@ -7,14 +23,5 @@ from MRK_Marker m1, MRK_Marker m2
 where m1._Species_key = 1
 and m1.symbol = m2.symbol + "-pending"
 and m2._Species_key = 1
-go
-
-print ""
-print "Duplicate Markers For Other Species"
-print ""
-
-select _Marker_key, _Species_key, symbol, chromosome, cytogeneticOffset from MRK_Marker
-where _Species_key != 1
-group by _Species_key, symbol having count(*) > 1
 go
 
