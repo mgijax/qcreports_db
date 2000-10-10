3d6cb286ff9607a49865db4b7c680e78b097d76d
Lori Corbani <Lori.Corbani@jax.org>
2000-10-10 17:27:43 +0000

    TR 2004

--- mgd/GXD_NotGene.sql
+++ mgd/GXD_NotGene.sql
@@ -9,3 +9,14 @@ and m._Marker_Type_key != 1
 order by v.symbol
 go
 
+print ""
+print "GXD Index with Markers which are not Genes"
+print ""
+
+select v.jnumID, v.symbol
+from GXD_Index_View v, MRK_Marker m
+where v._Marker_key = m._Marker_key
+and m._Marker_Type_key != 1
+order by v.symbol
+go
+
