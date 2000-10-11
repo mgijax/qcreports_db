74fa764198df23a433f376e09a5b45d700e7b88d
Lori Corbani <Lori.Corbani@jax.org>
2000-10-11 18:50:58 +0000

    TR 2004

--- mgd/GXD_NotGene.sql
+++ mgd/GXD_NotGene.sql
@@ -2,10 +2,11 @@ print ""
 print "GXD Assays with Markers which are not Genes"
 print ""
 
-select v.mgiID, v.jnumID, v.symbol, v.assayType
-from GXD_Assay_View v, MRK_Marker m
+select v.mgiID, v.jnumID, v.symbol, t.name, v.assayType
+from GXD_Assay_View v, MRK_Marker m, MRK_Types t
 where v._Marker_key = m._Marker_key
 and m._Marker_Type_key != 1
+and m._Marker_Type_key = t._Marker_Type_key
 order by v.symbol
 go
 
@@ -13,10 +14,11 @@ print ""
 print "GXD Index with Markers which are not Genes"
 print ""
 
-select v.jnumID, v.symbol
-from GXD_Index_View v, MRK_Marker m
+select v.jnumID, v.symbol, t.name
+from GXD_Index_View v, MRK_Marker m, MRK_Types t
 where v._Marker_key = m._Marker_key
 and m._Marker_Type_key != 1
+and m._Marker_Type_key = t._Marker_Type_key
 order by v.symbol
 go
 
