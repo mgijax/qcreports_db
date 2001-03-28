96c4a6093487f9852824c2631313f049826dd389
Lori Corbani <Lori.Corbani@jax.org>
2001-03-28 18:04:05 +0000

    TR 2440

--- mgd/PRB_StrainStd.sql
+++ mgd/PRB_StrainStd.sql
@@ -1,9 +1,12 @@
 print ""
-print "Strains - Standard"
+print "Strains - Standard (excluding F1s and F2s)"
 print ""
 
 select strain from PRB_Strain
 where standard = 1
+and strain not like '(%'
+and strain not like '%)F1'
+and strain not like '%)F2'
 order by strain
 go
 
