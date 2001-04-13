bd4d4c37ba41e152900f0fbccb3f174fe3433ea3
Lori Corbani <Lori.Corbani@jax.org>
2001-04-13 17:11:36 +0000

    TR 2495

--- mgd/MRK_Alleles.sql
+++ mgd/MRK_Alleles.sql
@@ -21,20 +21,30 @@ where a._Marker_key = m._Marker_key
 and a.symbol not like m.symbol + "%"
 go
 
-print ""
-print "Markers which contain Alleles"
-print ""
+set nocount on
+go
 
-select distinct m.symbol "Symbol", m.chromosome "Chr", substring(m.name, 1, 40) "Name"
-from MRK_Marker m, ALL_Allele a
-where m._Species_key = 1
-and m._Marker_key = a._Marker_key
-order by m.symbol
+select _Allele_key
+into #duplicates
+from ALL_Allele
+group by symbol having count(*) > 1
+go
+
+set nocount off
 go
 
 print ""
-print "Withdrawn Allele Markers not found in Allele Table"
+print "Duplicate Allele Symbols"
 print ""
+
+select a.symbol, a.markerSymbol
+from #duplicates d, ALL_Allele_View a
+where d._Allele_key = a._Allele_key
+order by markerSymbol
+go
+
+set nocount on
+go
 
 select m2._Marker_key, m2.symbol, _Allele_key = m1._Marker_key, allele = m1.symbol
 into #alleles
@@ -44,6 +54,13 @@ and m1.name like '%allele of%'
 and m1._Marker_key = c._Marker_key
 and c._Current_key = m2._Marker_key
 go
+
+set nocount off
+go
+
+print ""
+print "Withdrawn Allele Markers not found in Allele Table"
+print ""
 
 select m.symbol "Marker Symbol", m.allele "Allele Symbol"
 from #alleles m
