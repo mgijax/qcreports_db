3f13def7889166d55b52eb74e810cd540f66b640
Lori Corbani <Lori.Corbani@jax.org>
2001-09-19 15:07:50 +0000

    fix

--- mgd/MLP_Nomenclature.sql
+++ mgd/MLP_Nomenclature.sql
@@ -1,11 +1,13 @@
 print ""
-print "Strains Affected by recent Nomenclature changes"
+print "Strains Affected by Recent Nomenclature Changes"
+print ""
+print "These Strains have their 'Needs Review' flag set to 'Yes'."
+print "To remove a Strain from this report, set the flag to 'No'."
 print ""
 
 select strain 
 from PRB_Strain 
 where needsReview = 1
-and strain not like "%<%>%"
 order by strain
 go
 
