750fe38160353b0087206ea1acc845679387eea8
Lori Corbani <Lori.Corbani@jax.org>
2001-09-24 14:30:07 +0000

    TR 2961

--- mgd/MRK_DashPending.sql
+++ mgd/MRK_DashPending.sql
@@ -4,17 +4,12 @@ go
 select m._Marker_key, m.symbol, name = substring(m.name,1,50), m.creation_date
 into #marker
 from MRK_Marker m
-where m.symbol like '%-pending'
+where m._Species_key = 1
+and m.symbol like '%-pending'
 go
 
-set nocount off
-go
-
-print ""
-print "-pending Markers"
-print ""
-
-select distinct m.symbol "Mouse Symbol", m2.symbol "Human Symbol", m.name "Mouse Name", m.creation_date
+select distinct msymbol = m.symbol, hsymbol = m2.symbol, m.name, m.creation_date
+into #homology
 from #marker m, HMD_Homology r1, HMD_Homology_Marker h1,
 HMD_Homology r2, HMD_Homology_Marker h2, MRK_Marker m2
 where m._Marker_key = h1._Marker_key
@@ -34,6 +29,35 @@ and r1._Class_key = r2._Class_key
 and r2._Homology_key = h2._Homology_key
 and h2._Marker_key = m2._Marker_key
 and m2._Species_key = 2)
-order by m.creation_date, m.symbol
+go
+
+select h.*, type = "O"
+into #results
+from #homology h, tempdb..LL l
+where h.hsymbol = l.osymbol
+and l.taxID = 9606
+union
+select h.*, type = "I"
+from #homology h, tempdb..LL l
+where h.hsymbol = l.isymbol
+and l.taxID = 9606
+union
+select h.*, type = "?"
+from #homology h
+where not exists (select 1 from tempdb..LL l
+where l.taxID = 9606 and (h.hsymbol = l.osymbol or h.hsymbol = l.isymbol))
+go
+
+set nocount off
+go
+
+print ""
+print "-pending Markers with Human Homologies"
+print "(sorted by human symbol type, creation date, mouse symbol)"
+print ""
+
+select msymbol "Mouse Symbol", type "Type", hsymbol "Human Symbol", name "Mouse Name", creation_date
+from #results
+order by type desc, creation_date, msymbol
 go
 
