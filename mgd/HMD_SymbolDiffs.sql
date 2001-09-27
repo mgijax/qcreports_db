a1243e76817f9df309b49e24b39fb41947a33f9b
Lori Corbani <Lori.Corbani@jax.org>
2001-09-27 15:07:55 +0000

    TR 2976

--- mgd/HMD_SymbolDiffs.sql
+++ mgd/HMD_SymbolDiffs.sql
@@ -1,20 +1,13 @@
-print ""
-print "MGD Mouse Symbols which differ from Orthologous Human Symbols"
-print ""
+set nocount on
+go
 
 select distinct 
-	m1.symbol "MGI Symbol", 
-	m2.symbol "MGI Human Symbol", 
-	a1.accID "MGI ID", 
-	substring(m1.name, 1, 40) "MGI Name",
-	l.locusID "LL ID",
-	substring(m2.name, 1, 40) "Human Name"
-
+       m_Marker_key = m1._Marker_key, msymbol = m1.symbol, mname = substring(m1.name, 1, 40), 
+       h_Marker_key = m2._Marker_key, hsymbol = m2.symbol, hname = substring(m2.name, 1, 40)
+into #homology
 from HMD_Homology r1, HMD_Homology_Marker h1,
      HMD_Homology r2, HMD_Homology_Marker h2,
-     MRK_Marker m1, MRK_Marker m2, MRK_ACC_View a1, MRK_ACC_View a2,
-     tempdb..LL l
-
+     MRK_Marker m1, MRK_Marker m2
 where m1._Species_key = 1 
 and m1._Marker_key = h1._Marker_key 
 and h1._Homology_key = r1._Homology_key 
@@ -23,13 +16,59 @@ and r2._Homology_key = h2._Homology_key
 and h2._Marker_key = m2._Marker_key 
 and m2._Species_key = 2 
 and m1.symbol != m2.symbol
-and m2.symbol not like '*%'
-and m1._Marker_key = a1._Object_key
+go
+
+select h.*, mgiID = a1.accID, locusID = a2.accID
+into #markers
+from #homology h, MRK_ACC_View a1, MRK_ACC_View a2
+where h.m_Marker_key = a1._Object_key
 and a1.prefixPart = "MGI:"
 and a1.preferred = 1
-and m2._Marker_key = a2._Object_key
-and a2.prefixPart = "GDB:"
-and a2.accID = l.gsdbid
-order by m1.symbol
+and h.h_Marker_key = a2._Object_key
+and a2._LogicalDB_key = 24
+union
+select h.*, mgiID = a1.accID, locusID = null
+from #homology h, MRK_ACC_View a1
+where h.m_Marker_key = a1._Object_key
+and a1.prefixPart = "MGI:"
+and a1.preferred = 1
+and not exists (select 1 from MRK_ACC_View a2
+where h.h_Marker_key = a2._Object_key
+and a2._LogicalDB_key = 24)
+go
+
+select m.*, type = "O"
+into #results
+from #markers m, tempdb..LL l
+where m.locusID = l.locusID
+and m.hsymbol = l.osymbol
+union
+select m.*, type = "I"
+from #markers m, tempdb..LL l
+where m.locusID = l.locusID
+and m.hsymbol = l.isymbol
+union
+select m.*, type = "?"
+from #markers m
+where not exists (select 1 from tempdb..LL l
+where m.locusID = l.locusID and (m.hsymbol = l.osymbol or m.hsymbol = l.isymbol))
+go
+
+set nocount off
+go
+
+print ""
+print "MGD Mouse Symbols which differ from Orthologous Human Symbols"
+print ""
+
+select msymbol "MGI Symbol", 
+       hsymbol "MGI Human Symbol", 
+       type "Type",
+       mgiID "MGI ID", 
+       mname "MGI Name",
+       locusID "LL ID",
+       hname "Human Name"
+from #results
+order by type desc, msymbol
 go
 
