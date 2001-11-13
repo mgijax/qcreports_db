e858a65134b9348fbf822bd3708ef5d4929af02f
Lori Corbani <Lori.Corbani@jax.org>
2001-11-13 13:49:11 +0000

    TR 3095

--- mgd/HMD_LocusLink2.sql
+++ mgd/HMD_LocusLink2.sql
@@ -4,6 +4,30 @@
 /* ftp site using the locuslinkload product */
 /* This file is dumped into tempdb..LLHomology */
 
+set nocount on
+go
+
+select type = "O", h.*
+into #homology
+from tempdb..LLHomology h, tempdb..LL l
+where h.species1 = "Homo sapiens"
+and h.species2 = "Mus musculus"
+and h.symbol1 = h.symbol2
+and h.locusID1 = l.locusID
+and l.osymbol is not null
+union
+select type = "I",  h.*
+from tempdb..LLHomology h, tempdb..LL l
+where h.species1 = "Homo sapiens"
+and h.species2 = "Mus musculus"
+and h.symbol1 = h.symbol2
+and h.locusID1 = l.locusID
+and l.isymbol is not null
+go
+
+set nocount off
+go
+
 print ""
 print "The mouse/human orthologous pairs that LocusLink is reporting that we are"
 print "not reporting in our homology data, where we have neither the mouse nor the"
@@ -15,7 +39,7 @@ print "HUMAN AND MOUSE SYMBOLS ARE NOT THE SAME"
 print ""
 
 select h.*
-from tempdb..LLHomology h
+from #homology h
 where h.species1 = "Homo sapiens"
 and h.species2 = "Mus musculus"
 and h.symbol1 != h.symbol2
@@ -24,7 +48,7 @@ where m._Species_key = 1
 and m.symbol = h.symbol2)
 union
 select h.*
-from tempdb..LLHomology h
+from #homology h
 where h.species1 = "Homo sapiens"
 and h.species2 = "Mus musculus"
 and h.symbol1 != h.symbol2
@@ -32,6 +56,6 @@ and
 not exists (select 1 from MRK_Marker m
 where m._Species_key = 2
 and m.symbol = h.symbol1)
-order by h.symbol1
+order by h.type desc, h.symbol1
 go
 
