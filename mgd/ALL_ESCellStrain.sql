cbab2d336f65eeaf37b3a81e13deaf20eaaa22d7
Lori Corbani <Lori.Corbani@jax.org>
2001-12-10 16:18:35 +0000

    per cml

--- mgd/ALL_ESCellStrain.sql
+++ mgd/ALL_ESCellStrain.sql
@@ -2,14 +2,15 @@ print ""
 print "ES Cell Line Strains Modified Last Month - Sorted by Modification Date"
 print ""
 
-select distinct e.cellLine, s.strain, s.modification_date
-from ALL_CellLine e, PRB_Strain s
+select distinct a.mgiID, e.cellLine, s.strain, s.modification_date
+from ALL_Allele_View a, ALL_CellLine e, PRB_Strain s
 where e._Strain_key = s._Strain_key
 and datepart(year, s.modification_date) = datepart(year, getdate())
 and datepart(month, s.modification_date) = datepart(month, getdate()) - 1
+and e._CellLine_key = a._CellLine_key
 union
-select distinct null, s.strain, s.modification_date
-from ALL_Allele a, PRB_Strain s
+select distinct a.mgiID, null, s.strain, s.modification_date
+from ALL_Allele_View a, PRB_Strain s
 where a._Strain_key = s._Strain_key
 and datepart(year, s.modification_date) = datepart(year, getdate())
 and datepart(month, s.modification_date) = datepart(month, getdate()) - 1
