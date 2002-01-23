4b84f96f4c3905e25013af376a377d6d23c0ad61
Lori Corbani <Lori.Corbani@jax.org>
2002-01-23 16:13:11 +0000

    TR 3301

---
+++ mgd/HMD_DupGDB.sql
@@ -0,0 +1,31 @@
+set nocount on
+go
+
+select distinct m._Marker_key, a.accID
+into #markers
+from MRK_Marker m, MRK_Acc_View a
+where m._Species_key = 2
+and m._Marker_key = a._Object_key
+and a.prefixPart = "GDB:"
+go
+
+select *
+into #dups
+from #markers
+group by _Marker_key having count(*) > 1
+go
+
+set nocount off
+go
+
+print ""
+print "Human Markers with > 1 GDB record ID"
+print ""
+
+select m.symbol, a.accID "LL ID", d.accID "GDB ID"
+from #dups d, MRK_Marker m, MRK_Acc_View a
+where d._Marker_key = m._Marker_key
+and d._Marker_key = a._Object_key
+and a._LogicalDB_key = 24
+order by m.symbol
+go
---
+++ mgd/HMD_GDBLocusLink.sql
@@ -0,0 +1,34 @@
+set nocount on
+go
+
+select distinct m.symbol, a.accID
+into #markers
+from MRK_Marker m, MRK_Acc_View a
+where m._Species_key = 2
+and m._Marker_key = a._Object_key
+and a.prefixPart = "GDB:"
+go
+
+create index index_symbol on #markers (symbol)
+go
+
+set nocount off
+go
+
+print ""
+print "Human Markers with > 1 GDB record ID in LocusLink"
+print ""
+
+select m.symbol, l.locusID "LL ID", m.accID "MGD GDB ID", l.gsdbID "LL GDB ID"
+from #markers m, tempdb..LL l
+where m.symbol = l.osymbol
+and l.taxID = 9606
+and m.accID != l.gsdbID
+union
+select m.symbol, l.locusID, m.accID, l.gsdbID
+from #markers m, tempdb..LL l
+where m.symbol = l.isymbol
+and l.taxID = 9606
+and m.accID != l.gsdbID
+order by m.symbol
+go
