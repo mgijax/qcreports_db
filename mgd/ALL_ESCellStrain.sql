7ff11a97694b1a05358a73108bf103e872933184
Lori Corbani <Lori.Corbani@jax.org>
2002-04-03 16:56:47 +0000

    TR 3531

--- mgd/ALL_ESCellStrain.sql
+++ mgd/ALL_ESCellStrain.sql
@@ -7,13 +7,8 @@ from ALL_Allele_View a, ALL_CellLine e, PRB_Strain s
 where e._Strain_key = s._Strain_key
 and datepart(year, s.modification_date) = datepart(year, getdate())
 and datepart(month, s.modification_date) = datepart(month, getdate()) - 1
+and e._CellLine_key != -2
 and e._CellLine_key = a._CellLine_key
-union
-select distinct a.mgiID, null, s.strain, s.modification_date
-from ALL_Allele_View a, PRB_Strain s
-where a._Strain_key = s._Strain_key
-and datepart(year, s.modification_date) = datepart(year, getdate())
-and datepart(month, s.modification_date) = datepart(month, getdate()) - 1
 order by s.modification_date desc, s.strain
 
 go
