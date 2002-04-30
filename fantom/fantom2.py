b04ad44125eca41894a7c87868a3d3bf804ab620
Lori Corbani <Lori.Corbani@jax.org>
2002-04-30 12:30:17 +0000

    TR 3639

--- fantom/fantom2.py
+++ fantom/fantom2.py
@@ -97,24 +97,7 @@ def curated_clusters():
 
 	cmds.append('create unique index index_cluster on #curated_cluster(riken_cluster)')
 
-def invalid_finalmgiID():
-
-	#
-	# the set of clones where the final MGI id is an invalid clone id
-	#
-
-	global cmds
-
-	cmds.append('select distinct f1._Fantom2_key, f1.riken_cloneid, f1.riken_cluster ' + \
-		'into #invalidmgiID ' + \
-		'from MGI_Fantom2 f1 ' + \
-		'where f1.final_mgiID != "zilch" ' + \
-		'and f1.final_mgiID != "zzilch" ' + \
-		'and f1.final_mgiID not like "MGI:%" ' + \
-		'and not exists (select 1 from MGI_Fantom2 f2 ' + \
-		'where f1.final_mgiID = f2.riken_cloneid)')
-
-	cmds.append('create unique index index_key on #invalidmgiID(_Fantom2_key)')
+	# results[0] and results[1]
 
 def incomplete_cluster():
 
@@ -139,6 +122,37 @@ def incomplete_cluster():
 
 	cmds.append('create unique index index_key on #incomplete_cluster(_Fantom2_key)')
 
+	# results[5]
+	cmds.append('select f1.riken_cloneid, f1.riken_cluster ' + \
+		'from #incomplete_cluster c, MGI_Fantom2 f1 ' + 
+		'where c._Fantom2_key = f1._Fantom2_key ' + \
+		'order by f1.riken_cluster')
+
+def invalid_finalmgiID():
+
+	#
+	# the set of clones where the final MGI id is an invalid clone id
+	#
+
+	global cmds
+
+	cmds.append('select distinct f1._Fantom2_key, f1.riken_cloneid, f1.riken_cluster ' + \
+		'into #invalidmgiID ' + \
+		'from MGI_Fantom2 f1 ' + \
+		'where f1.final_mgiID != "zilch" ' + \
+		'and f1.final_mgiID != "zzilch" ' + \
+		'and f1.final_mgiID not like "MGI:%" ' + \
+		'and not exists (select 1 from MGI_Fantom2 f2 ' + \
+		'where f1.final_mgiID = f2.riken_cloneid)')
+
+	cmds.append('create unique index index_key on #invalidmgiID(_Fantom2_key)')
+
+	# results[8]
+	cmds.append('select f1.riken_cloneid, f1.riken_cluster, f1.final_mgiID ' + \
+		'from #invalidmgiID c, MGI_Fantom2 f1 ' + \
+		'where c._Fantom2_key = f1._Fantom2_key ' + \
+		'order by f1.final_mgiID')
+
 def multiple_representative():
 
 	#
@@ -147,7 +161,8 @@ def multiple_representative():
 
 	global cmds
 
-	cmds.append('select f1._Fantom2_key, f1.riken_cloneid, f1.final_mgiID, f1.genbank_id, f1.riken_cluster ' + \
+	cmds.append('select f1._Fantom2_key, f1.riken_cloneid, f1.final_mgiID, ' + \
+		'f1.genbank_id, f1.riken_cluster ' + \
 		'into #rep2 ' + \
 		'from MGI_Fantom2 f1 ' + \
 		'where f1.seq_note = "Representative" ' + \
@@ -155,6 +170,9 @@ def multiple_representative():
 
 	cmds.append('select * into #mult_rep from #rep2 group by final_mgiID having count(*) > 1')
 	cmds.append('create unique index index_key on #mult_rep(_Fantom2_key)')
+
+	# results[12]
+	cmds.append('select * from #mult_rep order by final_mgiID')
 
 def no_rep_clone():
 
@@ -165,7 +183,8 @@ def no_rep_clone():
 
 	global cmds
 
-	cmds.append('select distinct f1._Fantom2_key, f1.final_mgiID, f1.riken_cloneid, f1.riken_cluster ' + \
+	cmds.append('select distinct f1._Fantom2_key, f1.final_mgiID, ' + \
+		'f1.riken_cloneid, f1.riken_cluster ' + \
 		'into #curated ' + \
 		'from MGI_Fantom2 f1 ' + \
 		'where f1.final_mgiID like "MGI:%" ' + \
@@ -191,6 +210,9 @@ def no_rep_clone():
 
 	cmds.append('create unique index index_key on #no_rep_clone(_Fantom2_key)')
 
+	# results[20]
+	cmds.append('select * from #no_rep_clone order by final_mgiID')
+
 def nonriken_rep_clone():
 	
 	#
@@ -210,6 +232,12 @@ def nonriken_rep_clone():
 	cmds.append('select * into #nonriken from #nondups where riken_seqid = -1')
 
 	cmds.append('create index index_key on #nonriken(riken_cluster)')
+
+	# results[25]
+	cmds.append('select f1.final_mgiID, f1.genbank_id, f1.unigene_id, f1.tiger_tc ' + \
+	            'from #nonriken n, MGI_Fantom2 f1 ' + \
+		    'where n.final_mgiID = f1.final_mgiID ' + \
+		    'order by final_mgiID')
 
 def rep_discrepency():
 
@@ -236,56 +264,62 @@ def rep_discrepency():
 
 	cmds.append('create unique index index_key on #rep_discr(_Fantom2_key)')
 
+	# results[30]
+	cmds.append('select * from #rep_discr order by final_mgiID')
+
+def riken():
+
+	#
+	# the set of curated clusters
+	#
+
+	global cmds
+
+	# results[33]
+	cmds.append('select f1._Fantom2_key, f2.note ' + \
+		'from #curated_cluster c, MGI_Fantom2 f1, MGI_Fantom2Notes f2 ' + \
+		'where c.riken_cluster = f1.riken_cluster ' + \
+		'and not exists (select 1 from #excluded_cluster e ' + \
+		'where f1.riken_cluster = e.riken_cluster) ' + \
+		'and f1._Fantom2_key = f2._Fantom2_key ' + \
+		'and f2.noteType = "C" ')
+
+	# results[34]
+	cmds.append('select f1._Fantom2_key, f1.riken_seqid, f1.riken_cloneid, ' + \
+		'f1.riken_cluster, f1.final_mgiID, ' + \
+		'f1.seq_quality, f1.seq_note ' + \
+		'from #curated_cluster c, MGI_Fantom2 f1 ' + \
+		'where c.riken_cluster = f1.riken_cluster ' + \
+		'and not exists (select 1 from #excluded_cluster e ' + \
+		'where f1.riken_cluster = e.riken_cluster) ' + \
+		'order by f1.final_mgiID, f1.riken_cluster')
+
+def incomplete_cluster_report():
+
+	fp = reportlib.init('FAN2_IncompleteCluster', outputdir = os.environ['QCREPORTOUTPUTDIR'], printHeading = 0)
+
+	for r in results[5]:
+		fp.write(r['riken_cloneid'] + TAB + \
+			`r['riken_cluster']` + CRT)
+	
+	reportlib.finish_nonps(fp)
+
 def invalid_finalmgiID_report():
 
 	fp = reportlib.init('FAN2_InvalidFinalMGIID', outputdir = os.environ['QCREPORTOUTPUTDIR'], printHeading = 0)
 
-	invalid_finalmgiID()
-
-	cmds.append('select f1.riken_cloneid, f1.riken_cluster, f1.final_mgiID ' + \
-		'from #invalidmgiID c, MGI_Fantom2 f1 ' + \
-		'where c._Fantom2_key = f1._Fantom2_key ' + \
-		'order by f1.final_mgiID')
-
-	results = db.sql(cmds, 'auto')
-
-	for r in results[-1]:
+	for r in results[8]:
 		fp.write(r['riken_cloneid'] + TAB + \
 			`r['riken_cluster']` + TAB + \
 			r['final_mgiID'] + CRT)
 
 	reportlib.finish_nonps(fp)
 
-def incomplete_cluster_report():
-
-	fp = reportlib.init('FAN2_IncompleteCluster', outputdir = os.environ['QCREPORTOUTPUTDIR'], printHeading = 0)
-
-	incomplete_cluster()
-
-	cmds.append('select f1.riken_cloneid, f1.riken_cluster ' + \
-		'from #incomplete_cluster c, MGI_Fantom2 f1 ' + 
-		'where c._Fantom2_key = f1._Fantom2_key ' + \
-		'order by f1.riken_cluster')
-
-	results = db.sql(cmds, 'auto')
-	
-	for r in results[-1]:
-		fp.write(r['riken_cloneid'] + TAB + \
-			`r['riken_cluster']` + CRT)
-	
-	reportlib.finish_nonps(fp)
-
 def multiple_representative_report():
 
 	fp = reportlib.init('FAN2_MultRepClone', outputdir = os.environ['QCREPORTOUTPUTDIR'], printHeading = 0)
 
-	multiple_representative()
-
-	cmds.append('select * from #mult_rep order by final_mgiID')
-
-	results = db.sql(cmds, 'auto')
-
-	for r in results[-1]:
+	for r in results[12]:
 		fp.write(r['riken_cloneid'] + TAB + \
 			r['genbank_id'] + TAB + \
 			`r['riken_cluster']` + TAB + \
@@ -297,13 +331,7 @@ def no_rep_clone_report():
 
 	fp = reportlib.init('FAN2_NoRepClone', outputdir = os.environ['QCREPORTOUTPUTDIR'], printHeading = 0)
 
-	no_rep_clone()
-
-	cmds.append('select * from #no_rep_clone order by final_mgiID')
-
-	results = db.sql(cmds, 'auto')
-
-	for r in results[-1]:
+	for r in results[20]:
 		fp.write(r['riken_cloneid'] + TAB + \
 			`r['riken_cluster']` + TAB + \
 			r['final_mgiID'] + CRT)
@@ -314,16 +342,7 @@ def nonriken_rep_clone_report():
 
 	fp = reportlib.init('FAN2_NonRikenRepClone', outputdir = os.environ['QCREPORTOUTPUTDIR'], printHeading = 0)
 
-	nonriken_rep_clone()
-
-	cmds.append('select f1.final_mgiID, f1.genbank_id, f1.unigene_id, f1.tiger_tc ' + \
-	            'from #nonriken n, MGI_Fantom2 f1 ' + \
-		    'where n.final_mgiID = f1.final_mgiID ' + \
-		    'order by final_mgiID')
-
-	results = db.sql(cmds, 'auto')
-
-	for r in results[-1]:
+	for r in results[25]:
 		fp.write(r['genbank_id'] + TAB + \
 			r['unigene_id'] + TAB + \
 			r['tiger_tc'] + TAB + \
@@ -335,13 +354,7 @@ def rep_discrepency_report():
 
 	fp = reportlib.init('FAN2_DiscrRepClone', outputdir = os.environ['QCREPORTOUTPUTDIR'], printHeading = 0)
 
-	rep_discrepency()
-
-	cmds.append('select * from #rep_discr order by final_mgiID')
-
-	results = db.sql(cmds, 'auto')
-	
-	for r in results[-1]:
+	for r in results[30]:
 		fp.write(r['riken_cloneid'] + TAB + \
 			r['riken_cluster'] + TAB + \
 			r['seq_note'] + TAB + \
@@ -353,45 +366,15 @@ def riken_report():
 
 	fp = reportlib.init(sys.argv[0], fileExt = '.mgi', outputdir = os.environ['QCREPORTOUTPUTDIR'], printHeading = 0)
 
-	invalid_finalmgiID()
-	incomplete_cluster()
-	multiple_representative()
-	no_rep_clone()
-	nonriken_rep_clone()
-	rep_discrepency()
-	excluded_clusters()
-
-	#
-	# get curator notes
-	#
-
-	cmds.append('select f1._Fantom2_key, f2.note ' + \
-		'from #curated_cluster c, MGI_Fantom2 f1, MGI_Fantom2Notes f2 ' + \
-		'where c.riken_cluster = f1.riken_cluster ' + \
-		'and not exists (select 1 from #excluded_cluster e ' + \
-		'where f1.riken_cluster = e.riken_cluster) ' + \
-		'and f1._Fantom2_key = f2._Fantom2_key ' + \
-		'and f2.noteType = "C" ')
-
-	cmds.append('select f1._Fantom2_key, f1.riken_seqid, f1.riken_cloneid, f1.riken_cluster, f1.final_mgiID, ' + \
-		'f1.seq_quality, f1.seq_note ' + \
-		'from #curated_cluster c, MGI_Fantom2 f1 ' + \
-		'where c.riken_cluster = f1.riken_cluster ' + \
-		'and not exists (select 1 from #excluded_cluster e ' + \
-		'where f1.riken_cluster = e.riken_cluster) ' + \
-		'order by f1.final_mgiID, f1.riken_cluster')
-
-	results = db.sql(cmds, 'auto')
-	
 	notes = {}
-	for r in results[-2]:
+	for r in results[33]:
 		if not notes.has_key(r['_Fantom2_key']):
 			notes[r['_Fantom2_key']] = r['note']
 		else:
 			newnote = notes[r['_Fantom2_key']] + r['note']
 			notes[r['_Fantom2_key']] = newnote
 
-	for r in results[-1]:
+	for r in results[34]:
 		fp.write(`r['riken_seqid']` + TAB + \
 			r['riken_cloneid'] + TAB + \
 			`r['riken_cluster']` + TAB + \
@@ -417,16 +400,30 @@ def riken_report():
 # Main
 #
 
+# use dbo login since the tables are read-protected
 db.set_sqlUser(os.environ['DBOUSER'])
 db.set_sqlPassword(string.strip(open(os.environ['DBOPASSWORDFILE'], 'r').readline()))
 
+# initialize the queries
 cmds = []
+incomplete_cluster()
+invalid_finalmgiID()
+multiple_representative()
+no_rep_clone()
+nonriken_rep_clone()
+rep_discrepency()
+excluded_clusters()	# set of clusters to exlude from final riken report
+riken()
 
+# execute all of the queries
+results = db.sql(cmds, 'auto')
+
+# print the reports
 incomplete_cluster_report()
 invalid_finalmgiID_report()
 multiple_representative_report()
 no_rep_clone_report()
 nonriken_rep_clone_report()
 rep_discrepency_report()
-#riken_report()
+riken_report()
 
