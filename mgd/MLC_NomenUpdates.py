864036da519a02b7198431026a5904980238a469
Lori Corbani <Lori.Corbani@jax.org>
2002-06-28 18:41:28 +0000

    refined

--- mgd/MLC_NomenUpdates.py
+++ mgd/MLC_NomenUpdates.py
@@ -27,6 +27,7 @@
 import sys 
 import os
 import db
+import mgi_html
 import mgi_utils
 import reportlib
 
@@ -45,7 +46,12 @@ os.system('mv %s/MLCNomenclature-*.html %s' % (os.environ['QCREPORTOUTPUTDIR'], 
 
 reportName = 'MLCNomenclature-' + mgi_utils.date('%Y-%m-%d')
 
-currentDate = mgi_utils.date('%m/%d/%Y')
+if len(sys.argv) > 1:
+	reportName = 'MLCNomenclature-' + sys.argv[1]
+	currentDate = sys.argv[2]
+else:
+	reportName = 'MLCNomenclature-' + mgi_utils.date('%Y-%m-%d')
+	currentDate = mgi_utils.date('%m/%d/%Y')
 
 results = db.sql('select convert(varchar(25), dateadd(day, -7, "%s"))' % (currentDate), 'auto')
 bdate = results[0]['']
@@ -60,60 +66,59 @@ fp.write('J:23000 generally indicates gene family nomenclature revision event.\n
 
 cmd = []
 
-cmd.append('select m._Marker_key, m.mgiID, c.sequenceNum, h._Refs_key ' + \
-'into #m1 ' + \
-'from MRK_Mouse_View m, MRK_History h, MRK_Chromosome c, MLC_Text_edit mlc ' + \
-'where m.creation_date between dateadd(day, -7, "%s") ' % (currentDate) + \
-'and "%s" ' % (currentDate) + \
-'and m._Marker_key = h._Marker_key ' + \
+cmd.append('select distinct h._Marker_key, m.symbol, name = substring(m.name,1,35), ' + \
+'chromosome = substring(m.chromosome,1,2), ' + \
+'c.sequenceNum, ' + \
+'b.jnumID, author = substring(b._primary,1,16) ' + \
+'into #markers ' + \
+'from MRK_Marker m, MLC_Text_edit mlc, MRK_History h, MRK_Chromosome c, BIB_All_View b ' + \
+'where m._Species_key = 1 ' + \
 'and m._Marker_key = h._History_key ' + \
-'and h._Marker_Event_key = 1 ' + \
+'and h.event_date between dateadd(day, -7, "%s") ' % (currentDate) + \
+'and dateadd(day, 1, "%s") ' % (currentDate) + \
+'and h._Marker_key = mlc._Marker_key ' + \
 'and m.chromosome = c.chromosome ' + \
 'and m._Species_key = c._Species_key ' + \
-'and m._Marker_key = mlc._Marker_key ' + \
-'union ' + \
-'select h._History_key, m.mgiID, sequenceNum = 100, h._Refs_key ' + \
-'from MRK_History h, MRK_Mouse_View m, MLC_Text_edit mlc ' + \
-'where h.event_date between dateadd(day, -7, "%s") ' % (currentDate) + \
-'and "%s" ' % (currentDate) + \
-'and h._Marker_key = m._Marker_key ' + \
-'and h._Marker_Event_key in (2,3,4,5) ' + \
-'and m._Marker_key = mlc._Marker_key'
-)
+'and h._Refs_key = b._Refs_key ')
 
-cmd.append('select ' + \
-'chr = substring(r.chromosome,1,2), ' + \
-'m.mgiID, r.symbol,  ' + \
-'name = substring(r.name,1,25),  ' + \
-'b.jnumID, jnum = convert(char(6), b.jnum), ' + \
-'author = substring(b._primary, 1, 16) ' + \
-'from #m1 m, MRK_Marker r, BIB_All_View b ' + \
-'where m._Marker_key = r._Marker_key ' + \
-'and m._Refs_key = b._Refs_key ' + \
-'order by m.sequenceNum, r.symbol'
-)
+cmd.append('create nonclustered index idx_key on #markers(_Marker_key)')
+
+# get primary MGI ids (2)
+cmd.append('select distinct m._Marker_key, a.accID ' + \
+'from #markers m, MRK_Acc_View a ' + \
+'where m._Marker_key = a._Object_key ' + \
+'and a.prefixPart = "MGI:" ' + \
+'and a.preferred = 1')
+
+# retrieve markers, sort (3)
+cmd.append('select * from #markers order by sequenceNum, symbol')
 
 results = db.sql(cmd, 'auto')
 
-fp.write('%-2s %-25s %-25s %-6s %-16s\n' % ('Ch', 'Symbol', 'Gene Name', 'J#', 'First Author'))
-fp.write('%-2s %-25s %-25s %-6s %-16s\n' % ('--', '------', '---------', '--', '------------'))
+primaryID = {}
+for r in results[2]:
+	primaryID[r['_Marker_key']] = r['accID']
+
+fp.write('%-2s %-25s %-35s %-10s %-16s\n' % ('Ch', 'Symbol', 'Gene Name', 'J#', 'First Author'))
+fp.write('%-2s %-25s %-35s %-10s %-16s\n' % ('--', '------', '---------', '--', '------------'))
 
 rows = 0
-for r in results[1]:
-	fp.write('%-2s ' % (r['chr']))
+for r in results[3]:
 
-	if r['mgiID'] != "None":
-		fp.write('%s%-10s%s ' % (reportlib.create_accession_anchor(r['mgiID']), r['symbol'], reportlib.close_accession_anchor()))
-	else:
-		fp.write('%-25s ' % (r['symbol']))
-		
-	fp.write('%-25s ' % (r['name']))
-	fp.write('%s%-10s%s ' % (reportlib.create_accession_anchor(r['jnumID']), r['jnum'], reportlib.close_accession_anchor()))
-	fp.write('%-16s\n' % (r['author']))
+	key = r['_Marker_key']
+	symbol = mgi_html.escape(r['symbol'])
+
+	fp.write('%-2s ' % (r['chromosome']))
+
+	fp.write('%s%-25s%s ' % (reportlib.create_accession_anchor(primaryID[key]), symbol, reportlib.close_accession_anchor()))
+	fp.write('%-35s ' % (r['name']))
+	fp.write('%s%-10s%s ' % (reportlib.create_accession_anchor(r['jnumID']), r['jnumID'], reportlib.close_accession_anchor()))
+	fp.write('%-20s ' % (r['author']))
+	fp.write(reportlib.CRT)
+
 	rows = rows + 1
 
-fp.write('\n(%d rows affected)\n' % (rows))
-
+fp.write(reportlib.CRT + '(%d rows affected)' % (rows) + reportlib.CRT)
 reportlib.trailer(fp)
 reportlib.finish_nonps(fp, isHTML = 1)	# non-postscript file
 
