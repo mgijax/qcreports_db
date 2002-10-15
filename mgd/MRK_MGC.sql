ba44d4da19e44b6fda314289bf8642a046f4b8e3
Lori Corbani <Lori.Corbani@jax.org>
2002-10-15 15:56:29 +0000

    TR 4135

---
+++ mgd/MRK_MGC.sql
@@ -0,0 +1,17 @@
+print ""
+print "MGC Sequence Accession IDs in MGD which are also"
+print "associated with a Nomen record with a status other"
+print "than Deleted or Broadcast"
+print ""
+
+select a.accID
+from ACC_Accession a
+where a._LogicalDB_key = 9
+and a.prefixPart = "BC"
+and exists (select 1 from $NOMEN..ACC_Accession na, $NOMEN..MRK_Nomen n
+where a.accID = na.accID
+and na._Object_key = n._Nomen_key
+and n._Marker_Status_key not in (2,5))
+order by a.accID
+go
+
