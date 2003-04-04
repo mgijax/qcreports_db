438dbd927e550207099714451bef5a60decbdde8
Lori Corbani <Lori.Corbani@jax.org>
2003-04-04 13:17:11 +0000

    TR 4690

--- mgd/GXD_MechDev.sql
+++ mgd/GXD_MechDev.sql
@@ -3,8 +3,21 @@ go
 
 /* get all references, except only clones = 1 */
 
-select distinct i._Refs_key
+select distinct i._Refs_key, type = 'blot'
 into #refs
+from GXD_Index i, GXD_Index_Stages s, BIB_Refs b
+where i._Refs_key = b._Refs_key
+and b.journal = "Mech Dev"
+and (northern = 1
+     or western = 1
+     or rt_pcr = 1
+     or rnase = 1 
+     or nuclease = 1
+     or primer_extension =1
+)
+and not exists (select 1 from GXD_Assay a where i._Refs_key = a._Refs_key)
+union
+select distinct i._Refs_key, type = 'insitu'
 from GXD_Index i, GXD_Index_Stages s, BIB_Refs b
 where i._Refs_key = b._Refs_key
 and b.journal = "Mech Dev"
@@ -12,12 +25,6 @@ and (insitu_protein_section = 1
      or insitu_rna_section = 1
      or insitu_protein_mount = 1 
      or insitu_rna_mount = 1
-     or rnase = 1 
-     or nuclease = 1
-     or primer_extension =1
-     or northern = 1
-     or western = 1
-     or rt_pcr = 1
 )
 and not exists (select 1 from GXD_Assay a where i._Refs_key = a._Refs_key)
 go
@@ -25,8 +32,17 @@ go
 delete #refs
 from #refs r, GXD_Index i, GXD_Index_Stages s
 where r._Refs_key = i._Refs_key
+and r.type = 'blot'
 and i.index_id = s.index_id
-and s.stage_id between 40 and 41
+and s.stage_id = 40
+go
+
+delete #refs
+from #refs r, GXD_Index i, GXD_Index_Stages s
+where r._Refs_key = i._Refs_key
+and r.type = 'insitu'
+and i.index_id = s.index_id
+and s.stage_id in (40,41)
 go
 
 select r.*, i._Marker_key
