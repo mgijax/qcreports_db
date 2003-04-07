49159d582c08817b1aa123c0e8e5a7b297c342a4
Lori Corbani <Lori.Corbani@jax.org>
2003-04-07 13:49:55 +0000

    TR 4690

--- mgd/GXD_GeneExprPatterns.sql
+++ mgd/GXD_GeneExprPatterns.sql
@@ -1,93 +1,86 @@
 set nocount on
 go
 
+/* get set of references not coded */
+
 select distinct i._Refs_key
 into #refs
 from GXD_Index i, GXD_Index_Stages s, BIB_Refs b
 where i._Refs_key = b._Refs_key
 and b.journal = "Gene Expr Patterns"
+and (s.northern = 1
+     or s.western = 1
+     or s.rt_pcr = 1
+     or s.rnase = 1
+     or s.insitu_protein_section = 1
+     or s.insitu_rna_section = 1
+     or s.insitu_protein_mount = 1 
+     or s.insitu_rna_mount = 1
+     )
+and not exists (select 1 from GXD_Assay a where i._Refs_key = a._Refs_key)
+go
+
+select distinct i._Refs_key
+into #todelete
+from #refs r, GXD_Index i, GXD_Index_Stages s
+where r._Refs_key = i._Refs_key
+and i.index_id = s.index_id
+and s.stage_id = 40
 and s.northern = 1
-and s.stage_id != 40
-and not exists (select 1 from GXD_Assay a where i._Refs_key = a._Refs_key)
 union
 select distinct i._Refs_key
-from GXD_Index i, GXD_Index_Stages s, BIB_Refs b
-where i._Refs_key = b._Refs_key
-and b.journal = "Gene Expr Patterns"
+from #refs r, GXD_Index i, GXD_Index_Stages s
+where r._Refs_key = i._Refs_key
+and i.index_id = s.index_id
+and s.stage_id = 40
 and s.western = 1
-and s.stage_id != 40
-and not exists (select 1 from GXD_Assay a where i._Refs_key = a._Refs_key)
 union
 select distinct i._Refs_key
-from GXD_Index i, GXD_Index_Stages s, BIB_Refs b
-where i._Refs_key = b._Refs_key
-and b.journal = "Gene Expr Patterns"
+from #refs r, GXD_Index i, GXD_Index_Stages s
+where r._Refs_key = i._Refs_key
+and i.index_id = s.index_id
+and s.stage_id = 40
 and s.rt_pcr = 1
-and s.stage_id != 40
-and not exists (select 1 from GXD_Assay a where i._Refs_key = a._Refs_key)
 union
 select distinct i._Refs_key
-from GXD_Index i, GXD_Index_Stages s, BIB_Refs b
-where i._Refs_key = b._Refs_key
-and b.journal = "Gene Expr Patterns"
+from #refs r, GXD_Index i, GXD_Index_Stages s
+where r._Refs_key = i._Refs_key
+and i.index_id = s.index_id
+and s.stage_id = 40
 and s.rnase = 1
-and s.stage_id != 40
-and not exists (select 1 from GXD_Assay a where i._Refs_key = a._Refs_key)
 union
 select distinct i._Refs_key
-from GXD_Index i, GXD_Index_Stages s, BIB_Refs b
-where i._Refs_key = b._Refs_key
-and b.journal = "Gene Expr Patterns"
-and s.nuclease = 1
-and s.stage_id != 40
-and not exists (select 1 from GXD_Assay a where i._Refs_key = a._Refs_key)
+from #refs r, GXD_Index i, GXD_Index_Stages s
+where r._Refs_key = i._Refs_key
+and i.index_id = s.index_id
+and s.stage_id in (40, 41)
+and s.insitu_protein_section = 1
 union
 select distinct i._Refs_key
-from GXD_Index i, GXD_Index_Stages s, BIB_Refs b
-where i._Refs_key = b._Refs_key
-and b.journal = "Gene Expr Patterns"
-and s.primer_extension = 1
-and s.stage_id != 40
-and not exists (select 1 from GXD_Assay a where i._Refs_key = a._Refs_key)
+from #refs r, GXD_Index i, GXD_Index_Stages s
+where r._Refs_key = i._Refs_key
+and i.index_id = s.index_id
+and s.stage_id in (40, 41)
+and s.insitu_rna_section = 1
 union
 select distinct i._Refs_key
-from GXD_Index i, GXD_Index_Stages s, BIB_Refs b
-where i._Refs_key = b._Refs_key
-and b.journal = "Gene Expr Patterns"
-and s.clones = 1
-and not exists (select 1 from GXD_Assay a where i._Refs_key = a._Refs_key)
+from #refs r, GXD_Index i, GXD_Index_Stages s
+where r._Refs_key = i._Refs_key
+and i.index_id = s.index_id
+and s.stage_id in (40, 41)
+and s.insitu_protein_mount = 1 
 union
 select distinct i._Refs_key
-from GXD_Index i, GXD_Index_Stages s, BIB_Refs b
-where i._Refs_key = b._Refs_key
-and b.journal = "Gene Expr Patterns"
-and insitu_protein_section = 1
-and s.stage_id not in (40, 41)
-and not exists (select 1 from GXD_Assay a where i._Refs_key = a._Refs_key)
-union
-select distinct i._Refs_key
-from GXD_Index i, GXD_Index_Stages s, BIB_Refs b
-where i._Refs_key = b._Refs_key
-and b.journal = "Gene Expr Patterns"
-and insitu_rna_section = 1
-and s.stage_id not in (40, 41)
-and not exists (select 1 from GXD_Assay a where i._Refs_key = a._Refs_key)
-union
-select distinct i._Refs_key
-from GXD_Index i, GXD_Index_Stages s, BIB_Refs b
-where i._Refs_key = b._Refs_key
-and b.journal = "Gene Expr Patterns"
-and insitu_protein_mount = 1 
-and s.stage_id not in (40, 41)
-and not exists (select 1 from GXD_Assay a where i._Refs_key = a._Refs_key)
-union
-select distinct i._Refs_key
-from GXD_Index i, GXD_Index_Stages s, BIB_Refs b
-where i._Refs_key = b._Refs_key
-and b.journal = "Gene Expr Patterns"
-and insitu_rna_mount = 1
-and s.stage_id not in (40, 41)
-and not exists (select 1 from GXD_Assay a where i._Refs_key = a._Refs_key)
+from #refs r, GXD_Index i, GXD_Index_Stages s
+where r._Refs_key = i._Refs_key
+and i.index_id = s.index_id
+and s.stage_id in (40, 41)
+and s.insitu_rna_mount = 1
+go
+
+delete #refs
+from #refs r, #todelete d
+where d._Refs_key = r._Refs_key
 go
 
 select distinct r._Refs_key, i._Marker_key
