306fb0c42d6ffeea6fdc6cd15aea53111aee3a3c
Lori Corbani <Lori.Corbani@jax.org>
2003-04-07 13:16:05 +0000

    TR 4690

--- mgd/GXD_GeneExprPatterns.sql
+++ mgd/GXD_GeneExprPatterns.sql
@@ -1,48 +1,93 @@
 set nocount on
 go
 
-/* get all references, except only clones = 1 */
-
-select distinct i._Refs_key, type = 'blot'
+select distinct i._Refs_key
 into #refs
 from GXD_Index i, GXD_Index_Stages s, BIB_Refs b
 where i._Refs_key = b._Refs_key
 and b.journal = "Gene Expr Patterns"
-and (northern = 1
-     or western = 1
-     or rt_pcr = 1
-     or rnase = 1 
-     or nuclease = 1
-     or primer_extension =1
-)
+and s.northern = 1
+and s.stage_id != 40
 and not exists (select 1 from GXD_Assay a where i._Refs_key = a._Refs_key)
 union
-select distinct i._Refs_key, type = 'insitu'
+select distinct i._Refs_key
 from GXD_Index i, GXD_Index_Stages s, BIB_Refs b
 where i._Refs_key = b._Refs_key
 and b.journal = "Gene Expr Patterns"
-and (insitu_protein_section = 1
-     or insitu_rna_section = 1
-     or insitu_protein_mount = 1 
-     or insitu_rna_mount = 1
-)
+and s.western = 1
+and s.stage_id != 40
 and not exists (select 1 from GXD_Assay a where i._Refs_key = a._Refs_key)
-go
-
-delete #refs
-from #refs r, GXD_Index i, GXD_Index_Stages s
-where r._Refs_key = i._Refs_key
-and r.type = 'blot'
-and i.index_id = s.index_id
-and s.stage_id = 40
-go
-
-delete #refs
-from #refs r, GXD_Index i, GXD_Index_Stages s
-where r._Refs_key = i._Refs_key
-and r.type = 'insitu'
-and i.index_id = s.index_id
-and s.stage_id in (40,41)
+union
+select distinct i._Refs_key
+from GXD_Index i, GXD_Index_Stages s, BIB_Refs b
+where i._Refs_key = b._Refs_key
+and b.journal = "Gene Expr Patterns"
+and s.rt_pcr = 1
+and s.stage_id != 40
+and not exists (select 1 from GXD_Assay a where i._Refs_key = a._Refs_key)
+union
+select distinct i._Refs_key
+from GXD_Index i, GXD_Index_Stages s, BIB_Refs b
+where i._Refs_key = b._Refs_key
+and b.journal = "Gene Expr Patterns"
+and s.rnase = 1
+and s.stage_id != 40
+and not exists (select 1 from GXD_Assay a where i._Refs_key = a._Refs_key)
+union
+select distinct i._Refs_key
+from GXD_Index i, GXD_Index_Stages s, BIB_Refs b
+where i._Refs_key = b._Refs_key
+and b.journal = "Gene Expr Patterns"
+and s.nuclease = 1
+and s.stage_id != 40
+and not exists (select 1 from GXD_Assay a where i._Refs_key = a._Refs_key)
+union
+select distinct i._Refs_key
+from GXD_Index i, GXD_Index_Stages s, BIB_Refs b
+where i._Refs_key = b._Refs_key
+and b.journal = "Gene Expr Patterns"
+and s.primer_extension = 1
+and s.stage_id != 40
+and not exists (select 1 from GXD_Assay a where i._Refs_key = a._Refs_key)
+union
+select distinct i._Refs_key
+from GXD_Index i, GXD_Index_Stages s, BIB_Refs b
+where i._Refs_key = b._Refs_key
+and b.journal = "Gene Expr Patterns"
+and s.clones = 1
+and not exists (select 1 from GXD_Assay a where i._Refs_key = a._Refs_key)
+union
+select distinct i._Refs_key
+from GXD_Index i, GXD_Index_Stages s, BIB_Refs b
+where i._Refs_key = b._Refs_key
+and b.journal = "Gene Expr Patterns"
+and insitu_protein_section = 1
+and s.stage_id not in (40, 41)
+and not exists (select 1 from GXD_Assay a where i._Refs_key = a._Refs_key)
+union
+select distinct i._Refs_key
+from GXD_Index i, GXD_Index_Stages s, BIB_Refs b
+where i._Refs_key = b._Refs_key
+and b.journal = "Gene Expr Patterns"
+and insitu_rna_section = 1
+and s.stage_id not in (40, 41)
+and not exists (select 1 from GXD_Assay a where i._Refs_key = a._Refs_key)
+union
+select distinct i._Refs_key
+from GXD_Index i, GXD_Index_Stages s, BIB_Refs b
+where i._Refs_key = b._Refs_key
+and b.journal = "Gene Expr Patterns"
+and insitu_protein_mount = 1 
+and s.stage_id not in (40, 41)
+and not exists (select 1 from GXD_Assay a where i._Refs_key = a._Refs_key)
+union
+select distinct i._Refs_key
+from GXD_Index i, GXD_Index_Stages s, BIB_Refs b
+where i._Refs_key = b._Refs_key
+and b.journal = "Gene Expr Patterns"
+and insitu_rna_mount = 1
+and s.stage_id not in (40, 41)
+and not exists (select 1 from GXD_Assay a where i._Refs_key = a._Refs_key)
 go
 
 select distinct r._Refs_key, i._Marker_key
