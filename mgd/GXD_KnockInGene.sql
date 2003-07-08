4e86061b34c85a4ced759c6998bc45d974fdf52a
Lori Corbani <Lori.Corbani@jax.org>
2003-07-08 19:29:17 +0000

    MGI 2.97

---
+++ mgd/GXD_KnockInGene.sql
@@ -0,0 +1,28 @@
+set nocount on
+go
+
+select a._Assay_key, a._Refs_key, s.specimenLabel
+into #knockin
+from GXD_Assay a, GXD_Specimen s
+where a._Assay_key = 9
+and a._Assay_key = s._Assay_key
+and not exists (select 1 from GXD_AlleleGenotype g
+where s._Genotype_key = g._Genotype_key
+and a._Marker_key = g._Marker_key)
+go
+
+set nocount off
+go
+
+print ""
+print "GXD Knock Ins where the assayed Gene is not the Gene which is mutated in the Genotype"
+print ""
+
+select a.accID "Assay", b.accID "J:", k.specimenLabel
+from #knockin k, GXD_Assay_Acc_View a, BIB_Acc_View b
+where k._Assay_key = a._Object_key
+and k._Refs_key = b._Object_key
+and b._LogicalDB_key = 1
+and b.prefixPart = "J:"
+go
+
