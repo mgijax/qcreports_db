bd7e0e5d7cf798ede60c4610b8ad8b5fcc929ce9
Lori Corbani <Lori.Corbani@jax.org>
2003-07-29 17:22:45 +0000

    removed Fantom

--- qcnightly_reports.sh
+++ qcnightly_reports.sh
@@ -39,19 +39,9 @@ foreach i (*.py)
 $i
 end
 
-cd $QCFANTOM
-foreach i (*.py)
-$i
-end
-
 cd $QCREPORTOUTPUTDIR
 foreach i (NOMEN_Reserved.rpt NOMEN_Pending.rpt)
 rcp $i $HUGOWEBDIR
 rcp $i $HUGOFTPDIR
-end
-
-foreach i (fantom2.mgi)
-rcp $i $RIKENFTPDIR
-rm -rf $i
 end
 
