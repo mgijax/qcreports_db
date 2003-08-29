914bfd756f40f2c321262de18d585e08d10b3cb5
Lori Corbani <Lori.Corbani@jax.org>
2003-08-29 11:41:32 +0000

    Bob Sinclair

---
+++ mgd/ACC_GenBank.sql
@@ -0,0 +1,19 @@
+print ""
+print "All GenBank IDs in MGI"
+print "(excludes GenBank IDs attached to non-Mouse Markers)"
+print ""
+
+select distinct a.accID
+from ACC_Accession a
+where a._LogicalDB_key = 9
+and a._MGIType_key = 3
+union
+select distinct a.accID
+from ACC_Accession a, MRK_Marker m
+where a._LogicalDB_key = 9
+and a._MGIType_key = 2
+and a._Object_key = m._Marker_key
+and m._Species_key = 1
+order by a.accID
+go
+
