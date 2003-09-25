4e168f42f9bd79fc163b2cf39249695c081c8cab
Lori Corbani <Lori.Corbani@jax.org>
2003-09-25 19:04:25 +0000

    TR 5112

--- mgd/BIB_NeedNLM.sql
+++ mgd/BIB_NeedNLM.sql
@@ -1,26 +1,17 @@
-set nocount on
-go
+print ""
+print "References Which Need NLM Updates"
+print "in Medline query format:"
+print ""
+print "'first author' [AU] AND 'year' [YR] AND 'journal name' [TA]"
+print ""
 
-select _Refs_key
-into #nlm
+select "'" + r._primary + "' [AU] AND '"  + convert(char(4), r.year) + "' [YR] AND '" + r.journal + "' [TA]"
 from BIB_Refs r
 where NLMstatus = 'Y'
 and not exists
 (select a._Accession_key from BIB_Acc_View a
 where a._Object_key = r._Refs_key
-and a._LogicalDB_key in (7,29))
-go
-
-set nocount off
-go
-
-print ""
-print "References Which Need NLM Updates"
-print ""
-
-select b.jnumID, substring(b.short_citation, 1, 75)
-from BIB_All_View b, #nlm n
-where n._Refs_key = b._Refs_key
-order by year, _primary
+and a._LogicalDB_key = 29)
+order by r.year, r._primary
 go
 
