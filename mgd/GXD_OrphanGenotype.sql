fc17b33b682e95e978f99572ea3b9c5af86e81e0
Lori Corbani <Lori.Corbani@jax.org>
2003-11-12 14:20:17 +0000

    new

---
+++ mgd/GXD_OrphanGenotype.sql
@@ -0,0 +1,13 @@
+print ""
+print "Orphan Genotypes (can be deleted)"
+print ""
+
+select g._Genotype_key, strain = substring(g.strain, 1, 65)
+from GXD_Genotype_View g
+where not exists (select 1 from GXD_AlleleGenotype a where g._Genotype_key = a._Genotype_key)
+and not exists (select 1 from GXD_Expression a where g._Genotype_key = a._Genotype_key)
+and not exists (select 1 from GXD_GelLane a where g._Genotype_key = a._Genotype_key)
+and not exists (select 1 from GXD_Specimen a where g._Genotype_key = a._Genotype_key)
+and not exists (select 1 from VOC_Annot a where g._Genotype_key = a._Object_key and a._AnnotType_key in (1001,10
+order by g.strain
+go
