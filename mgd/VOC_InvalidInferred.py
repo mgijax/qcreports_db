46e0cf4355bb39d20feeea2eba2cde117a518cae
Lori Corbani <Lori.Corbani@jax.org>
2003-11-14 16:38:02 +0000

    TR 5311

--- mgd/VOC_InvalidInferred.py
+++ mgd/VOC_InvalidInferred.py
@@ -39,6 +39,8 @@ import reportlib
 #
 
 fp = reportlib.init(sys.argv[0], outputdir = os.environ['QCREPORTOUTPUTDIR'], printHeading = 0)
+fp.write('Invalid "Inferred From" Values in Annotations (MGI and InterPro only)' + 2 * reportlib.CRT)
+rows = 0
 
 results = db.sql('select distinct inferredFrom from VOC_Evidence where inferredFrom != null', 'auto')
 for r in results:
@@ -67,12 +69,15 @@ for r in results:
 	    idResult = db.sql('select _Object_key from ACC_Accession where accID = "%s"' % (id), 'auto')
 	    if len(idResult) == 0:
 		fp.write(id + reportlib.CRT)
+		rows = rows + 1
 
 	if string.find(id, 'INTERPRO:') >= 0:
 	    [prefixPart, idPart] = string.split(id, 'INTERPRO:')
 	    idResult = db.sql('select _Object_key from ACC_Accession where accID = "%s"' % (idPart), 'auto')
 	    if len(idResult) == 0:
 		fp.write(id + reportlib.CRT)
+		rows = rows + 1
 
+fp.write('\n(%d rows affected)\n' % (rows))
 reportlib.finish_nonps(fp)
 
