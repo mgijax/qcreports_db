57663d4cc10aa9e6be90095d2a6c3a3cf805450b
Lori Corbani <Lori.Corbani@jax.org>
2004-02-12 15:23:58 +0000

    add invalid acc id checks

--- mgd/ACC_Accession.sql
+++ mgd/ACC_Accession.sql
@@ -194,3 +194,22 @@ from ACC_Accession
 where _MGIType_key = 9 and _LogicalDB_Key = 1 and preferred = 1
 group by accID having count(*) > 1
 go
+
+print ""
+print "Invalid LocusLink IDs"
+print ""
+
+select _Accession_key, accID, prefixPart, numericPart, creation_date
+from ACC_Accession
+where _LogicalDB_key = 24 and prefixPart is not null
+go
+
+print ""
+print "Invalid RefSeq IDs"
+print ""
+
+select _Accession_key, accID, prefixPart, numericPart, creation_date
+from ACC_Accession
+where _LogicalDB_key = 27 and prefixPart not in ('NM_', 'NP_', 'XM_', 'XP_')
+go
+
