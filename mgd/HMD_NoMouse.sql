3c7ce07b166cbd9d0431ad9e20272026480691d8
Lori Corbani <Lori.Corbani@jax.org>
2004-03-05 13:19:09 +0000

    JSAM

--- mgd/HMD_NoMouse.sql
+++ mgd/HMD_NoMouse.sql
@@ -27,13 +27,13 @@ print "Homology Records w/out Mouse Genes"
 print "(The mouse symbol is listed if it is the same as one of the other organism)"
 print ""
 
-select distinct h._Class_key, h.symbol, m.symbol "mouse symbol", organism = h.commonName, h.jnumID
+select distinct h._Class_key, h.symbol, m.symbol "mouse symbol", h.organism, h.jnumID
 from #homology h, MRK_Marker m
 where h.symbol not like '*%'
 and h.symbol *= m.symbol
 and m._Organism_key = 1
 union
-select distinct h._Class_key, h.symbol, m.symbol, organism = h.commonName, h.jnumID
+select distinct h._Class_key, h.symbol, m.symbol, h.organism, h.jnumID
 from #homology h, MRK_Marker m
 where h.symbol like '*%'
 and substring(h.symbol, 2, char_length(h.symbol) - 2) *= m.symbol
