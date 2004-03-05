d192b70079def07009d1aab1e074191ed18ac9b3
Lori Corbani <Lori.Corbani@jax.org>
2004-03-05 19:42:03 +0000

    TR 5636

---
+++ mgd/GXD_DevImages.py
@@ -0,0 +1,74 @@
+#!/usr/local/bin/python
+
+'''
+#
+# GXD_DevImages.py
+#
+# Report:
+#       Produce a report of all J numbers from the journal Development
+#       that are included in the GXD database that do not have images
+#       attached to them.
+#
+# Usage:
+#       GXD_DevImages.py
+#
+# Notes:
+#
+# History:
+#
+# lec	03/05/2004
+#	- converted to QC (TR 5636)
+#
+# dbm   12/4/2002
+#       - created (TR 4296)
+#
+'''
+ 
+import sys
+import os
+import db
+import reportlib
+
+CRT = reportlib.CRT
+SPACE = reportlib.SPACE
+TAB = reportlib.TAB
+PAGE = reportlib.PAGE
+
+#
+# Main
+#
+
+fp = reportlib.init(sys.argv[0], '"Development" Papers Requiring Images', outputdir = os.environ['QCREPORTOUTPUTDIR'])
+
+cmd = 'select b.jnumID ' + \
+      'from GXD_Assay a, BIB_All_View b, ACC_Accession ac, ' + \
+           'IMG_Image i, IMG_ImagePane p ' + \
+      'where a._ImagePane_key = p._ImagePane_key and ' + \
+            'p._Image_key = i._Image_key and ' + \
+            'i.xDim is NULL and ' + \
+            'a._Refs_key = b._Refs_key and ' + \
+            'b.journal = "Development" and ' + \
+            'a._AssayType_key not in (1, 5, 7) and ' + \
+            'a._Assay_key = ac._Object_key and ' + \
+            'ac._MGIType_key = 8 ' + \
+      'union ' + \
+      'select b.jnumID ' + \
+      'from GXD_Assay a, BIB_All_View b, ACC_Accession ac, ' + \
+           'GXD_Specimen g, GXD_ISResultImage_View r ' + \
+      'where g._Assay_key = a._Assay_key and ' + \
+            'g._Specimen_key = r._Specimen_key and ' + \
+            'r.xDim is NULL and ' + \
+            'a._Refs_key = b._Refs_key and ' + \
+            'b.journal = "Development" and ' + \
+            'a._Assay_key = ac._Object_key and ' + \
+            'ac._MGIType_key = 8 ' + \
+      'order by b.jnumID'
+
+results = db.sql(cmd, 'auto')
+
+for r in results:
+	fp.write(r['jnumID'] + CRT)
+
+fp.write(CRT + 'Total J numbers: ' + str(len(results)) + CRT)
+
+reportlib.finish_nonps(fp)
