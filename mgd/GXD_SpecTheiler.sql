ae3d1880e3b9854e1818e17771f2fca177272284
Lori Corbani <Lori.Corbani@jax.org>
2004-04-27 16:32:17 +0000

    TR 5762

--- mgd/GXD_SpecTheiler.sql
+++ mgd/GXD_SpecTheiler.sql
@@ -9,8 +9,7 @@ where i._Result_key = r._Result_key
 and r._Structure_key = s._Structure_key
 and s._Stage_key = t._Stage_key
 and s._StructureName_key = sn._StructureName_key
-and not (t.stage = 28 and (sn.structure = "placenta" or sn.structure = "decidua"
-or sn.structure = "uterus"))
+and not (t.stage = 28 and (sn.structure = "placenta" or sn.structure = "decidua"))
 go
 
 select distinct _Specimen_key 
@@ -30,8 +29,7 @@ and i._GelLane_key = r._GelLane_key
 and r._Structure_key = s._Structure_key
 and s._Stage_key = t._Stage_key
 and s._StructureName_key = sn._StructureName_key
-and not (t.stage = 28 and (sn.structure = "placenta" or sn.structure = "decidua"
-or sn.structure = "uterus"))
+and not (t.stage = 28 and (sn.structure = "placenta" or sn.structure = "decidua"))
 go
 
 select distinct _GelLane_key 
@@ -46,7 +44,7 @@ go
 
 print ""
 print "InSitu Specimens annotated to structures of > 1 Theiler Stage"
-print "(excludes TS28:placenta and TS28:decidua and TS28:uterus)"
+print "(excludes TS28:placenta, TS28:decidua)"
 print ""
 
 select a.mgiID, a.jnumID, specimenLabel = substring(s.specimenLabel, 1, 50)
@@ -57,7 +55,7 @@ go
 
 print ""
 print "Gel Lane Specimens annotated to structures of > 1 Theiler Stage"
-print "(excludes TS28:placenta and TS28:decidua and TS28:uterus)"
+print "(excludes TS28:placenta, TS28:decidua)"
 print ""
 
 select a.mgiID, a.jnumID, laneLabel = substring(s.laneLabel, 1, 50)
