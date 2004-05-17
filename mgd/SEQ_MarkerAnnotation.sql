a931f0090dcad09d0f5b453919785245f53efcbb
Lori Corbani <Lori.Corbani@jax.org>
2004-05-17 15:51:35 +0000

    JSAM

--- mgd/SEQ_MarkerAnnotation.sql
+++ mgd/SEQ_MarkerAnnotation.sql
@@ -2,8 +2,10 @@
 set nocount on
 go
 
-select ma._Object_key, ma.accID
-into #markers
+/* select all where the a secondary id is annotated */
+
+select ma._Object_key, ma.accID, sequenceKey = sa._Object_key
+into #markers1
 from ACC_Accession ma, ACC_Accession sa
 where ma._MGIType_key = 2
 and ma.accID = sa.accID
@@ -12,7 +14,27 @@ and ma._LogicalDB_key = sa._LogicalDB_key
 and sa.preferred = 0
 go
 
-create nonclustered index idx_key on #markers(_Object_key)
+create nonclustered index idx_mkey on #markers1(_Object_key)
+go
+
+create nonclustered index idx_skey on #markers1(sequenceKey)
+go
+
+/* select all where the primary is not annotated */
+
+select m.*
+into #markers
+from #markers1 m
+where not exists (select 1 from ACC_Accession ma, ACC_Accession sa
+where m._Object_key = ma._Object_key
+and ma._MGIType_key = 2
+and m.sequenceKey = sa._Object_key
+and sa._MGIType_key = 19
+and ma.accID = sa.accID
+and sa.preferred = 1)
+go
+
+create nonclustered index idx_mkey on #markers(_Object_key)
 go
 
 set nocount off
