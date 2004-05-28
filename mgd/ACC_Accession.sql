f64571c271191b999d1bcc3292afcb9c230c2e9c
Lori Corbani <Lori.Corbani@jax.org>
2004-05-28 16:57:48 +0000

    JSAM

--- mgd/ACC_Accession.sql
+++ mgd/ACC_Accession.sql
@@ -219,6 +219,6 @@ print ""
 
 select _Accession_key, accID, prefixPart, numericPart, creation_date
 from ACC_Accession
-where _LogicalDB_key = 27 and prefixPart not in ('NM_', 'NP_', 'XM_', 'XP_')
+where _LogicalDB_key = 27 and prefixPart not in ('NM_', 'NP_', 'NR_', 'XM_', 'XP_', 'XR_')
 go
 
