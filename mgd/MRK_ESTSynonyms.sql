841321efe8218e21a3abcf3fdc5d29a063d35f30
Lori Corbani <Lori.Corbani@jax.org>
2004-06-15 17:20:47 +0000

    TR 5923

--- mgd/MRK_ESTSynonyms.sql
+++ mgd/MRK_ESTSynonyms.sql
@@ -1,12 +1,13 @@
 
 print ""
-print "EST IDs as Synonyms"
+print "EST IDs and Gm Markers as Synonyms"
 print ""
 
 select m.symbol, o.name "synonym"
 from MRK_Marker m, MRK_Other o
 where m._Marker_key = o._Marker_key
-and o.name like "[A-Z][A-Z][0-9][0-9][0-9][0-9][0-9][0-9]"
+and (o.name like "[A-Z][A-Z][0-9][0-9][0-9][0-9][0-9][0-9]"
+or o.name like "Gm%")
 order by m.symbol
 go
 
