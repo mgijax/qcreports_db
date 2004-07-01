e6b67324dd7812d4b0af3d9603a846e15d11ee3c
Lori Corbani <Lori.Corbani@jax.org>
2004-07-01 15:48:57 +0000

    TR 5998

--- mgd/NOMEN_Pending.sql
+++ mgd/NOMEN_Pending.sql
@@ -2,10 +2,12 @@ print ""
 print "Nomenclature In Progress Symbols"
 print ""
 
-select n.symbol, n.chromosome, n.createdBy, r.jnumID
-from NOM_Marker_View n, MGI_Reference_Nomen_View r
-where n.status = "In Progress"
+select n.symbol, n.chromosome, u.login, r.jnumID
+from NOM_Marker n, VOC_Term t, MGI_User u, MGI_Reference_Nomen_View r
+where n._NomenStatus_key = t._Term_key
+and t.term = "In Progress"
+and n._CreatedBy_key = u._User_key
 and n._Nomen_key = r._Object_key
 and r.assocType = "Primary"
-order by n.createdBy, n.symbol
+order by u.login, n.symbol
 go
