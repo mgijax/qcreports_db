83121b287062cbc1bd3ede0e68500d71d93dea56
Lori Corbani <Lori.Corbani@jax.org>
2005-01-20 14:00:53 +0000

    missing some files

---
+++ mgd/GXD_FullCode.py
@@ -0,0 +1,137 @@
+#!/usr/local/bin/python
+
+'''
+#
+# TR 6469
+#
+# Report:
+#
+# Usage:
+#       GXD_FullCode.py
+#
+# Notes:
+#
+# History:
+#
+# lec	01/12/2005
+#	- converted GXD_DevBiol.sql, GXD_Development.sql, GXD_MechDev.sql, GXD_GeneExprPatterns.sql to one report
+#	- added Dev Dyn
+#
+'''
+
+import sys
+import os
+import string
+import regsub
+import db
+import reportlib
+
+CRT = reportlib.CRT
+SPACE = reportlib.SPACE
+TAB = reportlib.TAB
+PAGE = reportlib.PAGE
+
+journals = ['Dev Biol', 'Dev Dyn', 'Development', 'Mech Dev', 'Gene Expr Patterns']
+
+#
+# Main
+#
+
+for j in journals:
+
+    fileName = 'GXD_' + regsub.gsub(' ', '', j)
+    fp = reportlib.init(fileName, outputdir = os.environ['QCOUTPUTDIR'])
+
+    fp.write('Reference and Number of Markers Analyzed for Journal: %s' % (j) + 2*CRT)
+    fp.write(string.ljust('accID', 30))
+    fp.write(SPACE)
+    fp.write(string.ljust('markerCount', 12))
+    fp.write(SPACE)
+    fp.write(CRT)
+    fp.write(string.ljust('-----', 30))
+    fp.write(SPACE)
+    fp.write(string.ljust('-----------', 12))
+    fp.write(SPACE)
+    fp.write(CRT)
+
+    # get set of references not coded with high priority
+
+    db.sql('select distinct i._Refs_key, i._Marker_key ' + \
+           'into #markers1 ' + \
+           'from GXD_Index i, BIB_Refs b, VOC_Term_GXDIndexPriority_View a ' + \
+           'where i._Refs_key = b._Refs_key ' + \
+           'and b.journal = "%s" ' % (j) + \
+           'and i._Priority_key =  a._Term_key ' + \
+           'and a.term = "High" ' + \
+           'and not exists (select 1 from GXD_Assay a where i._Refs_key = a._Refs_key)', None)
+
+    db.sql('create index idx1 on #markers1(_Refs_key)', None)
+    db.sql('select m.*, markerCount = count(*) into #mcount1 from #markers1 m group by _Refs_key', None)
+    db.sql('create index idx1 on #mcount1(_Refs_key)', None)
+
+    results = db.sql('select distinct a.accID, i.markerCount ' + \
+                     'from #mcount1 i, ACC_Accession a ' + \
+                     'where i._Refs_key = a._Object_key ' + \
+                     'and a._MGIType_key = 1 ' + \
+                     'and a.prefixPart = "J:" ' + \
+                     'order by markerCount desc', 'auto')
+
+    for r in results:
+	fp.write(string.ljust(r['accID'], 30))
+	fp.write(SPACE)
+	fp.write(string.ljust(str(r['markerCount']), 12))
+	fp.write(SPACE)
+	fp.write(CRT)
+    fp.write('\n(%d rows affected)\n' % (len(results)))
+
+    #
+    # new genes
+    #
+
+    fp.write(CRT)
+    fp.write('%s:  Papers with New Genes' % (j) + 2*CRT)
+    fp.write(string.ljust('accID', 30))
+    fp.write(SPACE)
+    fp.write(string.ljust('markerCount', 12))
+    fp.write(SPACE)
+    fp.write(CRT)
+    fp.write(string.ljust('-----', 30))
+    fp.write(SPACE)
+    fp.write(string.ljust('-----------', 12))
+    fp.write(SPACE)
+    fp.write(CRT)
+
+    # get set of references/markers not coded with high priority
+
+    db.sql('select distinct i._Refs_key, i._Marker_key ' + \
+           'into #markers2 ' + \
+           'from GXD_Index i, BIB_Refs b, VOC_Term_GXDIndexPriority_View a ' + \
+           'where i._Refs_key = b._Refs_key ' + \
+           'and b.journal = "%s" ' % (j) + \
+           'and i._Priority_key =  a._Term_key ' + \
+           'and a.term = "High" ' + \
+           'and not exists (select 1 from GXD_Assay a where i._Refs_key = a._Refs_key) ' + \
+           'and not exists (select 1 from GXD_Assay a where i._Marker_key = a._Marker_key) ', None)
+
+    db.sql('create index idx1 on #markers2(_Refs_key)', None)
+    db.sql('select m.*, markerCount = count(*) into #mcount2 from #markers2 m group by _Refs_key', None)
+    db.sql('create index idx1 on #mcount2(_Refs_key)', None)
+
+    results = db.sql('select distinct a.accID, i.markerCount ' + \
+                     'from #mcount2 i, ACC_Accession a ' + \
+                     'where i._Refs_key = a._Object_key ' + \
+                     'and a._MGIType_key = 1 ' + \
+                     'and a.prefixPart = "J:" ' + \
+                     'order by markerCount desc', 'auto')
+
+    for r in results:
+	fp.write(string.ljust(r['accID'], 30))
+	fp.write(SPACE)
+	fp.write(string.ljust(str(r['markerCount']), 12))
+	fp.write(SPACE)
+	fp.write(CRT)
+    fp.write('\n(%d rows affected)\n' % (len(results)))
+
+    reportlib.trailer(fp)
+    reportlib.finish_nonps(fp)
+
---
+++ mgd/GXD_LackingImages.py
@@ -0,0 +1,65 @@
+#!/usr/local/bin/python
+
+'''
+#
+# GXD_LackingImages.py
+#
+# Report:
+#       Produce a report of all J numbers from the journals specified
+#	that are full coded in GXD database but do not have images
+#       attached to them.
+#
+# Usage:
+#       GXD_LackingImages.py
+#
+# Notes:
+#
+# History:
+#
+# lec	01/13/2005
+#	- TR 6480
+#
+'''
+ 
+import sys
+import os
+import string
+import db
+import reportlib
+
+CRT = reportlib.CRT
+SPACE = reportlib.SPACE
+TAB = reportlib.TAB
+PAGE = reportlib.PAGE
+
+journals = ['Dev Biol', 'Cell Death Differ', 'Oncogene', 'Nature', 'Nat Cell Biol', 'Nat Genet', 'Nat Immunol', 'Nat Med', 'Nat Neurosci', 'Nat Struct Biol', 'Nat Biotechnol', 'Biotechnology', 'Nat Rev Cancer', 'Nat Rev Genet', 'Nat Rev Immunol', 'Nat Rev Mol Cell Bio', 'Nat Rev Neurosci']
+
+#
+# Main
+#
+
+fp = reportlib.init(sys.argv[0], 'Fully Coded Papers Lacking Images', outputdir = os.environ['QCOUTPUTDIR'])
+
+fp.write(TAB + 'Journals Checked:' + CRT)
+for j in journals:
+    fp.write(2*TAB + j + CRT)
+fp.write(CRT)
+fp.write(TAB + 'J#' + TAB + 'short_citation' + CRT)
+fp.write(TAB + '--' + TAB + '--------------' + CRT)
+
+db.sql('select distinct r._Refs_key, r.journal into #refs ' + \
+	'from BIB_Refs r, GXD_Assay a ' + \
+	'where r.journal in ("' + string.join(journals, '","') + '") ' + \
+	'and r._Refs_key = a._Refs_key ' + \
+	'and exists (select 1 from IMG_Image a where r._Refs_key = a._Refs_key and xDim is null)', None)
+db.sql('create index idx1 on #refs(_Refs_key)', None)
+
+results = db.sql('select b.jnumID, b.short_citation from #refs r, BIB_All_View b ' + \
+	'where r._Refs_key = b._Refs_key order by r.journal, b.jnumID', 'auto')
+for r in results:
+	fp.write(TAB + r['jnumID'] + TAB + r['short_citation'] + CRT)
+
+fp.write(CRT + 'Total J numbers: ' + str(len(results)) + CRT)
+
+reportlib.trailer(fp)
+reportlib.finish_nonps(fp)
