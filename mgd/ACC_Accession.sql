f55cba9b58f5f84f0dba0afa14a3b6beca0941a5
Lori Corbani <Lori.Corbani@jax.org>
2005-02-15 14:32:09 +0000

    updated

--- mgd/ACC_Accession.sql
+++ mgd/ACC_Accession.sql
@@ -1,9 +1,16 @@
 print ""
-print "Invalid RefSeq IDs"
+print "Invalid RefSeq IDs Associated to Mouse Markers"
 print ""
 
-select _Accession_key, accID, prefixPart, numericPart, creation_date
-from ACC_Accession
-where _LogicalDB_key = 27 and prefixPart not in ('NM_', 'NP_', 'NR_', 'XM_', 'XP_', 'XR_')
+select a.accID, m.symbol, u.login
+from ACC_Accession a, ACC_AccessionReference r, MRK_Marker m, MGI_User u
+where a._LogicalDB_key = 27 
+and a.prefixPart not in ('XM_', 'XR_')
+and a._MGIType_key = 2
+and a._Object_key = m._Marker_key
+and m._Organism_key = 1
+and a._Accession_key = r._Accession_key
+and r._Refs_key not in (53672, 64047)
+and a._CreatedBy_key = u._User_key
 go
 
