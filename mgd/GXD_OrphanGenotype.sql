bcbbceead42372ecbec9486b734cac82fe287a3b
Lori Corbani <Lori.Corbani@jax.org>
2005-03-28 20:40:32 +0000

    fix

--- mgd/GXD_OrphanGenotype.sql
+++ mgd/GXD_OrphanGenotype.sql
@@ -2,12 +2,15 @@ print ""
 print "Orphan Genotypes (can be deleted)"
 print ""
 
-select g._Genotype_key, strain = substring(g.strain, 1, 65)
-from GXD_Genotype_View g
-where not exists (select 1 from GXD_AlleleGenotype a where g._Genotype_key = a._Genotype_key)
-and not exists (select 1 from GXD_Expression a where g._Genotype_key = a._Genotype_key)
+select a.accID, strain = substring(s.strain, 1, 65)
+from GXD_Genotype g, PRB_Strain s, ACC_Accession a
+where not exists (select 1 from GXD_Expression a where g._Genotype_key = a._Genotype_key)
 and not exists (select 1 from GXD_GelLane a where g._Genotype_key = a._Genotype_key)
 and not exists (select 1 from GXD_Specimen a where g._Genotype_key = a._Genotype_key)
 and not exists (select 1 from VOC_Annot a where g._Genotype_key = a._Object_key and a._AnnotType_key in (1001,1002))
-order by g.strain
+and not exists (select 1 from PRB_Strain_Genotype a where g._Genotype_key = a._Genotype_key)
+and g._Strain_key = s._Strain_key
+and g._Genotype_key = a._Object_key
+and a._MGIType_key = 10
+order by s.strain
 go
