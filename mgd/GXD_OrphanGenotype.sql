537022625ab7cbfade661b1073a42cd1b85c1102
Lori Corbani <Lori.Corbani@jax.org>
2005-03-28 20:33:29 +0000

    MPR

--- mgd/GXD_OrphanGenotype.sql
+++ mgd/GXD_OrphanGenotype.sql
@@ -4,8 +4,7 @@ print ""
 
 select g._Genotype_key, strain = substring(s.strain, 1, 65)
 from GXD_Genotype g, PRB_Strain s
-where not exists (select 1 from GXD_AlleleGenotype a where g._Genotype_key = a._Genotype_key)
-and not exists (select 1 from GXD_Expression a where g._Genotype_key = a._Genotype_key)
+where not exists (select 1 from GXD_Expression a where g._Genotype_key = a._Genotype_key)
 and not exists (select 1 from GXD_GelLane a where g._Genotype_key = a._Genotype_key)
 and not exists (select 1 from GXD_Specimen a where g._Genotype_key = a._Genotype_key)
 and not exists (select 1 from VOC_Annot a where g._Genotype_key = a._Object_key and a._AnnotType_key in (1001,1002))
