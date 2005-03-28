355025fb0d682bb0ac14bbba3521f999a0e74e8f
Lori Corbani <Lori.Corbani@jax.org>
2005-03-28 19:05:40 +0000

    MPR

--- mgd/GXD_OrphanGenotype.sql
+++ mgd/GXD_OrphanGenotype.sql
@@ -2,12 +2,13 @@ print ""
 print "Orphan Genotypes (can be deleted)"
 print ""
 
-select g._Genotype_key, strain = substring(g.strain, 1, 65)
-from GXD_Genotype_View g
+select g._Genotype_key, strain = substring(s.strain, 1, 65)
+from GXD_Genotype g, PRB_Strain s
 where not exists (select 1 from GXD_AlleleGenotype a where g._Genotype_key = a._Genotype_key)
 and not exists (select 1 from GXD_Expression a where g._Genotype_key = a._Genotype_key)
 and not exists (select 1 from GXD_GelLane a where g._Genotype_key = a._Genotype_key)
 and not exists (select 1 from GXD_Specimen a where g._Genotype_key = a._Genotype_key)
 and not exists (select 1 from VOC_Annot a where g._Genotype_key = a._Object_key and a._AnnotType_key in (1001,1002))
-order by g.strain
+and g._Strain_key = s._Strain_key
+order by s.strain
 go
