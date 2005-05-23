bfd93c3111decabce3bdeca3bce37db0a9fdbf9b
Lori Corbani <Lori.Corbani@jax.org>
2005-05-23 14:24:39 +0000

    add OMIM/Genotype

--- mgd/GXD_OrphanGenotype.sql
+++ mgd/GXD_OrphanGenotype.sql
@@ -7,7 +7,7 @@ from GXD_Genotype g, PRB_Strain s, ACC_Accession a
 where not exists (select 1 from GXD_Expression a where g._Genotype_key = a._Genotype_key)
 and not exists (select 1 from GXD_GelLane a where g._Genotype_key = a._Genotype_key)
 and not exists (select 1 from GXD_Specimen a where g._Genotype_key = a._Genotype_key)
-and not exists (select 1 from VOC_Annot a where g._Genotype_key = a._Object_key and a._AnnotType_key in (1001,1002))
+and not exists (select 1 from VOC_Annot a where g._Genotype_key = a._Object_key and a._AnnotType_key in (1001,1002, 1005))
 and not exists (select 1 from PRB_Strain_Genotype a where g._Genotype_key = a._Genotype_key)
 and g._Strain_key = s._Strain_key
 and g._Genotype_key = a._Object_key
