a31696cc6418d5bc5ae2ea5a10f1dfe4ae040590
mbw <>
2005-06-21 20:01:18 +0000

    initial version

---
+++ egload/dummy.py
@@ -0,0 +1,5 @@
+#!/usr/local/bin/python
+
+# This is a stub file which is a place holder for potential future qc reports. 
+# Currently there are no reports defined from this product for the egload. 
+# If one is added initially, then this dummy file can be removed
