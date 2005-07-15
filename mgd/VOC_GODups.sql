044e705f2077f4368886f29952ff280df473e659
Lori Corbani <Lori.Corbani@jax.org>
2005-07-15 19:54:28 +0000

    TR 6490/VOC_GODups.sql

---
+++ mgd/VOC_GODups.sql
@@ -0,0 +1,32 @@
+
+set nocount on
+go
+
+select a._Term_key, a._Object_key, a.isNot, e._EvidenceTerm_key, e._Refs_key
+into #a
+from VOC_Annot a, VOC_Evidence e
+where a._AnnotType_key = 1000
+and a._Annot_key = e._Annot_key
+go
+
+select a.*
+into #dup
+from #a a
+group by _Term_key, _Object_key, isNot, _EvidenceTerm_key, _Refs_key
+having count(*) > 1
+go
+
+select nocount off
+go
+
+print ""
+print "Duplicate GO Annotations"
+print ""
+
+select m.symbol, substring(t.term,1,50) 
+from #dup d, MRK_Marker m, VOC_Term t
+where d._Object_key = m._Marker_key
+and d._Term_key = t._Term_key
+order by m.symbol
+go
+
