972095ea0f5ddf06da4af705c2e77b3379bc4840
Lori Corbani <Lori.Corbani@jax.org>
2005-08-03 17:19:20 +0000

    added HUGOFASTANOMENFASTA

--- qcnightly_reports.sh
+++ qcnightly_reports.sh
@@ -26,21 +26,22 @@ if ( $i == "${QCMGD}/MRK_MarkerClip.sql" ) then
 	reportisql.csh $i ${QCOUTPUTDIR}/`basename $i`.${DATE}.rpt ${DSQUERY} ${MGD}
 	ln -s ${QCOUTPUTDIR}/`basename $i`.${DATE}.rpt ${QCOUTPUTDIR}/`basename $i`.current.rpt
 else
-	reportisql.csh $i $QCOUTPUTDIR/`basename $i`.rpt $DSQUERY $MGD
+	reportisql.csh $i ${QCOUTPUTDIR}/`basename $i`.rpt ${DSQUERY} ${MGD}
 endif
 echo $i, `date`
 end
 
-cd $QCMGD
+cd ${QCMGD}
 foreach i (*.py)
 echo $i, `date`
 $i >>& ${LOG}
 echo $i, `date`
 end
 
-cd $QCOUTPUTDIR
+cd ${QCOUTPUTDIR}
 foreach i (NOMEN_Reserved.rpt NOMEN_Pending.rpt HMD_SymbolDiffs2.rpt)
-rcp $i $HUGOWEBDIR
-rcp $i $HUGOFTPDIR
+rcp $i ${HUGOWEBDIR}
+rcp $i ${HUGOFTPDIR}
 end
 
+rcp ${HUGOFASTANOMENFASTA} ${HUGOWEBDIR}
