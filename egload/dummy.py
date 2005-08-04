7046acb91ca5fcc6bb131d7f04fd04bffa96337b
mbw <>
2005-08-04 13:42:07 +0000

    comments only

--- egload/dummy.py
+++ egload/dummy.py
@@ -2,4 +2,5 @@
 
 # This is a stub file which is a place holder for potential future qc reports. 
 # Currently there are no reports defined from this product for the egload. 
-# If one is added initially, then this dummy file can be removed
+# If one is added initially, then this dummy file can be removed.
+# To add a qc report just place a program in this directory with the suffix 'py'
