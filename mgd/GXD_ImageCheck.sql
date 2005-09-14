9d0bd86a74397005054134037fe73b0755d82441
Lori Corbani <Lori.Corbani@jax.org>
2005-09-14 14:57:03 +0000

    fix

--- mgd/GXD_ImageCheck.sql
+++ mgd/GXD_ImageCheck.sql
@@ -15,9 +15,12 @@ print "GXD Images with Copyright containing '*' or '(||)'"
 print ""
 
 select distinct i.jnumID + ";" + i.figureLabel
-from IMG_Image_View i
-where i.copyrightNote like "%*%"
-or i.copyrightNote like "%(||)%"
+from IMG_Image_View i, MGI_Note n, MGI_NoteChunk nc
+where i._Image_key = n._Object_key
+and n._MGIType_key = 12
+and n._NoteType_key = 1023
+and n._Note_key = nc._Note_key
+and (nc.note like "%*%" or nc.note like "%(||)%")
 order by i.jnum
 go
 
