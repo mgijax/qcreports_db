216df5993846ba7b4123d5b202001006776104e3
Lori Corbani <Lori.Corbani@jax.org>
2005-10-14 15:06:52 +0000

    qcreports_db-tr7062-BP

--- mgd/SEQ_Deleted.sql
+++ mgd/SEQ_Deleted.sql
@@ -11,7 +11,7 @@ go
 create index idx1 on #deleted1(_Sequence_key)
 go
 
-select a.accID, a._LogicalDB_key
+select a.accID, d._Sequence_key
 into #deleted2
 from #deleted1 d, ACC_Accession a
 where d._Sequence_key = a._Object_key
@@ -19,21 +19,19 @@ and a._MGIType_key = 19
 go
 
 create index idx1 on #deleted2(accID)
-create index idx2 on #deleted2(_LogicalDB_key)
+create index idx2 on #deleted2(_Sequence_key)
 go
 
 select seqID = d.accID, mgiID = ma2.accID, name = m.symbol
 into #mdeleted
-from #deleted2 d, ACC_Accession ma, ACC_Accession ma2, MRK_Marker m
-where d.accID = ma.accID
-and d._LogicalDB_key = ma._LogicalDB_key
-and ma._MGIType_key = 2
-and ma._Object_key = ma2._Object_key 
+from #deleted2 d, SEQ_Marker_Cache ma, ACC_Accession ma2, MRK_Marker m
+where d._Sequence_key = ma._Sequence_key
+and ma._Marker_key = ma2._Object_key 
 and ma2._MGIType_key = 2
 and ma2._LogicalDB_key = 1
 and ma2.prefixPart = 'MGI:'
 and ma2.preferred = 1
-and ma._Object_key = m._Marker_key 
+and ma._Marker_key = m._Marker_key 
 go
 
 create index idx1 on #mdeleted(seqID)
@@ -41,16 +39,14 @@ go
 
 select seqID = d.accID, mgiID = pa2.accID, p.name
 into #pdeleted
-from #deleted2 d, ACC_Accession pa, ACC_Accession pa2, PRB_Probe p
-where d.accID = pa.accID
-and d._LogicalDB_key = pa._LogicalDB_key
-and pa._MGIType_key = 3
-and pa._Object_key = pa2._Object_key
+from #deleted2 d, SEQ_Probe_Cache pa, ACC_Accession pa2, PRB_Probe p
+where d._Sequence_key = pa._Sequence_key
+and pa._Probe_key = pa2._Object_key
 and pa2._MGIType_key = 3
 and pa2._LogicalDB_key = 1
 and pa2.prefixPart = 'MGI:'
 and pa2.preferred = 1
-and pa._Object_key = p._Probe_key
+and pa._Probe_key = p._Probe_key
 go
 
 create index idx1 on #pdeleted(seqID)
