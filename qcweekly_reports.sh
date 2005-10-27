afa25f03e6ee9faac62acae5dbdcce67fa590d6e
Lori Corbani <Lori.Corbani@jax.org>
2005-10-27 18:59:34 +0000

    add logging

--- qcweekly_reports.sh
+++ qcweekly_reports.sh
@@ -10,6 +10,10 @@
 
 cd `dirname $0` && source ./Configuration
 
+setenv LOG      ${QCLOGSDIR}/`basename $0`.log
+rm -rf ${LOG}
+touch ${LOG}
+
 #foreach i ($QCWEEKLY/*.sql)
 #if ( $i == "$QCWEEKLY/ALL_ImmuneAnnot.sql" ) then
 #	mv -f $QCOUTPUTDIR/`basename $i`.[0-9]*.rpt $QCALLELEARCHIVE
@@ -24,11 +28,13 @@ cd `dirname $0` && source ./Configuration
 cd weekly
 foreach i (*.py)
 if ( $i == "ALL_ImmuneAnnot.py" ) then
+	echo $i, `date`
         echo "$QCOUTPUTDIR/`basename $i py`[0-9]*.rpt"
 	mv -f $QCOUTPUTDIR/`basename $i py`[0-9]*.rpt $QCALLELEARCHIVE
 	rm -rf $QCOUTPUTDIR/`basename $i py`current.rpt
-	$i
+	$i >>& ${LOG}
 	ln -s $QCOUTPUTDIR/`basename $i py`${DATE}.rpt $QCOUTPUTDIR/`basename $i py`current.rpt
+	echo $i, `date`
 else
 	$i
 endif
