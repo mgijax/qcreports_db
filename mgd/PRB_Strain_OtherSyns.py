275c6cf68ce8a67d1a6b9dfe76e98133c98bb212
CVS 2 Git Conversion <mgiadmin@jax.org>
2005-11-15 17:31:09 +0000

    This commit was manufactured by cvs2svn to create branch 'tr7062'.
    Cherrypick from master 2005-11-15 17:31:08 UTC Lori Corbani <Lori.Corbani@jax.org> 'TR 7248':
        mgd/PRB_Strain_NomenSyns.py
        mgd/PRB_Strain_OtherSyns.py
        weekly/ALL_Progress.py

---
+++ mgd/PRB_Strain_OtherSyns.py
@@ -0,0 +1,73 @@
+#!/usr/local/bin/python
+
+'''
+#
+# PRB_Strain_OtherSyns.py
+#
+# Report:
+#       Tab-delimited file of Strains w/ Other Synonyms
+#
+# Usage:
+#       PRB_Strain_OtherSyns.py
+#
+# Used by:
+#       Internal Report
+#
+# Notes:
+#
+# History:
+#
+# lec	11/14/2005
+#	- TR 7247
+#
+'''
+ 
+import sys
+import os
+import string
+import db
+import mgi_utils
+import reportlib
+
+#
+# Main
+#
+
+fp = reportlib.init(sys.argv[0], title = 'Strain Other Synonyms', outputdir = os.environ['QCOUTPUTDIR'])
+fp.write(string.ljust('MGI id', 35))
+fp.write(string.ljust('strain', 85))
+fp.write('synonyms' + reportlib.CRT)
+fp.write(string.ljust('------------------------------', 35))
+fp.write(string.ljust('--------------------------------------------------------------------------------', 85))
+fp.write('--------------------------------------------------' + reportlib.CRT)
+
+db.sql('select s._Strain_key, strain = substring(s.strain,1,80), sy.synonym, a.accID ' + \
+	'into #strains ' + \
+	'from PRB_Strain s, MGI_Synonym sy, ACC_Accession a ' + \
+	'where s._Strain_key = sy._Object_key ' + \
+	'and sy._MGIType_key = 10 ' + \
+	'and sy._SynonymType_key != 1001 ' + \
+	'and s._Strain_key = a._Object_key ' + \
+	'and a._MGIType_key = 10 ' + \
+	'and a._LogicalDB_key = 1 '+ \
+	'and a.preferred = 1', None)
+
+syns = {}
+results = db.sql('select _Strain_key, synonym from #strains', 'auto')
+for r in results:
+    key = r['_Strain_key']
+    value = r['synonym']
+    if not syns.has_key(key):
+        syns[key] = []
+    syns[key].append(value)
+
+results = db.sql('select distinct _Strain_key, strain, accID from #strains order by strain', 'auto')
+for r in results:
+    fp.write(string.ljust(r['accID'], 35))
+    fp.write(string.ljust(r['strain'], 85))
+    fp.write(string.joinfields(syns[r['_Strain_key']], ',') + reportlib.CRT)
+
+fp.write('\n(%d rows affected)\n' % (len(results)))
+reportlib.trailer(fp)
+reportlib.finish_nonps(fp)
+
