6e7054445d8ff7dca2e3c573e400da69234c8533
Lori Corbani <Lori.Corbani@jax.org>
2005-11-21 13:15:01 +0000

    re-write

--- mgd/HMD_ConservedUN.sql
+++ mgd/HMD_ConservedUN.sql
@@ -2,11 +2,17 @@ print ""
 print "Orthologies w/ Conserved Map Location and Chromosome = UN"
 print ""
 
-select h.symbol, h.commonName, h.jnum
-from HMD_Homology_View h, HMD_Homology_Assay a
-where h._Homology_key = a._Homology_key
+select m.symbol, o.commonName, jnum = aa.accID
+from HMD_Homology_Marker hm, MRK_Marker m, MGI_Organism o, HMD_Homology_Assay a, HMD_Homology h, ACC_Accession aa
+where hm._Homology_key = a._Homology_key
 and a._Assay_key = 14
-and h.chromosome = "UN"
-order by h._Homology_key, h.symbol
+and hm._Marker_key = m._Marker_key
+and m.chromosome = "UN"
+and m._Organism_key = o._Organism_key
+and hm._Homology_key = h._Homology_key
+and h._Refs_key = aa._Object_key
+and aa._LogicalDB_key = 1
+and aa.prefixPart = 'J:'
+order by hm._Homology_key, m.symbol
 go
 
