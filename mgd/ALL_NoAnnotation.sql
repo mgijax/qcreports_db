c216071a66c120e9cf43793df757b3246ec39954
Lori Corbani <Lori.Corbani@jax.org>
2005-12-02 20:00:54 +0000

    TR 7298

--- mgd/ALL_NoAnnotation.sql
+++ mgd/ALL_NoAnnotation.sql
@@ -1,15 +1,26 @@
 
 print ""
-print "Alleles w/ 'Associated Phenotype Controlled Terms' in Notes with no MP annotation"
+print "Alleles w/ 'Associated Phenotype Controlled Terms' in Notes"
 print ""
 
-select distinct a.symbol
+select distinct a.symbol, annotation = 'no '
 from MGI_Note n, MGI_NoteChunk nc, ALL_Allele a
 where nc.note like '%Associated Phenotype Controlled Terms%'
 and n._Object_key = a._Allele_key
 and n._MGIType_key = 11
 and n._Note_key = nc._Note_key
 and not exists (select 1 from GXD_AlleleGenotype g, VOC_Annot v
+where a._Allele_key = g._Allele_key
+and g._Genotype_key = v._Object_key
+and v._AnnotType_key = 1002)
+union
+select distinct a.symbol, annotation = 'yes'
+from MGI_Note n, MGI_NoteChunk nc, ALL_Allele a
+where nc.note like '%Associated Phenotype Controlled Terms%'
+and n._Object_key = a._Allele_key
+and n._MGIType_key = 11
+and n._Note_key = nc._Note_key
+and exists (select 1 from GXD_AlleleGenotype g, VOC_Annot v
 where a._Allele_key = g._Allele_key
 and g._Genotype_key = v._Object_key
 and v._AnnotType_key = 1002)
