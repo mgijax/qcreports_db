30ab74b32833a7c68636c1ac64f6989a79f2839f
Lori Corbani <Lori.Corbani@jax.org>
2006-02-17 16:48:04 +0000

    TR 7495

--- mgd/PRB_StrainJAX2.py
+++ mgd/PRB_StrainJAX2.py
@@ -18,6 +18,9 @@
 #
 # History:
 #
+# lec	02/17/2006
+#	- TR 7495/convert to tab-delimited
+#
 # lec	12/20/2005
 #	- TR 7342
 #
@@ -30,6 +33,9 @@ import db
 import mgi_utils
 import reportlib
 
+TAB = reportlib.TAB
+CRT = reportlib.CRT
+
 #
 # Main
 #
@@ -37,14 +43,9 @@ import reportlib
 title = 'Public JAX strains with exact/inexact genotype matches\nwhere genotype MP annotations have been modified within the last 7 days'
 fp = reportlib.init(sys.argv[0], title, outputdir = os.environ['QCOUTPUTDIR'])
 
-fp.write(string.ljust('JR#', 10))
-fp.write(string.ljust('Strain', 75))
-fp.write(string.ljust('Genotypes w/ MP Annotations modified within the last 7 days', 50))
-fp.write(reportlib.CRT)
-fp.write(string.ljust('------', 10))
-fp.write(string.ljust('--------------------------------------------------', 75))
-fp.write(string.ljust('-----------------------------------------------------------', 50))
-fp.write(reportlib.CRT)
+fp.write('JR#' + TAB)
+fp.write('Strain' + TAB)
+fp.write('Genotypes w/ MP Annotations modified within the last 7 days' + CRT)
 
 # Retrieve all Strains that have a JRS ID and a genotype association
 
@@ -63,15 +64,19 @@ db.sql('create index idx2 on #strains(_Genotype_key)', None)
 # Genotypes w/ MP annotations within the last 7 days
 
 currentDate = mgi_utils.date('%m/%d/%Y')
-db.sql('select distinct s._Strain_key, ag._Genotype_key ' + \
-	'into #genotypes ' + \
-	'from #strains s, PRB_Strain_Marker sm, GXD_AlleleGenotype ag, VOC_Annot g, VOC_Evidence e ' + \
-	'where s._Strain_key = sm._Strain_key ' + \
-	'and sm._Allele_key = ag._Allele_key ' + \
-	'and ag._Genotype_key = g._Object_key ' + \
-	'and g._AnnotType_key = 1002 ' + \
+db.sql('select distinct g._Object_key ' + \
+	'into #geno1 from VOC_Annot g, VOC_Evidence e ' + \
+	'where g._AnnotType_key = 1002 ' + \
 	'and g._Annot_key = e._Annot_key ' + \
 	'and e.modification_date between dateadd(day, -7, "%s") and "%s" ' % (currentDate, currentDate), None)
+db.sql('create index idx1 on #geno1(_Object_key)', None)
+
+db.sql('select distinct s._Strain_key, ag._Genotype_key ' + \
+	'into #genotypes ' + \
+	'from #strains s, PRB_Strain_Marker sm, GXD_AlleleGenotype ag, #geno1 g ' + \
+	'where s._Strain_key = sm._Strain_key ' + \
+	'and sm._Allele_key = ag._Allele_key ' + \
+	'and ag._Genotype_key = g._Object_key ', 'auto')
 db.sql('create index idx1 on #genotypes(_Strain_key)', None)
 db.sql('create index idx2 on #genotypes(_Genotype_key)', None)
 
@@ -94,12 +99,11 @@ results = db.sql('select s.* from #strains s ' + \
 	'where exists (select 1 from #genotypes g where s._Strain_key = g._Strain_key) ' + \
 	'order by strain', 'auto')
 for r in results:
-    fp.write(string.ljust(r['jrs'], 10))
-    fp.write(string.ljust(r['strain'], 75))
-    fp.write(string.join(genotypes[r['_Strain_key']], ',') + reportlib.CRT)
+    fp.write(r['jrs'] + TAB)
+    fp.write(r['strain'] + TAB)
+    fp.write(string.join(genotypes[r['_Strain_key']], ',') + CRT)
 
 fp.write('\n(%d rows affected)\n' % (len(results)))
 
-reportlib.trailer(fp)
 reportlib.finish_nonps(fp)
 
