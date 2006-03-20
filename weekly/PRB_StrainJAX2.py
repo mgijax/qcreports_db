6098a1da274e0f4a2127dab72f5319edaeffc1c6
Lori Corbani <Lori.Corbani@jax.org>
2006-03-20 19:28:20 +0000

    PRB_StrainJAX3.py replacing PRB_StrainJAX2.py

--- weekly/PRB_StrainJAX2.py
+++ weekly/PRB_StrainJAX2.py
@@ -2,14 +2,15 @@
 
 '''
 #
-# PRB_StrainJRS2.py 12/20/2005
+# PRB_StrainJRS3.py 03/20/2006
 #
 # Report:
-#	Public Strains with JRS IDs that have a genotype association
+#	Public Strains with JRS IDs whose Alleles are used in a Genotype
+#	but there is nod corresponding strain/genotype association.
 #       
 #
 # Usage:
-#       PRB_StrainJRS2.py
+#       PRB_StrainJRS3.py
 #
 # Used by:
 #       Internal Report
@@ -18,11 +19,8 @@
 #
 # History:
 #
-# lec	02/17/2006
-#	- TR 7495/convert to tab-delimited
-#
-# lec	12/20/2005
-#	- TR 7342
+# lec	03/20/2006
+#	- new qc report
 #
 '''
  
@@ -36,54 +34,42 @@ import reportlib
 TAB = reportlib.TAB
 CRT = reportlib.CRT
 
+currentDate = mgi_utils.date('%m/%d/%Y')
+
 #
 # Main
 #
 
-title = 'Public JAX strains with exact/inexact genotype matches\nwhere genotype MP annotations have been modified within the last 7 days'
+title = 'Public JAX strains whose Alleles are used in a Genotype but there is no corresponding Strain/Genotype association\n'
+title = title + '(where Genotypes have been created within the last week)'
+
 fp = reportlib.init(sys.argv[0], title, outputdir = os.environ['QCOUTPUTDIR'], fileExt = '.' + os.environ['DATE'] + '.rpt')
 
 fp.write('JR#' + TAB)
 fp.write('Strain' + TAB)
-fp.write('Genotypes w/ MP Annotations modified within the last 7 days' + CRT)
+fp.write('Genotypes' + CRT*2)
 
-# Retrieve all Strains that have a JRS ID and a genotype association
+# Retrieve all Strains that have a JRS ID and whose Alleles are used in a Genotype
 
-db.sql('select s._Strain_key, strain = substring(s.strain,1,70), jrs = substring(a.accID,1,6), g._Genotype_key ' + \
+db.sql('select distinct s._Strain_key, strain = substring(s.strain,1,70), jrs = substring(a.accID,1,6), g._Genotype_key ' + \
 	'into #strains ' + \
-	'from PRB_Strain s, ACC_Accession a, PRB_Strain_Genotype g ' + \
+	'from PRB_Strain s, ACC_Accession a, PRB_Strain_Marker sm, GXD_Genotype g, GXD_AlleleGenotype ag ' + \
 	'where s.private = 0 ' + \
 	'and s._Strain_key = a._Object_key ' + \
 	'and a._MGIType_key = 10 ' + \
 	'and a._LogicalDB_key = 22 ' + \
-	'and s._Strain_key = g._Strain_key', None)
+	'and s._Strain_key = sm._Strain_key ' + \
+	'and sm._Allele_key = ag._Allele_key ' + \
+	'and ag._Genotype_key = g._Genotype_key ' + \
+	'and g.creation_date between dateadd(day, -7, "%s") and "%s" ' % (currentDate, currentDate), None)
 
 db.sql('create index idx1 on #strains(_Strain_key)', None)
 db.sql('create index idx2 on #strains(_Genotype_key)', None)
 
-# Genotypes w/ MP annotations within the last 7 days
-
-currentDate = mgi_utils.date('%m/%d/%Y')
-db.sql('select distinct g._Object_key ' + \
-	'into #geno1 from VOC_Annot g, VOC_Evidence e ' + \
-	'where g._AnnotType_key = 1002 ' + \
-	'and g._Annot_key = e._Annot_key ' + \
-	'and e.modification_date between dateadd(day, -7, "%s") and "%s" ' % (currentDate, currentDate), None)
-db.sql('create index idx1 on #geno1(_Object_key)', None)
-
-db.sql('select distinct s._Strain_key, ag._Genotype_key ' + \
-	'into #genotypes ' + \
-	'from #strains s, PRB_Strain_Marker sm, GXD_AlleleGenotype ag, #geno1 g ' + \
-	'where s._Strain_key = sm._Strain_key ' + \
-	'and sm._Allele_key = ag._Allele_key ' + \
-	'and ag._Genotype_key = g._Object_key ', 'auto')
-db.sql('create index idx1 on #genotypes(_Strain_key)', None)
-db.sql('create index idx2 on #genotypes(_Genotype_key)', None)
-
 genotypes = {}
-results = db.sql('select distinct g._Strain_key, a.accID ' + \
-	'from #genotypes g, ACC_Accession a ' + \
-	'where g._Genotype_key = a._Object_key ' + \
+results = db.sql('select distinct s._Strain_key, a.accID ' + \
+	'from #strains s, ACC_Accession a ' + \
+	'where s._Genotype_key = a._Object_key ' + \
 	'and a._MGIType_key = 12 ' + \
 	'and a._LogicalDB_key = 1 ' + \
 	'and a.prefixPart = "MGI:" ' + \
@@ -95,8 +81,10 @@ for r in results:
 	genotypes[key] = []
     genotypes[key].append(value)
 
-results = db.sql('select s.* from #strains s ' + \
-	'where exists (select 1 from #genotypes g where s._Strain_key = g._Strain_key) ' + \
+results = db.sql('select distinct s._Strain_key, s.strain, s.jrs ' + \
+	'from #strains s ' + \
+	'where not exists ' + \
+	'(select 1 from PRB_Strain_Genotype g where s._Strain_key = g._Strain_key and s._Genotype_key = g._Genotype_key) ' + \
 	'order by strain', 'auto')
 for r in results:
     fp.write(r['jrs'] + TAB)
