6c7948fcfd767170e64dc862a80a09e58a54d4b4
Lori Corbani <Lori.Corbani@jax.org>
2006-05-09 13:44:14 +0000

    TR 7614

---
+++ weekly/PRB_StrainJAX3.sql
@@ -0,0 +1,23 @@
+
+declare @printdate varchar(255)
+select @printDate = "New JR#s created between " + 
+	convert(char(10), dateadd(day, -7, getdate()), 101) + 
+	" and " + convert(char(10), getdate(), 101)
+
+print ""
+print @printDate
+print ""
+
+select a.accID
+from ACC_Accession a
+where a._MGIType_key = 10
+and a._LogicalDB_key = 22
+and a.creation_date between dateadd(day, -7, getdate()) and getdate()
+go
+
+quit
+
+END
+
+cat /mgi/software/customSQL/bin/trailer >> $0.rpt
+
