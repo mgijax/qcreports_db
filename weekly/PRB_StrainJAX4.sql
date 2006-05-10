604fb4d7692487475396d88580541e1b22cc2912
CVS 2 Git Conversion <mgiadmin@jax.org>
2006-05-10 16:30:15 +0000

    This commit was manufactured by cvs2svn to create branch 'mgi34-patch'.
    Sprout from master 2006-04-12 17:57:40 UTC Lori Corbani <Lori.Corbani@jax.org> 'qcreports_db-3-4-3-10'
    Cherrypick from master 2006-05-10 16:30:13 UTC Lori Corbani <Lori.Corbani@jax.org> 'qcreports_db-3-5-0-3':
        mgd/PRB_StrainJAX3.py
        weekly/PRB_StrainJAX4.sql

---
+++ weekly/PRB_StrainJAX4.sql
@@ -0,0 +1,29 @@
+
+set nocount on
+go
+
+declare @printDate varchar(255)
+select @printDate = "New JR#s created between " + 
+	convert(char(10), dateadd(day, -7, getdate()), 101) + 
+	" and " + convert(char(10), getdate(), 101)
+
+print ""
+print @printDate
+print ""
+
+set nocount off
+go
+
+select a.accID
+from ACC_Accession a
+where a._MGIType_key = 10
+and a._LogicalDB_key = 22
+and a.creation_date between dateadd(day, -7, getdate()) and getdate()
+go
+
+quit
+
+END
+
+cat /mgi/software/customSQL/bin/trailer >> $0.rpt
+
