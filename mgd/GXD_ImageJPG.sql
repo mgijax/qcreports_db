74f72414e3cfc02f8acd47f1d25afe9cac23a986
Lori Corbani <Lori.Corbani@jax.org>
2006-05-19 13:31:43 +0000

    TR 7684

--- mgd/GXD_ImageJPG.sql
+++ mgd/GXD_ImageJPG.sql
@@ -3,25 +3,28 @@ set nocount on
 go
 
 /* select all Gel Assays with Image Panes that have JPGs (xDim is not null) */
+/* exclude J:80502 */
 
-select distinct a._Assay_key, i._Refs_key, a._ImagePane_key, ip._Image_key
+select distinct a._Assay_key, a._AssayType_key, i._Refs_key, a._ImagePane_key, ip._Image_key
 into #assays 
 from GXD_Assay a, IMG_Image i, IMG_ImagePane ip 
 where a._ImagePane_key = ip._ImagePane_key 
 and ip._Image_key = i._Image_key 
 and i.xDim is not null
+and a._Refs_key not in (81463)
 go
 
 /* select all InSitu Assays with Image Panes that have JPGs (xDim is not null) */
 
 insert into #assays 
-select distinct s._Assay_key, i._Refs_key, iri._ImagePane_key, ip._Image_key
-from GXD_InSituResultImage iri, GXD_InSituResult r, GXD_Specimen s, IMG_Image i, IMG_ImagePane ip 
+select distinct s._Assay_key, a._AssayType_key, i._Refs_key, iri._ImagePane_key, ip._Image_key
+from GXD_InSituResultImage iri, GXD_InSituResult r, GXD_Specimen s, GXD_Assay a, IMG_Image i, IMG_ImagePane ip 
 where iri._ImagePane_key = ip._ImagePane_key 
 and ip._Image_key = i._Image_key 
 and i.xDim is not null 
 and iri._Result_key = r._Result_key 
 and r._Specimen_key = s._Specimen_key
+and s._Assay_key = a._Assay_key
 go
 
 create index idx1 on #assays(_ImagePane_key)
@@ -29,7 +32,9 @@ go
 
 /* select those that are annotated to more than 2 assays */
 
-select * into #final from #assays group by _ImagePane_key having count(*) > 2
+select * into #final from #assays where _AssayType_key = 2 group by _ImagePane_key having count(*) > 2
+union
+select * from #assays where _AssayType_key != 2 group by _ImagePane_key having count(*) > 1
 go
 
 create index idx1 on #final(_Refs_key)
@@ -40,7 +45,7 @@ set nocount off
 go
 
 print ""
-print "Displayed Image Panes annotated to more than 2 assays."
+print "Displayed Image Panes annotated to >2 assays (if Immunohistochemistry), >1 assay for all others."
 print ""
 
 select distinct imageID = i.accID, refID = r.accID 
