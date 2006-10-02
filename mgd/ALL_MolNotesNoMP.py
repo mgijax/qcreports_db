a6210de2e6dce1b05f6d2f57d5c1b03c86dbf43d
Lori Corbani <Lori.Corbani@jax.org>
2006-10-02 18:38:23 +0000

    qcreports_db-3-5-1-7

--- mgd/ALL_MolNotesNoMP.py
+++ mgd/ALL_MolNotesNoMP.py
@@ -7,11 +7,16 @@
 # Report:
 #       Aleles that have Molecular Notes but no MP Annotations
 #
-#       Allele creation date
+#       Allele approval date
 #	Allele MGI ID
 #	Allele Type
 #	Allele symbol
 #	J#'s (comma-separated, sorted by J#, highest first)
+#
+#	exclude non-approved Alleles
+#	exclude Alleles of type QTL
+#	exclude Alleles of type "transgenic (cre/flp)"
+#	exclude Alleles of type "transgenic (reporter)"
 #
 # Usage:
 #       ALL_MolNotesNoMP.py
@@ -43,17 +48,20 @@ PAGE = reportlib.PAGE
 fp = reportlib.init(sys.argv[0], 'Alleles that have Molecular Notes but no MP Annotations', os.environ['QCOUTPUTDIR'])
 
 
-db.sql('select a._Allele_key, a.symbol, a.creation_date, alleleType = t.term ' + \
+db.sql('select a._Allele_key, a.symbol, a.approval_date, alleleType = t.term ' + \
     'into #alleles  ' + \
     'from ALL_Allele a, VOC_Term t ' + \
-    'where a._Allele_Type_key = t._Term_key ' + \
+    'where a.approval_date is not NULL ' + \
+    'and a._Allele_Type_key not in (847130, 847128, 847129, 847127) ' + \
+    'and a._Allele_Type_key = t._Term_key ' + \
     'and exists (select 1 from MGI_Note n, MGI_NoteType nt  ' + \
     'where a._Allele_key = n._Object_key  ' + \
     'and n._MGIType_key = 11  ' + \
     'and n._NoteType_key = nt._NoteType_key  ' + \
     'and nt.noteType = "Molecular")  ' + \
-    'and not exists (select 1 from VOC_Annot t ' + \
-    'where a._Allele_key = t._Object_key ' + \
+    'and not exists (select 1 from VOC_Annot t, GXD_AlleleGenotype g ' + \
+    'where a._Allele_key = g._Allele_key ' + \
+    'and g._Genotype_key = t._Object_key ' + \
     'and t._AnnotType_key = 1002)', None)
 db.sql('create index idex1 on #alleles(_Allele_key)', None)
 
@@ -76,14 +84,14 @@ for r in results:
 	refs[key] = []
     refs[key].append(value)
 
-results = db.sql('select a._Allele_key, cDate = convert(char(10), a.creation_date, 101), a1.accID, a.alleleType, a.symbol ' + \
+results = db.sql('select a._Allele_key, cDate = convert(char(10), a.approval_date, 101), a1.accID, a.alleleType, a.symbol ' + \
     'from #alleles a, ACC_Accession a1 ' + \
     'where a._Allele_key = a1._Object_key ' + \
     'and a1._MGIType_key = 11 ' + \
     'and a1._LogicalDB_key = 1 ' + \
     'and a1.prefixPart = "MGI:" ' + \
     'and a1.preferred = 1 ' + \
-    'order by a.creation_date desc, a.symbol', 'auto')
+    'order by a.approval_date desc, a.symbol', 'auto')
 
 for r in results:
 
