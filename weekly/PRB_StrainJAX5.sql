a7b39f21f36c0680e2d44f467e2b47de511bdaa4
David Miers <dave.miers@jax.org>
2007-04-24 12:17:06 +0000

    New

---
+++ weekly/PRB_StrainJAX5.sql
@@ -0,0 +1,33 @@
+
+set nocount on
+go
+
+declare @printDate varchar(255)
+select @printDate = "JR#s with alleles added between " + 
+	convert(char(10), dateadd(day, -7, getdate()), 101) + 
+	" and " + convert(char(10), getdate(), 101)
+
+print ""
+print @printDate
+print ""
+
+set nocount off
+go
+select a.accID
+from ACC_Accession a, PRB_Strain s
+where a._MGIType_key = 10
+      and a._LogicalDB_key = 22
+      and a._Object_key = s._Strain_key
+      and s.private = 0
+      and exists (select 1
+                  from PRB_Strain_Marker sm
+                  where sm._Strain_key = s._Strain_key
+                        and sm._Qualifier_key = 615427
+                        and sm.creation_date between
+                            dateadd(day, -7, getdate()) and getdate())
+go
+
+quit
+
+END
+
