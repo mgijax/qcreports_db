b94b7625f9d075e298c0dbb2cb0ef0add68f67d5
Sharon Giannatto <Sharon.Giannatto@jax.org>
2007-05-04 16:18:39 +0000

    qcreports_db-3-5-3-3

--- mgd/MRK_MultCoordinate.sql
+++ mgd/MRK_MultCoordinate.sql
@@ -7,7 +7,7 @@ into #coord
 from SEQ_Marker_Cache s, SEQ_Coord_Cache c
 where s._Organism_key = 1
 and s._Sequence_key = c._Sequence_key
-and c.provider in ('NCBI Gene Model', 'Ensembl Gene Model')
+and c.provider in ('NCBI Gene Model', 'Ensembl Gene Model', 'VEGA Gene Model')
 go
 
 create index idx1 on #coord(_Marker_key)
@@ -28,7 +28,7 @@ set nocount off
 go
 
 print ""
-print "Symbols w/ > 1 Ensembl or NCBI Gene Model Association"
+print "Symbols w/ > 1 Ensembl, VEGA or NCBI Gene Model Association"
 print ""
 
 select m.symbol, s.accID, provider = substring(s.provider,1,50)
