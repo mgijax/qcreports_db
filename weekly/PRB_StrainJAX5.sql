e622238b7bb5a77112a25dd8a7ead5a7ed91bc96
David Miers <dave.miers@jax.org>
2007-05-08 13:06:26 +0000

    Restore original report and look for any marker/allele record changes

--- weekly/PRB_StrainJAX5.sql
+++ weekly/PRB_StrainJAX5.sql
@@ -15,15 +15,17 @@ set nocount off
 go
 select a.accID
 from ACC_Accession a, PRB_Strain s
-where a._MGIType_key = 10
-      and a._LogicalDB_key = 22
-      and a._Object_key = s._Strain_key
-      and s.private = 0
-      and exists (select 1
-                  from PRB_Strain_Genotype sg
-                  where sg._Strain_key = s._Strain_key
-                        and sg.creation_date between
-                            dateadd(day, -7, getdate()) and getdate())
+where a._MGIType_key = 10 and
+      a._LogicalDB_key = 22 and
+      a._Object_key = s._Strain_key and
+      s.private = 0 and
+      exists (select 1 from PRB_Strain_Marker sm
+              where sm._Strain_key = s._Strain_key and
+                    sm._Qualifier_key = 615427 and
+                    ((sm.creation_date between
+                        dateadd(day, -7, getdate()) and getdate()) or
+                    (sm.modification_date between
+                        dateadd(day, -7, getdate()) and getdate())))
 order by a.accID
 go
 
