b388e83214c98d8ea0839b1f26af5caec0962abe
Sharon Giannatto <Sharon.Giannatto@jax.org>
2007-05-17 18:54:58 +0000

    qcreports_db-3-5-3-6

---
+++ mgd/MRK_MultMarkerGeneModels.sql
@@ -0,0 +1,40 @@
+
+set nocount on
+go
+
+select s._Sequence_key, s._Marker_key, s.accID, c.provider
+into #coord
+from SEQ_Marker_Cache s, SEQ_Coord_Cache c
+where s._Organism_key = 1
+and s._Sequence_key = c._Sequence_key
+and c.provider in ('Ensembl Gene Model', 'VEGA Gene Model')
+go
+
+create index idx1 on #coord(_Sequence_key)
+create index idx2 on #coord(provider)
+go
+
+select _Sequence_key, _Marker_key, accID, provider
+into #dups
+from #coord
+group by provider, _Sequence_key having count(*) > 1
+go
+
+create index idx1 on #dups(_Marker_key)
+create index idx2 on #dups(_Sequence_key)
+go
+
+set nocount off
+go
+
+print ""
+print "Ensembl or VEGA Gene Models w/ > 1 Marker Association"
+print ""
+
+select s.accID, m.symbol, provider = substring(s.provider,1,40)
+from #dups s, MRK_Marker m
+where s._Marker_key = m._Marker_key
+order by s.provider, s.accID
+go
+
+
--- mgd/MRK_NoENSEMBL.py
+++ mgd/MRK_NoENSEMBL.py
@@ -29,22 +29,47 @@ PAGE = reportlib.PAGE
 #
 # Main
 #
-
+print "initializing"
 fp = reportlib.init(sys.argv[0], outputdir = os.environ['QCOUTPUTDIR'], printHeading = None)
 
 fp.write('#\n')
 fp.write('# Ensembl Gene Models with no Marker Association\n')
 fp.write('#\n')
 
-results = db.sql('select a.accID ' + \
-      'from ACC_Accession a ' + \
-      'where a._MGIType_key = 19 and ' + \
-      'a._LogicalDB_key = 60 and ' + \
-      'not exists (select 1 from SEQ_Marker_Cache s where a._Object_key = s._Sequence_key) ' + \
-	'order by a.accID', 'auto')
+# get the full set of Ensembl gene model ids
+# we exclude the obsolete vega ids (only include status = active)
+# we exclude all but type protein_coding and pseudogene
+db.sql('select accID ' + \
+    'into #ensemblGeneModel ' + \
+    'from ACC_Accession  a, SEQ_Sequence s ' + \
+    'where a._lOGicalDB_key = 60 ' + \
+    'and a._MGIType_key = 19 ' + \
+    'and a._Object_key = s._Sequence_key ' + \
+    'and s._SequenceStatus_key = 316342 ' + \
+    'and (s.description like "%protein_coding%" or s.description like "%pseudogene%")', None)
+db.sql('create index idxAccid on #ensemblGeneModel(accID)', None)
+print "done first query and index"
+# get the set of Ensembl ids with marker associations
+db.sql('select distinct accID ' + \
+    'into #ensemblGeneAssoc ' + \
+    'from ACC_Accession  ' + \
+    'where _LogicalDB_key = 60 ' + \
+    'and _MGIType_key = 2 ' + \
+    'and preferred = 1', None)
+db.sql('create index idxAccid on #ensemblGeneAssoc(accID)', None)
+print "done second query and index"
+
+results = db.sql('select gm.accid as ensemblGeneModelNoAssoc ' + \
+    'from #ensemblGeneModel gm ' + \
+    'where not exists (select 1 ' + \
+    'from #ensemblGeneAssoc ga ' + \
+    'where ga.accid = gm.accid) ' + \
+    'order by gm.accID', 'auto')
+print "done third query and writing out results"
 
 for r in results:
-    fp.write(r['accID'] + CRT)
+    fp.write(r['ensemblGeneModelNoAssoc'] + CRT)
+    print r['ensemblGeneModelNoAssoc']
 
 fp.write('\n(%d rows affected)\n' % (len(results)))
 
