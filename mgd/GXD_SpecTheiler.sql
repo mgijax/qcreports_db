804d36415aeed8d9edbb800a751cc1a8fcbc0593
Sharon Giannatto <Sharon.Giannatto@jax.org>
2007-08-16 14:50:50 +0000

    TR8389

--- mgd/GXD_SpecTheiler.sql
+++ mgd/GXD_SpecTheiler.sql
@@ -63,3 +63,114 @@ from #temp4 t, GXD_GelLane s, GXD_Assay_View a
 where t._GelLane_key = s._GelLane_key
 and s._Assay_key = a._Assay_key
 go
+
+set nocount on
+go
+
+/* Added 8/16/2007 TR8389 */
+
+/* get the structure key for reproductive system */
+select distinct _Structure_key
+into #repro
+from GXD_StructureName
+where structure = "reproductive system"
+go
+
+/* get all children of "reproductive system" */
+select distinct _Descendent_key
+into #child
+from GXD_StructureClosure
+where _Structure_key in (
+select _Structure_key
+from #repro)
+go
+
+/* get the structure key for "female" */
+select distinct _Structure_key
+into #reproF
+from #child c, GXD_StructureName sn
+where c._Descendent_key = sn._Structure_key
+and sn.structure = "female"
+go
+
+/* get all children of "female" */
+select distinct _Descendent_key
+into #fChild
+from GXD_StructureClosure
+where _Structure_key in (
+select _Structure_key
+from  #reproF)
+
+/* union female and its childrend */ 
+select _Structure_key
+into #allFStruct
+from #reproF
+union
+select _Descendent_key as _Structure_key
+from #fChild
+go
+
+/* get info about "reproductive system;female" and children */
+select distinct s._Specimen_key, s.specimenLabel, a.jnumID, a.mgiID
+into #fSpecimens
+from GXD_Specimen s, GXD_Assay_View a, GXD_InSituResult ir, GXD_ISResultStructure irs
+where s._Assay_key = a._Assay_key
+and a._AssayType_key in (1, 6, 9)
+and s._Specimen_key = ir._Specimen_key
+and ir._Result_key = irs._Result_key
+and irs._Structure_key in (
+select distinct _Structure_key
+from #allFStruct)
+go
+
+/* do same as above for male */
+select distinct _Structure_key
+into #reproM
+from #child c, GXD_StructureName sn
+where c._Descendent_key = sn._Structure_key
+and sn.structure = "male"
+go
+
+select distinct _Descendent_key
+into #mChild
+from GXD_StructureClosure
+where _Structure_key in (
+select _Structure_key
+from  #reproM)
+
+select _Structure_key
+into #allMStruct
+from #reproM
+union
+select _Descendent_key as _Structure_key
+from #mChild
+go
+
+select distinct s._Specimen_key
+into #mSpecimens
+from GXD_Specimen s, GXD_Assay_View a, GXD_InSituResult ir, GXD_ISResultStructure irs
+where s._Assay_key = a._Assay_key
+and a._AssayType_key in (1, 6, 9)
+and s._Specimen_key = ir._Specimen_key
+and ir._Result_key = irs._Result_key
+and irs._Structure_key in (
+select distinct _Structure_key
+from #allMStruct)
+
+go
+
+set nocount off
+go
+
+print ""
+print "InSitu Specimens with > 1 Sex" 
+print "(excludes J:80502)"
+print ""
+
+/* report all specimens with annotated to both male and female structures */
+select distinct mgiID, jnumID, substring(specimenLabel,1,50) as specimenLabel
+from #fSpecimens f, #mSpecimens m
+where f._Specimen_key = m._Specimen_key
+and f.jnumID != "J:80502"
+order by mgiID, jnumID
+go
