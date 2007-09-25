01485b31a38bb4a5a3b01764fb1136b4a0005efa
Lori Corbani <Lori.Corbani@jax.org>
2007-09-25 16:47:53 +0000

    TR 8429

---
+++ mgd/GXD_ImagesUnused.py
@@ -0,0 +1,120 @@
+#!/usr/local/bin/python
+
+'''
+#
+# GXD_ImagesUnused.py
+#
+# Report:
+#       TR 8429
+#
+#      The report should look at J numbers that are in the assay table (ie papers that 
+#      have been "full coded.") For the image stubs associated with those J numbers, 
+#      which image panes are not present in the assay table (ie which were entered but 
+#      not used for full coding)?
+# 
+#      Fields for report:
+#      MGI ID of image stub w/ unused panes (primary sort)
+#      J number (secondary sort)
+#      Figure Label of stub (tertiary sort)
+#      Pane Label
+# 
+#      Please add a row count.
+#
+# Usage:
+#
+# Notes:
+#
+# History:
+#
+# lec	09/25/2007
+#	- TR 8429
+#
+'''
+ 
+import sys
+import os
+import string
+import db
+import reportlib
+
+CRT = reportlib.CRT
+SPACE = reportlib.SPACE
+TAB = reportlib.TAB
+PAGE = reportlib.PAGE
+
+#
+# Main
+#
+
+fp = reportlib.init(sys.argv[0], 'Unused image panes from full coded references', outputdir = os.environ['QCOUTPUTDIR'])
+
+# Gel with Pane keys
+
+db.sql('select r._Refs_key, r.jnumID, a._ImagePane_key ' \
+     'into #gel ' + \
+     'from GXD_Assay a, BIB_Citation_Cache r ' \
+     'where a._Refs_key = r._Refs_key ' \
+     'and a._ImagePane_key is not null', 'None')
+
+db.sql('create index idx1 on #gel(_Refs_key)', None)
+db.sql('create index idx2 on #gel(_ImagePane_key)', None)
+
+# Specimen with Pane keys
+
+db.sql('select r._Refs_key, r.jnumID, p._ImagePane_key ' + \
+    'into #specimen ' + \
+    'from GXD_Assay a, BIB_Citation_Cache r, ' + \
+    'GXD_Specimen s, GXD_InSituResult i, GXD_InSituResultImage p ' + \
+    'where a._Refs_key = r._Refs_key ' + \
+    'and a._Assay_key = s._Assay_key ' + \
+    'and s._Specimen_key = i._Specimen_key ' + \
+    'and i._Result_key = p._Result_key', 'None')
+
+db.sql('create index idx1 on #specimen(_Refs_key)', None)
+db.sql('create index idx2 on #specimen(_ImagePane_key)', None)
+
+#
+# Images with Pane keys and stubs
+# that have been full-coded (jnumber exist in Assay)
+#
+
+db.sql('select r._Refs_key, r.jnumID, a._Image_key, a.figureLabel, aa._ImagePane_key, aa.paneLabel ' + \
+     'into #images ' + \
+     'from IMG_Image a, BIB_Citation_Cache r, IMG_ImagePane aa ' + \
+     'where exists (select 1 from GXD_Assay assay where a._Refs_key = assay._Refs_key) ' +
+     'and a._Refs_key = r._Refs_key ' + \
+     'and a._ImageType_key = 1072158 ' \
+     'and a.xDim is not null ' + \
+     'and a._Image_key = aa._Image_key '
+     'and aa.paneLabel is not null', 'None')
+
+db.sql('create index idx1 on #images(jnumID)', None)
+db.sql('create index idx2 on #images(figureLabel)', None)
+
+#
+# Image Stubs that are not used in Specimens or Gels
+#
+
+results = db.sql('select a.accID, i.jnumID, i.figureLabel, i.paneLabel ' + \
+    'from #images i, ACC_Accession a ' + \
+    'where not exists (select 1 from #specimen s ' + \
+    'where i._Refs_key = s._Refs_key ' + \
+    'and i._ImagePane_key = s._ImagePane_key) ' + \
+    'and not exists (select 1 from #gel s ' + \
+    'where i._Refs_key = s._Refs_key ' + \
+    'and i._ImagePane_key = s._ImagePane_key) ' + \
+    'and i._Image_key = a._Object_key ' + \
+    'and a._MGIType_key = 9 ' + \
+    'and a._LogicalDB_key = 1 ' + \
+    'order by a.accID, i.jnumID, i.figureLabel', 'auto')
+
+for r in results:
+    fp.write(r['accID'] + TAB)
+    fp.write(r['jnumID'] + TAB)
+    fp.write(r['figureLabel'] + TAB)
+    fp.write(r['paneLabel'] + CRT)
+
+fp.write(CRT + '(%d rows affected)' % (len(results)) + CRT)
+
+reportlib.finish_nonps(fp)	# non-postscript file
+
