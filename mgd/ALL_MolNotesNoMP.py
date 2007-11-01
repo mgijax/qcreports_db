06a2d0040d3eb5991ba3e552b6b701203a49b311
Lori Corbani <Lori.Corbani@jax.org>
2007-11-01 12:42:22 +0000

    TR 8553

--- mgd/ALL_MolNotesNoMP.py
+++ mgd/ALL_MolNotesNoMP.py
@@ -11,9 +11,11 @@
 #	Allele MGI ID
 #	Allele Type
 #	Allele symbol
+#       Number of References
 #	J#'s (comma-separated, sorted by J#, highest first)
 #
 #	exclude non-approved Alleles
+#	exclude not-applicable Alleles
 #	exclude Alleles of type QTL
 #	exclude Alleles of type "transgenic (cre/flp)"
 #	exclude Alleles of type "transgenic (reporter)"
@@ -33,6 +35,11 @@
 #
 # 08/10/2007	dbm
 #       - added code to exclude rows with only J:94077 (TR8442)
+#
+# 10/23/2007
+#	- TR 8553
+#         exclude Not Applicable alleles
+#	  add "number of references"
 #
 '''
  
@@ -54,11 +61,17 @@ PAGE = reportlib.PAGE
 fp = reportlib.init(sys.argv[0], 'Alleles that have Molecular Notes but no MP Annotations', os.environ['QCOUTPUTDIR'])
 
 
+fp.write(string.ljust('Approval', 15) + \
+         string.ljust('Acc ID', 15) + \
+         string.ljust('Allele Type', 45) + \
+         string.ljust('Symbol', 35) + \
+         string.ljust('References', 15) + CRT*2)
+
 db.sql('select a._Allele_key, a.symbol, a.approval_date, alleleType = t.term ' + \
     'into #alleles  ' + \
     'from ALL_Allele a, VOC_Term t ' + \
     'where a.approval_date is not NULL ' + \
-    'and a._Allele_Type_key not in (847130, 847128, 847129, 847127) ' + \
+    'and a._Allele_Type_key not in (847130, 847128, 847129, 847127, 847131) ' + \
     'and a._Allele_Type_key = t._Term_key ' + \
     'and exists (select 1 from MGI_Note n, MGI_NoteType nt  ' + \
     'where a._Allele_key = n._Object_key  ' + \
@@ -69,7 +82,7 @@ db.sql('select a._Allele_key, a.symbol, a.approval_date, alleleType = t.term ' +
     'where a._Allele_key = g._Allele_key ' + \
     'and g._Genotype_key = t._Object_key ' + \
     'and t._AnnotType_key = 1002)', None)
-db.sql('create index idex1 on #alleles(_Allele_key)', None)
+db.sql('create index idx1 on #alleles(_Allele_key)', None)
 
 results = db.sql('select distinct a._Allele_key, a1.numericPart ' + \
     'from #alleles a, MGI_Reference_Assoc r, ACC_Accession a1 ' + \
@@ -112,10 +125,13 @@ for r in results:
         skipped += 1
         continue
 
-    fp.write(r['cDate'] + TAB + \
-             r['accID'] + TAB + \
-             r['alleleType'] + TAB + \
-             r['symbol'] + TAB)
+    fp.write(string.ljust(r['cDate'], 15) + \
+             string.ljust(r['accID'], 15) + \
+             string.ljust(r['alleleType'], 45) + \
+             string.ljust(r['symbol'], 35))
+
+    # total number of references associated with each alleles
+    fp.write(string.ljust(str(len(listOfRefs)), 15))
 
     for l in listOfRefs:
         if l == listOfRefs[-1]:
