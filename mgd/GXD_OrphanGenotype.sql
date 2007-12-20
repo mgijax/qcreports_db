865a3df190cea9948c886faadb16a824978720a6
David Miers <dave.miers@jax.org>
2007-12-20 17:54:37 +0000

    Modified to exclude genotypes with images attached (TR 8689).

--- mgd/GXD_OrphanGenotype.sql
+++ mgd/GXD_OrphanGenotype.sql
@@ -9,6 +9,7 @@ and not exists (select 1 from GXD_GelLane a where g._Genotype_key = a._Genotype_
 and not exists (select 1 from GXD_Specimen a where g._Genotype_key = a._Genotype_key)
 and not exists (select 1 from VOC_Annot a where g._Genotype_key = a._Object_key and a._AnnotType_key in (1001,1002, 1005))
 and not exists (select 1 from PRB_Strain_Genotype a where g._Genotype_key = a._Genotype_key)
+and not exists (select 1 from IMG_ImagePane_Assoc a where g._Genotype_key = a._Object_key and a._MGIType_key = 12)
 and g._Strain_key = s._Strain_key
 and g._Genotype_key = a._Object_key
 and a._MGIType_key = 12
