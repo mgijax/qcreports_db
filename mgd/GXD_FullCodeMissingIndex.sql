bc00aea1a83dd7a43aa1c4618780f0e4aa5bd9dd
Lori Corbani <Lori.Corbani@jax.org>
2008-01-03 16:25:03 +0000

    TR 8691

--- mgd/GXD_FullCodeMissingIndex.sql
+++ mgd/GXD_FullCodeMissingIndex.sql
@@ -2,15 +2,10 @@
 print ""
 print "Full Coded References that are missing a Gene in the Index"
 print ""
-print "excluded:"
-print "  Freeman reference (J:46439)"
-print "  Deltagen reference (J:101679)"
-print ""
 
 select distinct b.accID, m.symbol
 from GXD_Assay a, MRK_Marker m, ACC_Accession b
-where a._Refs_key not in (46734, 102744)
-and not exists (select 1 from GXD_Index i
+where not exists (select 1 from GXD_Index i
 where a._Refs_key = i._Refs_key
 and a._Marker_key = i._Marker_key)
 and a._Marker_key = m._Marker_key
