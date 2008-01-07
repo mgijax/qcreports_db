bbd4d8d4e6fbb02f37bb1e0bf711b7d16cb72d1a
David Miers <dave.miers@jax.org>
2008-01-07 18:12:22 +0000

    Remove CVS headers/trailers

--- cloneload/CloneLoadRpt.sh
+++ cloneload/CloneLoadRpt.sh
@@ -1,8 +1,5 @@
 #!/bin/csh
 #
-#  $Header$
-#  $Name$
-#
 #  CloneLoadRpt.sh
 ###########################################################################
 #
@@ -74,44 +71,3 @@ foreach RPT (*.py)
 end
 
 exit 0
-
-
-#  $Log$
-#  Revision 1.2  2004/08/16 14:57:07  lec
-#  qcreports_db-23-0-6
-#
-#  Revision 1.1.2.1  2004/07/09 15:14:01  dbm
-#  Moved from parent directory
-#
-#  Revision 1.1  2004/07/09 13:46:36  dbm
-#  Moved from parent directory
-#
-#  Revision 1.2  2003/11/21 18:24:58  dbm
-#  Converted to csh
-#
-#  Revision 1.1  2003/11/07 19:31:16  dbm
-#  Initial version
-#
-#
-###########################################################################
-#
-# Warranty Disclaimer and Copyright Notice
-#
-#  THE JACKSON LABORATORY MAKES NO REPRESENTATION ABOUT THE SUITABILITY OR
-#  ACCURACY OF THIS SOFTWARE OR DATA FOR ANY PURPOSE, AND MAKES NO WARRANTIES,
-#  EITHER EXPRESS OR IMPLIED, INCLUDING MERCHANTABILITY AND FITNESS FOR A
-#  PARTICULAR PURPOSE OR THAT THE USE OF THIS SOFTWARE OR DATA WILL NOT
-#  INFRINGE ANY THIRD PARTY PATENTS, COPYRIGHTS, TRADEMARKS, OR OTHER RIGHTS.
-#  THE SOFTWARE AND DATA ARE PROVIDED "AS IS".
-#
-#  This software and data are provided to enhance knowledge and encourage
-#  progress in the scientific community and are to be used only for research
-#  and educational purposes.  Any reproduction or use for commercial purpose
-#  is prohibited without the prior express written permission of The Jackson
-#  Laboratory.
-#
-# Copyright \251 1996, 1999, 2002, 2003 by The Jackson Laboratory
-#
-# All Rights Reserved
-#
-##########################################################################
--- gbcdnaload/GBcDNALoadRpt.sh
+++ gbcdnaload/GBcDNALoadRpt.sh
@@ -1,8 +1,5 @@
 #!/bin/csh
 #
-#  $Header$
-#  $Name$
-#
 #  GBcDNALoadRpt.sh
 ###########################################################################
 #
@@ -72,32 +69,3 @@ foreach RPT (*.py)
 end
 
 exit 0
-
-
-#  $Log$
-#  Revision 1.1.2.1  2004/07/09 15:18:05  dbm
-#  Initial version
-#
-#
-###########################################################################
-#
-# Warranty Disclaimer and Copyright Notice
-#
-#  THE JACKSON LABORATORY MAKES NO REPRESENTATION ABOUT THE SUITABILITY OR
-#  ACCURACY OF THIS SOFTWARE OR DATA FOR ANY PURPOSE, AND MAKES NO WARRANTIES,
-#  EITHER EXPRESS OR IMPLIED, INCLUDING MERCHANTABILITY AND FITNESS FOR A
-#  PARTICULAR PURPOSE OR THAT THE USE OF THIS SOFTWARE OR DATA WILL NOT
-#  INFRINGE ANY THIRD PARTY PATENTS, COPYRIGHTS, TRADEMARKS, OR OTHER RIGHTS.
-#  THE SOFTWARE AND DATA ARE PROVIDED "AS IS".
-#
-#  This software and data are provided to enhance knowledge and encourage
-#  progress in the scientific community and are to be used only for research
-#  and educational purposes.  Any reproduction or use for commercial purpose
-#  is prohibited without the prior express written permission of The Jackson
-#  Laboratory.
-#
-# Copyright \251 1996, 1999, 2002, 2004 by The Jackson Laboratory
-#
-# All Rights Reserved
-#
-##########################################################################
