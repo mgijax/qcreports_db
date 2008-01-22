1619c6b6c7f96b1daa755d292e249e20835b8505
Lori Corbani <Lori.Corbani@jax.org>
2008-01-22 16:23:03 +0000

    TR8733

--- mgd/GXD_ImageCheck.sql
+++ mgd/GXD_ImageCheck.sql
@@ -33,3 +33,32 @@ where p.paneLabel like "%,%" and p._Image_key = i._Image_key
 order by i.jnum
 go
 
+print ""
+print "Elsevier: where the J# in the copyright does not match the J# of the stub"
+print ""
+
+select distinct i.jnumID, i.mgiID
+from IMG_Image_View i,MGI_Note_Image_View n
+where i._MGIType_key = 8
+and n._NoteType_key = 1023
+and n.note like "reprinted with permission from elsevier%"
+and n.note not like "%" + i.jnumID + "%"
+and n._Object_key = i._Image_key
+order by i.jnumID
+go
+
+print ""
+print "non-Elsevier: the first author in the copyright does not match the first author in the paper"
+print ""
+
+select distinct i.jnumID, i.mgiID, r._primary
+from IMG_Image_View i,MGI_Note_Image_View n, BIB_Refs r
+where i._MGIType_key = 8
+and n._NoteType_key = 1023
+and n.note like "this image is from%"
+and i._Refs_key = r._Refs_key
+and n.note not like "%" + substring(r._primary, 1, charindex(" ", r._primary) - 1) + "%"
+and n._Object_key = i._Image_key
+order by i.jnumID
+go
+
