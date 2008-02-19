ecc83c0b5d3140419c1f170d4576ea1f01c6da27
Lori Corbani <Lori.Corbani@jax.org>
2008-02-19 17:23:05 +0000

    TR 8796

---
+++ mgd/PRB_Strain_Standard.sql
@@ -0,0 +1,12 @@
+
+print ""
+print "STOCK or 'semicolon' strains marked standard"
+print ""
+
+select s.strain
+from PRB_Strain s
+where s.standard = 1
+and (s.strain like 'STOCK%'
+or (s.strain like '%;%' and s.strain not like '%T(%;%)%'))
+go
+
