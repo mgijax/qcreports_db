02ed61daf2ba7da650150a51f9b0958a1c099fee
David Miers <dave.miers@jax.org>
2008-03-20 16:31:11 +0000

    Fixed bug in date comparison.  Look for last 30 days.

--- weekly/ALL_ESCellStrain.sql
+++ weekly/ALL_ESCellStrain.sql
@@ -5,8 +5,7 @@ print ""
 select distinct mgiID = ac.accID, e.cellLine, s.strain, s.modification_date
 from ALL_Allele a, ACC_Accession ac, ALL_CellLine e, PRB_Strain s
 where e._Strain_key = s._Strain_key
-and datepart(year, s.modification_date) = datepart(year, getdate())
-and datepart(month, s.modification_date) = datepart(month, getdate()) - 1
+and datediff(day, s.modification_date, getdate()) <= 30
 and e._CellLine_key != -2
 and e._CellLine_key = a._ESCellLine_key
 and a._Allele_key = ac._Object_key
