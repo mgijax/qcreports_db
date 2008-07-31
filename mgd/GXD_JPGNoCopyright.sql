fb85e01d572de0e17e4b7ea3cd4d3efcd8890f67
Lori Corbani <Lori.Corbani@jax.org>
2008-07-31 12:41:53 +0000

    TR9133

--- mgd/GXD_JPGNoCopyright.sql
+++ mgd/GXD_JPGNoCopyright.sql
@@ -3,18 +3,16 @@ print ""
 print "JPGs but no Copyright Statement"
 print ""
 
-select i.mgiID , jnumID
+select i.mgiID, i.jnumID
 from IMG_Image_View i 
 where i._MGIType_key = 8
 and i._ImageType_key = 1072158
 and i.xDim is not null
 and i.yDim is not null 
-and _Image_key not in
-(select distinct i._Image_key
-from IMG_Image i, MGI_Note mn, MGI_NoteChunk mnc
+and not exists
+(select 1 from MGI_Note mn
 where i._Image_key = mn._Object_key
 and mn._MGIType_key = 9
-and mn._NoteType_key = 1023
-and mn._Note_key = mnc._Note_key)
+and mn._NoteType_key = 1023)
 go
 
