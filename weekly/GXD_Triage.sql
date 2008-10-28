5c664d08181226d00ecc7d754847c227a3a5331c
Lori Corbani <Lori.Corbani@jax.org>
2008-10-28 13:55:52 +0000

    TR9349

--- weekly/GXD_Triage.sql
+++ weekly/GXD_Triage.sql
@@ -20,40 +20,15 @@ and a._DataSet_key = 1004
 
 create index idx1 on #triageA(_Refs_key)
 
+set nocount off
+
 print ""
 print "Papers Selected For Expression from %1! to %2!", @bdate, @edate
 print "by Data Set creation date"
 print ""
 
-set nocount off
-
 select v.jnumID, substring(v.short_citation, 1, 50)
 from #triageA t, BIB_All_View v
-where t._Refs_key = v._Refs_key
-order by v.jnum
-
-set nocount on
-
-select r._Refs_key, r.creation_date
-into #triageB
-from BIB_Refs r, BIB_DataSet_Assoc a
-where r.creation_date between @bdate and @edate
-and r._Refs_key = a._Refs_key
-and a._DataSet_key = 1004
-
-create index idx1 on #triageB(_Refs_key)
-
-set nocount off
-
-print ""
-print "Papers Selected For Expression from %1! to %2!", @bdate, @edate
-print "by Reference creation date"
-print ""
-
-set nocount off
-
-select v.jnumID, substring(v.short_citation, 1, 50)
-from #triageB t, BIB_All_View v
 where t._Refs_key = v._Refs_key
 order by v.jnum
 go
