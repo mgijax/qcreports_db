7f4362e7674c9a24a930b7a993f9d39bb96c827f
Lori Corbani <Lori.Corbani@jax.org>
2009-05-28 13:27:54 +0000

    TR9665

---
+++ mgd/GXD_GelBandError.sql
@@ -0,0 +1,25 @@
+
+/* 
+ * TR 9665
+ * Gel Bands that are connected to the correct Gel Row
+ * and Gel Lane but have a different Gel Lane/Assay key
+*/
+
+select substring(a1.accID,1,15), 
+substring(u1.login,1,10), 
+substring(a2.accID,1,15), 
+substring(u2.login,1,10), 
+r._Assay_key, l._Assay_key
+from GXD_GelBand b, GXD_GelRow r, GXD_GelLane l, ACC_Accession a1, ACC_Accession a2,
+MGI_User u1, MGI_User u2
+where b._GelLane_key = l._GelLane_key 
+and r._GelRow_key = b._GelRow_key 
+and r._Assay_key != l._Assay_key
+and r._Assay_key = a1._Object_key
+and a1._MGIType_key = 8
+and l._Assay_key = a2._Object_key
+and a2._MGIType_key = 8
+and a1._CreatedBy_key = u1._User_key
+and a2._CreatedBy_key = u2._User_key
+go
+
