30c9b34a66a1ebfa8f630a639d9fe9e1fbd9a390
Lori Corbani <Lori.Corbani@jax.org>
2010-07-01 12:14:54 +0000

    TR10208

--- mgd/MRK_GmNoGeneModel.sql
+++ mgd/MRK_GmNoGeneModel.sql
@@ -1,13 +1,15 @@
 
 print ""
-print "Gm Markers wihtout Gene Model Associations"
+print "Gm Markers without Gene Model Associations"
+print ""
+print "   where name begins 'predicted gene'"
+print "   and status = 'official' or 'interim'"
 print ""
 
-select m.symbol, a.accID
-from MRK_Marker m, ACC_Accession a
+select distinct m.symbol, a.accID, substring(h.name,1,50) "old name"
+from MRK_Marker m, ACC_Accession a, MRK_History h
 where m._Organism_key = 1
 and m._Marker_Status_key in (1,3)
-and m.symbol like 'gm%'
 and m.name like 'predicted gene%'
 and not exists (select 1 from SEQ_Marker_Cache c
 where m._Marker_key = c._Marker_key
@@ -17,6 +19,8 @@ and a._MGIType_key = 2
 and a._LogicalDB_Key = 1
 and a.prefixPart = "MGI:"
 and a.preferred = 1
+and m._Marker_key = h._Marker_key
+and m.name != h.name
 order by m.symbol
 go
 
