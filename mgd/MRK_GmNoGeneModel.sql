558cba8cbacd8d3c96deb7025c9f74c6bdac6807
Lori Corbani <Lori.Corbani@jax.org>
2010-07-01 15:06:15 +0000

    TR10208

--- mgd/MRK_GmNoGeneModel.sql
+++ mgd/MRK_GmNoGeneModel.sql
@@ -1,13 +1,10 @@
 
-print ""
-print "Gm Markers without Gene Model Associations"
-print ""
-print "   where name begins 'predicted gene'"
-print "   and status = 'official' or 'interim'"
-print ""
+set nocount on
+go
 
-select distinct a.accID, m.symbol, substring(h.name,1,50) "old name"
-from MRK_Marker m, ACC_Accession a, MRK_History h
+select distinct m._Marker_key, a.accID, m.symbol, m.name
+into #markers
+from MRK_Marker m, ACC_Accession a
 where m._Organism_key = 1
 and m._Marker_Status_key in (1,3)
 and m.name like 'predicted gene%'
@@ -19,8 +16,34 @@ and a._MGIType_key = 2
 and a._LogicalDB_Key = 1
 and a.prefixPart = "MGI:"
 and a.preferred = 1
-and m._Marker_key = h._Marker_key
+go
+
+select m._Marker_key, hm.symbol, name = substring(h.name,1,50)
+into #history
+from #markers m, MRK_History h, MRK_Marker hm
+where m._Marker_key = h._Marker_key
 and m.name != h.name
+and h._History_key = hm._Marker_key
+and m.symbol != hm.symbol
+go
+
+set nocount off
+go
+
+print ""
+print "Gm Markers without Gene Model Associations"
+print ""
+print "   where name begins 'predicted gene'"
+print "   and status = 'official' or 'interim'"
+print ""
+
+select m.accID, m.symbol, h.symbol "old symbol", h.name
+from #markers m, #history h
+where m._Marker_key = h._Marker_key
+union
+select m.accID, m.symbol, null, null
+from #markers m
+where not exists (select 1 from #history h where m._Marker_key = h._Marker_key)
 order by m.symbol
 go
 
