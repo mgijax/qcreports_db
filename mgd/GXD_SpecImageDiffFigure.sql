ae582da0da57befd14283e95a50275abd1d4f11c
Lori Corbani <Lori.Corbani@jax.org>
2010-07-01 17:30:11 +0000

    TR10370

--- mgd/GXD_SpecImageDiffFigure.sql
+++ mgd/GXD_SpecImageDiffFigure.sql
@@ -26,48 +26,10 @@ and r.jnumID not in ("J:101025", "J:103446", "J:107820", "J:122989", "J:125993",
                      "J:80501", "J:80502", "J:91257", "J:93300", "J:157819", "J:137887")
 go
 
-/* save count of imageresults by assay/specimen/resultkey */
-
-select h.*, imageresults = count(*)
-into #imageresults
-from #hasimage h
-group by h._Assay_key, h._Specimen_key, h._Result_key
+create index idx1 on #hasimage(_Assay_key)
+create index idx2 on #hasimage(_Specimen_key)
+create index idx3 on #hasimage(figureLabel)
 go
-
-create index idx1 on #imageresults(_Assay_key)
-create index idx2 on #imageresults(_Specimen_key)
-create index idx3 on #imageresults(imageresults)
-create index idx4 on #imageresults(figureLabel)
-go
-
-/* comparing the number of images in a assay/specimen/imageresults set */
-/* if the number of images is different, then print out the row */
-
-select distinct r.numericPart, r.jnumID, r.accID, r.specimen, r.figureLabel
-into #final
-from #imageresults r 
-where exists (select 1 from #imageresults r2
-where r._Assay_key = r2._Assay_key
-and r._Specimen_key = r2._Specimen_key
-and r.imageresults != r2.imageresults)
-go
-
-/* if the number of images are equal, then compare the figureLabels */
-
-insert into #final
-select distinct r.numericPart, r.jnumID, r.accID, r.specimen, r.figureLabel
-from #imageresults r 
-where exists (select 1 from #imageresults r2
-where r._Assay_key = r2._Assay_key
-and r._Specimen_key = r2._Specimen_key
-and r.imageresults = r2.imageresults)
-and exists (select 1 from #imageresults r2
-where r._Assay_key = r2._Assay_key
-and r._Specimen_key = r2._Specimen_key
-and r.figureLabel != r2.figureLabel)
-go
-
-/* if counts are equall, then check that figures are equal */
 
 set nocount off
 go
@@ -94,7 +56,14 @@ print "     J:157819 Blackshaw2"
 print "     J:137887 JaxCre"
 print ""
 
-select * from #final
-order by numericPart, accID
+/* compare by assay/specimen/figureLabels */
+
+select distinct r.jnumID, r.accID, r.specimen, r.figureLabel
+from #hasimage r 
+where exists (select 1 from #hasimage r2
+where r._Assay_key = r2._Assay_key
+and r._Specimen_key = r2._Specimen_key
+and r.figureLabel != r2.figureLabel)
+order by r.numericPart, r.accID
 go
 
