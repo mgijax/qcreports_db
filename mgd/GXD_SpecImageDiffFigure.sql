20cbe4cf6578c8b63e341628a9e627df3b4a1d2a
Lori Corbani <Lori.Corbani@jax.org>
2010-07-01 17:11:40 +0000

    TR10270

--- mgd/GXD_SpecImageDiffFigure.sql
+++ mgd/GXD_SpecImageDiffFigure.sql
@@ -1,10 +1,11 @@
 set nocount on
 go
 
-/* results with images */
+/* imageresults with images */
 
 select distinct a._Assay_key, aa.accID, r.jnumID, r.numericPart, 
        specimen = substring(s.specimenLabel,1,50), 
+       s._Specimen_key, i._Result_key,
        figureLabel = substring(ii.figureLabel, 1, 50)
 into #hasimage
 from GXD_Assay a, ACC_Accession aa, BIB_Citation_Cache r, 
@@ -22,8 +23,51 @@ and p._ImagePane_key = ip._ImagePane_key
 and ip._Image_key = ii._Image_key
 and r.jnumID not in ("J:101025", "J:103446", "J:107820", "J:122989", "J:125993",
                      "J:128545", "J:132267", "J:139122", "J:141291", "J:56618",
-                     "J:80501", "J:80502", "J:91257", "J:93300")
+                     "J:80501", "J:80502", "J:91257", "J:93300", "J:157819", "J:137887")
 go
+
+/* save count of imageresults by assay/specimen/resultkey */
+
+select h.*, imageresults = count(*)
+into #imageresults
+from #hasimage h
+group by h._Assay_key, h._Specimen_key, h._Result_key
+go
+
+create index idx1 on #imageresults(_Assay_key)
+create index idx2 on #imageresults(_Specimen_key)
+create index idx3 on #imageresults(imageresults)
+create index idx4 on #imageresults(figureLabel)
+go
+
+/* comparing the number of images in a assay/specimen/imageresults set */
+/* if the number of images is different, then print out the row */
+
+select distinct r.numericPart, r.jnumID, r.accID, r.specimen, r.figureLabel
+into #final
+from #imageresults r 
+where exists (select 1 from #imageresults r2
+where r._Assay_key = r2._Assay_key
+and r._Specimen_key = r2._Specimen_key
+and r.imageresults != r2.imageresults)
+go
+
+/* if the number of images are equal, then compare the figureLabels */
+
+insert into #final
+select distinct r.numericPart, r.jnumID, r.accID, r.specimen, r.figureLabel
+from #imageresults r 
+where exists (select 1 from #imageresults r2
+where r._Assay_key = r2._Assay_key
+and r._Specimen_key = r2._Specimen_key
+and r.imageresults = r2.imageresults)
+and exists (select 1 from #imageresults r2
+where r._Assay_key = r2._Assay_key
+and r._Specimen_key = r2._Specimen_key
+and r.figureLabel != r2.figureLabel)
+go
+
+/* if counts are equall, then check that figures are equal */
 
 set nocount off
 go
@@ -42,15 +86,15 @@ print "     J:132267"
 print "     J:139122 Manabe"
 print "     J:141291 Tamplin1"
 print "     J:56618"
-print "     J:80501 Gitton"
-print "     J:80502 Reymond"
-print "     J:91257 Gray"
-print "     J:93300 Blackshaw"
+print "     J:80501  Gitton"
+print "     J:80502  Reymond"
+print "     J:91257  Gray"
+print "     J:93300  Blackshaw"
+print "     J:157819 Blackshaw2"
+print "     J:137887 JaxCre"
 print ""
 
-select h.jnumID, h.accID, h.specimen, h.figureLabel
-from #hasimage h
-group by h._Assay_key, h.specimen having count(*) > 1
-order by h.numericPart, h.accID
+select * from #final
+order by numericPart, accID
 go
 
