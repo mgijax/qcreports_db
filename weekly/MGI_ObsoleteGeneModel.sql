5cb8e1ca5b9e2c404421893b17e1ff45830f37e4
Lori Corbani <Lori.Corbani@jax.org>
2010-09-28 11:30:28 +0000

    TR10356

--- weekly/MGI_ObsoleteGeneModel.sql
+++ weekly/MGI_ObsoleteGeneModel.sql
@@ -1,25 +1,35 @@
 
+set nocount on
+go
+
+select accID as "EntrezGene"
+into #tt1
+from ACC_Accession
+where _LogicalDB_key = 55
+and _MGIType_key = 2
+go
+
+create index idx1 on #tt1(EntrezGene)
+go
+
 print ""
 print "Obsolete Gene Models associated with Markers"
 print ""
-print "This report lists Markers that have an association to NCBI Gene Model (ldb 59)"
-print "but have no association to Entrez Gene (ldb 55)"
+print "This report lists Markers that have an association to NCBI Gene Model (ldb 59) but have no association to Entrez Gene (ldb 55)"
 print ""
 
-select a.accID, ma.accID, m.symbol
-from MRK_Marker m, ACC_Accession a, ACC_Accession ma
-where a._MGIType_key = 2
-and a._LogicalDB_key = 59
+set nocount off
+go
+
+select distinct smc.accID as "NCBI Gene Model", a.accID as "MGI ID", m.symbol
+from SEQ_Marker_Cache smc, ACC_Accession a, MRK_Marker m
+where smc._LogicalDB_key = 59
+and a._MGIType_key = 2
+and a.preferred = 1
+and a._LogicalDB_key = 1
 and a._Object_key = m._Marker_key
-and m._Organism_key = 1
-and a._Object_key = ma._Object_key
-and ma._MGIType_key = 2
-and ma._LogicalDB_key = 1
-and ma.preferred = 1
-and not exists (select 1 from ACC_Accession aa
-where a._Object_key = aa._Object_key
-and aa._MGIType_key = 2
-and aa._LogicalDB_key = 55)
+and smc._Marker_key = m._Marker_key
+and not exists (select * from #tt1 t1 where t1.EntrezGene = smc.accID)
 order by m.symbol
 go
 
