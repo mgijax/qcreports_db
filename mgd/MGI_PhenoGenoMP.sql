640b09c32c7d3bcdbaabb42c7403453ca92a310d
Lori Corbani <Lori.Corbani@jax.org>
2011-08-24 18:02:29 +0000

    TR10829

---
+++ mgd/MGI_PhenoGenoMP.sql
@@ -0,0 +1,16 @@
+
+print ""
+print "Genotype Alleles that Contain No Markers"
+print ""
+
+select distinct aa.symbol
+        from GXD_AlleleGenotype g, ALL_Allele aa
+        where g._Allele_key = aa._Allele_key
+        and not exists (select 1 from ACC_Accession ac
+        where g._Marker_key = ac._Object_key 
+        and ac._MGIType_key = 2 
+        and ac._LogicalDB_key = 1 
+        and ac.prefixPart = "MGI:" 
+        and ac.preferred = 1)
+go
+
