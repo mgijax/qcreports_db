7056b9b982edee972481b09ceec51eb50ef2cc80
Lori Corbani <Lori.Corbani@jax.org>
2012-04-05 11:43:10 +0000

    qcreports_db-5-0-0-1/TR11028

--- mgd/PRB_StrainByDate.sql
+++ mgd/PRB_StrainByDate.sql
@@ -19,6 +19,7 @@ print "Strains - Non Standard - Sorted by Creation Date"
 print "(do not include any strains flagged as needing review by Janan's load)"
 print""
 print "excludes /Mmcd from 06/07/2011"
+print "excludes created-by = 'strainautoload'"
 print ""
 
 (
@@ -27,6 +28,7 @@ substring(s.strain,1,125) as "strain", s.creation_date
 from PRB_Strain s
      LEFT OUTER JOIN PRB_Strain_Attribute_View t on (s._Strain_key = t._Strain_key)
 where s.standard = 0
+and s._CreatedBy_key != 1521
 and not exists (select 1 from ACC_Accession a
 where a._Object_key = s._Strain_key
 and a._MGIType_key = 10
@@ -42,6 +44,7 @@ from PRB_Strain s
      LEFT OUTER JOIN PRB_Strain_Attribute_View t on (s._Strain_key = t._Strain_key),
      ACC_Accession a
 where s.standard = 0
+and s._CreatedBy_key != 1521
 and a._Object_key = s._Strain_key
 and a._MGIType_key = 10
 and a._LogicalDB_key = 22
@@ -66,6 +69,7 @@ substring(s.strain,1,125) as "strain", s.creation_date
 from PRB_Strain s
      LEFT OUTER JOIN PRB_Strain_Attribute_View t on (s._Strain_key = t._Strain_key)
 where s.standard = 0
+and s._CreatedBy_key != 1521
 and not exists (select 1 from ACC_Accession a
 where a._Object_key = s._Strain_key
 and a._MGIType_key = 10
@@ -81,6 +85,7 @@ from PRB_Strain s
      LEFT OUTER JOIN PRB_Strain_Attribute_View t on (s._Strain_key = t._Strain_key),
      ACC_Accession a
 where s.standard = 0
+and s._CreatedBy_key != 1521
 and a._Object_key = s._Strain_key
 and a._MGIType_key = 10
 and a._LogicalDB_key = 22
