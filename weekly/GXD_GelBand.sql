c178a20539905165f99a4027a3b749a54713117e
Lori Corbani <Lori.Corbani@jax.org>
2012-05-14 15:00:17 +0000

    qcreports_db-5-0-0-4/TR11066/TR11035

--- weekly/GXD_GelBand.sql
+++ weekly/GXD_GelBand.sql
@@ -2,7 +2,7 @@
 set nocount on
 go
 
-select l._Assay_key
+select l._Assay_key, l._GelLane_key
 into #assay
 from GXD_GelLane l, GXD_GelBand b
 where l._GelControl_key = 1
@@ -17,14 +17,15 @@ set nocount off
 go
 
 print ''
-print 'GXD Blot w/ Control = No and Strength = Not Applicable'
+print 'GXD Blot w/ Control = No and Strength = Not Applicable for all bands'
 print ''
 
-select distinct a.jnum, a.mgiID, s._Assay_key
+select distinct a.jnum, a.mgiID
 from #assay s, GXD_Assay_View a
 where s._Assay_key = a._Assay_key
 and not exists (select 1 from GXD_GelLane l, GXD_GelBand b
 	where s._Assay_key = l._Assay_key
+	and s._GelLane_key = l._GelLane_key
 	and l._GelLane_key = b._GelLane_key
 	and b._Strength_key != -2)
 order by a.jnum
