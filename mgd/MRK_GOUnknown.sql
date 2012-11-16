0d329b2e6679f8caba0e649a20ec8c07b88a83d6
Lori Corbani <Lori.Corbani@jax.org>
2012-11-16 16:39:04 +0000

    qcreports_db-5-1-0-6/TR11218

--- mgd/MRK_GOUnknown.sql
+++ mgd/MRK_GOUnknown.sql
@@ -4,9 +4,13 @@ go
 
 /* GO Annotations to unknown terms */
 /* only include non-FANTOM references */
+/* and J: creation date >= Annotation creation date */
 
 select distinct substring(m.symbol,1,25) as symbol, m._Marker_key, r._Refs_key,
-substring(r.pubmedID,1,20) as pubmedID, rr.creation_date
+substring(r.jnumID,1,20) as jnumID,
+substring(r.pubmedID,1,20) as pubmedID, 
+convert(char(10), rr.creation_date, 101) as jnumDate,
+convert(char(10), a.creation_date, 101) as annotDate
 into #temp1
 from MRK_Marker m, VOC_Annot_View a, MRK_Reference r, BIB_Refs rr
 where m._Organism_key = 1 
@@ -16,6 +20,7 @@ and a.accID in ('GO:0008150', 'GO:0003674', 'GO:0005575')
 and m._Marker_key = r._Marker_key
 and r.pubmedID not in ('11217851', '12466851', '14621295', '11125038', '12466854', '12466855', '12693553')
 and r._Refs_key = rr._Refs_key
+and rr.creation_date >= a.creation_date
 go
 
 create index temp1_idx1 on #temp1(_Marker_key)
@@ -25,7 +30,7 @@ go
 
 /* grab the marker accession ids */
 
-select t._Marker_key, t.symbol, t._Refs_key, t.pubmedID, t.creation_date, substring(ma.accID,1,20) as mgiID
+select t._Marker_key, t.symbol, t._Refs_key, t.jnumID, t.pubmedID, jnumDate, annotDate, substring(ma.accID,1,20) as mgiID
 into #temp2
 from #temp1 t, ACC_Accession ma
 where t._Marker_key = ma._Object_key 
@@ -67,7 +72,7 @@ go
 /* for those without GO annotations */
 
 /* tag 1: for those with GO annotations only */
-select t._Marker_key, t.symbol, t.mgiID, t.pubmedID, t.creation_date, '1' as tag
+select t._Marker_key, t.symbol, t.mgiID, t.jnumID, t.pubmedID, t.jnumDate, t.annotDate, '1' as tag
 into #temp3
 from #temp2 t
 where exists (select 1 from BIB_DataSet_Assoc a, BIB_DataSet d
@@ -89,7 +94,7 @@ go
 /* tag 2: for those with GO and A&P annotations only */
 /* and not in tag 1 */
 insert into #temp3
-select t._Marker_key, t.symbol, t.mgiID, t.pubmedID, t.creation_date, '2' as tag
+select t._Marker_key, t.symbol, t.mgiID, t.jnumID, t.pubmedID, t.jnumDate, t.annotDate, '2' as tag
 from #temp2 t
 where not exists (select 1 from #temp3 t3 
 where t.symbol = t3.symbol
@@ -118,7 +123,7 @@ go
 /* tag 3: any GO annotations */
 /* and not in tag 1 or 2 */
 insert into #temp3
-select t._Marker_key, t.symbol, t.mgiID, t.pubmedID, t.creation_date, '3' as tag
+select t._Marker_key, t.symbol, t.mgiID, t.jnumID, t.pubmedID, t.jnumDate, t.annotDate, '3' as tag
 from #temp2 t
 where not exists (select 1 from #temp3 t3 
 where t.symbol = t3.symbol
@@ -158,10 +163,13 @@ go
 
 print ''
 print 'All genes with ''unknown'' annotations with new indexed literature'
+print '(J: creation date >= Annotation creation date)'
 print 'and if reference is selected for GO and ''not used'' for any GO annotation'
 print '(excludes FANTOM papers 11217851 and 12466851, and 14621295, 11125038, 12466854, 12466855, and 12693553)'
 
 select 'Number of unique MGI Gene IDs:  ', count(distinct mgiID) from #temp4
+union
+select 'Number of unique MGI Gene IDs associated with OMIM:', count(distinct mgiID) from #temp4 where hasOMIM = 'Y'
 union
 select 'Number of total rows:  ', count(*) from #temp4
 go
@@ -175,8 +183,8 @@ print ''
 set nocount off
 go
 
-select symbol, mgiID, pubmedID, tag, hasOMIM, creation_date
+select distinct symbol, mgiID, jnumID, pubmedID, tag, hasOMIM, jnumDate, annotDate
 from #temp4 
-order by creation_date, symbol, tag
+order by symbol, tag
 go
 
