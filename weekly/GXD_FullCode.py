9d4fea9c38303bf41adda6aa86ea8db3ecc13e48
Lori Corbani <Lori.Corbani@jax.org>
2012-12-24 18:11:01 +0000

    TR11206/re-open Dev Biol and Development

--- weekly/GXD_FullCode.py
+++ weekly/GXD_FullCode.py
@@ -15,6 +15,9 @@
 #
 # History:
 #
+# lec	12/24/2012
+#	- TR11206/re-open Dev Biol and Development
+#
 # lec	06/11/2012
 #	- TR11105/retire Dev Biol and Development
 #
@@ -61,7 +64,7 @@ TAB = reportlib.TAB
 PAGE = reportlib.PAGE
 
 # The list of journals; one report for the set of papers within each sub list
-journals = [ ['Mech Dev'], ['Gene Expr Patterns', 'Brain Res Gene Expr Patterns'] ]
+journals = [ ['Mech Dev'], ['Gene Expr Patterns', 'Brain Res Gene Expr Patterns' ] , ['Dev Biol'], ['Development'] ]
 
 def processJournal(jList, fileName):
 
