febbbd99d65d5a697d0dfd57a88c2860b50e4666
Olin Blodgett <Olin.Blodgett@jax.org>
2013-11-14 15:28:30 +0000

    Added Checks for US51

---
+++ weekly/GXD_EMAPS_MappingChecks.sql
@@ -0,0 +1,61 @@
+
+print ''
+print 'Duplicate MGI and EMAPS entries in the Mapping'
+print ''
+
+select accId, emapsId from MGI_EMAPS_Mapping group by accid + emapsid having count(*) > 1
+go
+
+print ''
+print 'MGI Id''s that have no entry in the Accession Table'
+print ''
+
+select mem.accId from MGI_EMAPS_Mapping mem LEFT OUTER JOIN ACC_Accession acc on (mem.accId = acc.accID) where acc.accId is NULL
+go
+
+print ''
+print 'MGI Id''s that do not map to GXD Structure terms'
+print ''
+
+select mem.accId, ty1.tableName from MGI_EMAPS_Mapping mem, ACC_MGIType ty1, ACC_Accession acc LEFT OUTER JOIN GXD_Structure gs on (acc._Object_key = gs._Structure_key) where mem.accId = acc.accId and gs._Structure_key is NULL and acc._MGIType_key = ty1._MGIType_key
+go
+
+print ''
+print 'EMAPS Id''s that have no entry in the Accession Table'
+print ''
+
+select acc.accId from MGI_EMAPS_Mapping mem LEFT OUTER JOIN ACC_Accession acc on (mem.emapsId = acc.accID) where acc.accId is NULL
+go
+
+print ''
+print 'MGI Id''s that do not map to GXD Structure terms'
+print ''
+
+select mem.emapsId, ty1.tableName from MGI_EMAPS_Mapping mem, ACC_MGIType ty1, ACC_Accession acc LEFT OUTER JOIN VOC_Term voc on (acc._Object_key = voc._Term_key) where mem.emapsId = acc.accId and voc._Term_key is NULL and acc._MGIType_key = ty1._MGIType_key
+go
+
+print ''
+print 'Find Duplicate MGI Id in the Mapping table for AD terms in the Structure table'
+print ''
+
+select mem.accId from GXD_Structure gs, ACC_Accession acc, MGI_EMAPS_Mapping mem where gs._Structure_key = acc._Object_key and acc._MGIType_key = 38 and mem.accId = acc.accId group by mem.accId having count(mem.accId) > 1
+go
+
+print ''
+print 'Find AD Terms that do not map to EMAPS Id''s'
+print ''
+select acc.accId from GXD_Structure gs, ACC_Accession acc LEFT OUTER JOIN MGI_EMAPS_Mapping mem on (acc.accId = mem.accId) where gs._Structure_key = acc._Object_key and acc._MGIType_key = 38 and acc.prefixPart = "MGI:" and mem.accId is NULL
+go
+
+print ''
+print 'Find AD Term names and EMAPS Term names that do not match'
+print ''
+select mem.accId, gsn.structure, voct.term, mem.emapsId from GXD_Structure gs, GXD_TheilerStage gts, GXD_StructureName gsn, ACC_Accession aa1, ACC_MGIType ty1, MGI_EMAPS_Mapping mem, ACC_Accession aa2, ACC_MGIType ty2, VOC_Term voct, VOC_Term_EMAPS vte where gs._Structure_key = aa1._Object_key and gs._StructureName_key = gsn._StructureName_key and gs._Stage_key = gts._Stage_key and aa1.accId = mem.accID and aa1._MGIType_key = ty1._MGIType_key and ty1.tableName = 'GXD_Structure' and mem.emapsID = aa2.accId and aa2._MGIType_key = ty2._MGIType_key and ty2.tableName = 'VOC_Term' and aa2._Object_key = voct._Term_key and voct._Term_key = vte._Term_key and (case when voct.term = gsn.structure then 1 else 0 end) = 0
+go
+
+print ''
+print 'Find AD Stage and EMAPS Stage that are not the same'
+print ''
+
+select mem.accId, gts.stage, mem.emapsId, vte.stage from GXD_Structure gs, GXD_TheilerStage gts, GXD_StructureName gsn, ACC_Accession aa1, ACC_MGIType ty1, MGI_EMAPS_Mapping mem, ACC_Accession aa2, ACC_MGIType ty2, VOC_Term voct, VOC_Term_EMAPS vte where gs._Structure_key = aa1._Object_key and gs._StructureName_key = gsn._StructureName_key and gs._Stage_key = gts._Stage_key and aa1.accId = mem.accID and aa1._MGIType_key = ty1._MGIType_key and ty1.tableName = 'GXD_Structure' and mem.emapsID = aa2.accId and aa2._MGIType_key = ty2._MGIType_key and ty2.tableName = 'VOC_Term' and aa2._Object_key = voct._Term_key and voct._Term_key = vte._Term_key and (case when convert(INT, vte.stage) = gts.stage then 1 else 0 end) = 0
+go
