cc4387d493f299988a7e6598909ed878abc81949
Olin Blodgett <Olin.Blodgett@jax.org>
2014-01-08 17:15:16 +0000

    Added checks 2,3,4 for story 153

--- weekly/GXD_EMAPS_MappingChecks.sql
+++ weekly/GXD_EMAPS_MappingChecks.sql
@@ -35,27 +35,26 @@ select mem.emapsId, ty1.tableName from MGI_EMAPS_Mapping mem, ACC_MGIType ty1, A
 go
 
 print ''
-print 'Duplicate MGI Id in the Mapping table for one AD term in the Structure table'
+print 'EMAPS terms that are mapped to two AD terms'
+print ''
+
+print ''
+print 'Check 1 - AD terms that are mapped to two EMAPS terms'
 print ''
 
 select mem.accId from GXD_Structure gs, ACC_Accession acc, MGI_EMAPS_Mapping mem where gs._Structure_key = acc._Object_key and acc._MGIType_key = 38 and mem.accId = acc.accId group by mem.accId having count(mem.accId) > 1
 go
 
 print ''
-print 'AD Terms that do not map to EMAPS Id''s'
-print ''
-select acc.accId, gsn.structure from GXD_Structure gs, GXD_StructureName gsn, ACC_Accession acc LEFT OUTER JOIN MGI_EMAPS_Mapping mem on (acc.accId = mem.accId) where gs._Structure_key = acc._Object_key and gs._StructureName_key = gsn._StructureName_key and acc._MGIType_key = 38 and acc.prefixPart = "MGI:" and mem.accId is NULL
-go
-
-print ''
-print 'AD Term names and EMAPS Term names that do not match'
-print ''
-select mem.accId, gsn.structure, voct.term, mem.emapsId from GXD_Structure gs, GXD_TheilerStage gts, GXD_StructureName gsn, ACC_Accession aa1, ACC_MGIType ty1, MGI_EMAPS_Mapping mem, ACC_Accession aa2, ACC_MGIType ty2, VOC_Term voct, VOC_Term_EMAPS vte where gs._Structure_key = aa1._Object_key and gs._StructureName_key = gsn._StructureName_key and gs._Stage_key = gts._Stage_key and aa1.accId = mem.accID and aa1._MGIType_key = ty1._MGIType_key and ty1.tableName = 'GXD_Structure' and mem.emapsID = aa2.accId and aa2._MGIType_key = ty2._MGIType_key and ty2.tableName = 'VOC_Term' and aa2._Object_key = voct._Term_key and voct._Term_key = vte._Term_key and (case when voct.term = gsn.structure then 1 else 0 end) = 0
-go
-
-print ''
 print 'AD Stage and EMAPS Stage that are not the same'
 print ''
 
-select mem.accId, gts.stage, mem.emapsId, vte.stage from GXD_Structure gs, GXD_TheilerStage gts, GXD_StructureName gsn, ACC_Accession aa1, ACC_MGIType ty1, MGI_EMAPS_Mapping mem, ACC_Accession aa2, ACC_MGIType ty2, VOC_Term voct, VOC_Term_EMAPS vte where gs._Structure_key = aa1._Object_key and gs._StructureName_key = gsn._StructureName_key and gs._Stage_key = gts._Stage_key and aa1.accId = mem.accID and aa1._MGIType_key = ty1._MGIType_key and ty1.tableName = 'GXD_Structure' and mem.emapsID = aa2.accId and aa2._MGIType_key = ty2._MGIType_key and ty2.tableName = 'VOC_Term' and aa2._Object_key = voct._Term_key and voct._Term_key = vte._Term_key and (case when convert(INT, vte.stage) = gts.stage then 1 else 0 end) = 0
+select mem.accId as "AD ID", gts.stage "AD TS", gs.printName as "AD Printname", mem.emapsId as "EMAPS ID", vte.stage as "EMAPS TS", voct.term as "EMAPS Term Name" from GXD_Structure gs, GXD_TheilerStage gts, GXD_StructureName gsn, ACC_Accession aa1, ACC_MGIType ty1, MGI_EMAPS_Mapping mem, ACC_Accession aa2, ACC_MGIType ty2, VOC_Term voct, VOC_Term_EMAPS vte where gs._Structure_key = aa1._Object_key and gs._StructureName_key = gsn._StructureName_key and gs._Stage_key = gts._Stage_key and aa1.accId = mem.accID and aa1._MGIType_key = ty1._MGIType_key and ty1.tableName = 'GXD_Structure' and mem.emapsID = aa2.accId and aa2._MGIType_key = ty2._MGIType_key and ty2.tableName = 'VOC_Term' and aa2._Object_key = voct._Term_key and voct._Term_key = vte._Term_key and (case when convert(INT, vte.stage) = gts.stage then 1 else 0 end) = 0
 go
+
+print ''
+print 'AD Terms that do not map to EMAPS Id''s'
+print ''
+select acc.accId as "AD ID", gts.stage as "AD TS", vacc.annotCount as "Annotation Count, gs.printname from GXD_Structure gs, GXD_TheilerStage gts, VOC_Annot_Count_Cache vacc, GXD_StructureName gsn, ACC_Accession acc LEFT OUTER JOIN MGI_EMAPS_Mapping mem on (acc.accId = mem.accId) where gs._Structure_key = acc._Object_key and gs._Stage_key = gts._Stage_key and gs._StructureName_key = gsn._StructureName_key and acc._MGIType_key = 38 and acc.prefixPart = "MGI:" and mem.accId is NULL and gs.printname not like '%obsolete%' and gs._Structure_key = vacc._Term_key and vacc.annotType = 'AD'
+go
+
