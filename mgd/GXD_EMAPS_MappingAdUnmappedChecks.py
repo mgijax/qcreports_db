6c137c3723acf20b858c64344ede70dc3155e1ed
Olin Blodgett <Olin.Blodgett@jax.org>
2014-01-22 18:27:03 +0000

    qcreports_db-5-1-7-1

---
+++ mgd/GXD_EMAPS_MappingAdUnmappedChecks.py
@@ -0,0 +1,128 @@
+#!/usr/local/bin/python
+
+'''
+#
+# GXD_EMAPS_MappingAdUnmappedChecks.sql  /01/10/2014
+#
+# Report:
+#       
+#	AD Term names and EMAPS Term Names that do not match
+#
+# Fields:
+#	1. AD MGI ID
+#	2. AD Theiler Stage
+#	3. Annotation Count
+#	4. Print Name
+# 
+# Usage:
+#       GXD_EMAPS_MappingAdUnmappedChecks.py
+#
+# History:
+#
+# oblod	01/10/2014
+#	- TR11468 GXD Anatomy Project 
+#
+'''
+ 
+import sys 
+import os
+import string
+import reportlib
+
+try:
+    if os.environ['DB_TYPE'] == 'postgres':
+        import pg_db
+        db = pg_db
+        db.setTrace()
+        db.setAutoTranslateBE()
+    else:
+        import db
+except:
+    import db
+
+
+CRT = reportlib.CRT
+SPACE = reportlib.SPACE
+TAB = reportlib.TAB
+PAGE = reportlib.PAGE
+
+#
+# Main
+#
+
+title = 'Check 9 - AD Terms that do not map to EMAPS Ids'
+fp = reportlib.init(sys.argv[0], title = title, outputdir = os.environ['QCOUTPUTDIR'])
+
+fp.write('AD MGI ID%sAD Theiler Stage%sAnnotation Count%sPrint Name%s' % (TAB, TAB, TAB, CRT))
+
+results = db.sql('''
+
+	select
+		gs._Structure_key,
+		count(gs._Structure_key) as "SCount"
+	into #tmp_olin
+	from
+		GXD_Structure gs,
+		ACC_Accession acc
+	LEFT OUTER JOIN
+		MGI_EMAPS_Mapping mem on (acc.accId = mem.accId)
+	LEFT OUTER JOIN
+		GXD_Expression ge on (acc._Object_key = ge._Structure_key and isForGXD = 1)
+	where
+		gs._Structure_key = acc._Object_key and
+		acc._MGIType_key = 38 and
+		acc.prefixPart = "MGI:" and
+		mem.accId is NULL and
+		ge._Structure_key is not NULL
+	group by
+		gs._Structure_key
+union
+	select
+		gs._Structure_key,
+		0 as "SCount"
+	from
+		GXD_Structure gs,
+		ACC_Accession acc
+	LEFT OUTER JOIN
+		MGI_EMAPS_Mapping mem on (acc.accId = mem.accId)
+	LEFT OUTER JOIN
+		GXD_Expression ge on (acc._Object_key = ge._Structure_key and isForGXD = 1)
+	where
+		gs._Structure_key = acc._Object_key and
+		acc._MGIType_key = 38 and
+		acc.prefixPart = "MGI:" and
+		mem.accId is NULL and
+		ge._Structure_key is NULL
+	group by
+		gs._Structure_key
+       ''', 'auto')
+
+results = db.sql('''
+select
+	acc.accId,
+	gts.stage,
+	olin.Scount,
+	gs.printname
+from
+	#tmp_olin olin,
+	GXD_Structure gs,
+	ACC_Accession acc,
+	GXD_TheilerStage gts
+where
+	olin._Structure_key = gs._Structure_key and
+	gs._Structure_key = acc._Object_key and
+	gs._Stage_key = gts._Stage_key and
+	acc._MGIType_key = 38 and
+	acc.prefixPart = "MGI:"
+order by
+	olin.SCount desc, gs.printname
+       ''', 'auto')
+
+for r in results:
+    fp.write('%s%s%s%s%s%s%s%s' % (r['accId'], TAB, r['stage'], TAB, r['Scount'], TAB, r['printname'], CRT))
+
+fp.write('\n(%d rows affected)\n' % (len(results)))
+
+results = db.sql(''' drop table #tmp_olin ''', 'auto')
+
+reportlib.finish_nonps(fp)	# non-postscript file
---
+++ mgd/GXD_EMAPS_MappingChecks.sql
@@ -0,0 +1,233 @@
+
+print ''
+print 'Check 1'
+print 'Duplicate MGI and EMAPS entries in the Mapping Table'
+print ''
+
+select
+	accId,
+	emapsId
+from
+	MGI_EMAPS_Mapping
+group by
+	accid + emapsid
+having
+	count(*) > 1
+go
+
+print ''
+print 'Check 2'
+print 'MGI Id''s that do not have an entry in the Accession Table'
+print ''
+
+select
+	mem.accId
+from
+	MGI_EMAPS_Mapping mem
+LEFT OUTER JOIN
+	ACC_Accession acc on (mem.accId = acc.accID)
+where
+	acc.accId is NULL
+go
+
+print ''
+print 'Check 3'
+print 'MGI Id''s that do not map to GXD Structures in the GXD_Structure table'
+print 'May have multiple entries if MGI Id maps to other objects'
+print ''
+
+select
+	distinct
+	mem.accId,
+	substring(ty1.tableName, 1, 20) as "Object Name"
+from
+	MGI_EMAPS_Mapping mem,
+	ACC_MGIType ty1,
+	ACC_Accession acc
+LEFT OUTER JOIN
+	GXD_Structure gs on (acc._Object_key = gs._Structure_key and acc._MGIType_key = 38)
+where
+	mem.accId = acc.accId and
+	gs._Structure_key is NULL and
+	acc._MGIType_key = ty1._MGIType_key
+
+print ''
+print 'Check 4'
+print 'Id''s in the EMAPS Field that do not have an entry in the Accession Table'
+print ''
+
+select
+	mem.emapsId
+from
+	MGI_EMAPS_Mapping mem
+LEFT OUTER JOIN
+	ACC_Accession acc on (mem.emapsId = acc.accID)
+where
+	acc.accId is NULL
+go
+
+print ''
+print 'Check 5'
+print 'IDs in the EMAPS field that are not valid EMAPS IDs'
+print ''
+
+select
+	distinct
+	mem.emapsId,
+	substring(ty1.tableName, 1, 25) as "Object type"
+from
+	MGI_EMAPS_Mapping mem,
+	ACC_MGIType ty1,
+	ACC_Accession acc
+LEFT OUTER JOIN
+	VOC_Term_EMAPS vte on (acc._Object_key = vte._Term_key)
+where
+	mem.emapsId = acc.accId and
+	vte._Term_key is NULL and
+	acc._MGIType_key = ty1._MGIType_key
+go
+
+print ''
+print 'Check 6'
+print 'EMAPS terms that are mapped to two AD terms'
+print ''
+
+select
+	substring(mem.emapsId, 1, 15) as "EMAPS ID",
+	substring(vte.stage, 1, 5) as "EMAPS TS",
+	substring(voct.term, 1, 35) as "EMAPS Term Name",
+	substring(mem.accId, 1, 15) as "AD ID",
+	convert(varchar(5), gts.stage) as "AD TS",
+	substring(gs.printName, 1,35) as "AD Printname"
+from
+	GXD_Structure gs,
+	GXD_TheilerStage gts,
+	GXD_StructureName gsn,
+	ACC_Accession aa1,
+	ACC_MGIType ty1,
+	MGI_EMAPS_Mapping mem,
+	ACC_Accession aa2,
+	ACC_MGIType ty2,
+	VOC_Term voct,
+	VOC_Term_EMAPS vte
+where
+	gs._Structure_key = aa1._Object_key and
+	gs._StructureName_key = gsn._StructureName_key and
+	gs._Stage_key = gts._Stage_key and
+	aa1.accId = mem.accID and
+	aa1._MGIType_key = ty1._MGIType_key and
+	ty1.tableName = 'GXD_Structure' and
+	mem.emapsID = aa2.accId and
+	aa2._MGIType_key = ty2._MGIType_key and
+	ty2.tableName = 'VOC_Term' and
+	aa2._Object_key = voct._Term_key and
+	voct._Term_key = vte._Term_key and
+	mem.emapsId in (
+		select
+			mem.emapsId
+		from
+			MGI_EMAPS_Mapping mem,
+			ACC_Accession acc,
+			VOC_Term voct
+		where
+			acc.accID = mem.emapsId and
+			acc._Object_key = voct._Term_key and
+			_MGIType_key = 13
+		group by
+			mem.emapsId
+		having
+			count(mem.emapsId) > 1
+	)
+go
+
+print ''
+print 'Check 7'
+print 'AD terms that are mapped to two EMAPS terms'
+print ''
+
+select
+	substring(mem.accId, 1, 15) as "AD ID",
+	convert(varchar(5), gts.stage)  "AD TS",
+	substring(gs.printName, 1,35) as "AD Printname",
+	substring(mem.emapsId, 1, 15) as "EMAPS ID",
+	substring(vte.stage, 1, 5) as "EMAPS TS",
+	substring(voct.term, 1,35) as "EMAPS Term Name"
+from
+	GXD_Structure gs,
+	GXD_TheilerStage gts,
+	GXD_StructureName gsn,
+	ACC_Accession aa1,
+	ACC_MGIType ty1,
+	MGI_EMAPS_Mapping mem,
+	ACC_Accession aa2,
+	ACC_MGIType ty2,
+	VOC_Term voct,
+	VOC_Term_EMAPS vte
+where
+	gs._Structure_key = aa1._Object_key and
+	gs._StructureName_key = gsn._StructureName_key and
+	gs._Stage_key = gts._Stage_key and
+	aa1.accId = mem.accID and
+	aa1._MGIType_key = ty1._MGIType_key and
+	ty1.tableName = 'GXD_Structure' and
+	mem.emapsID = aa2.accId and
+	aa2._MGIType_key = ty2._MGIType_key and
+	ty2.tableName = 'VOC_Term' and
+	aa2._Object_key = voct._Term_key and
+	voct._Term_key = vte._Term_key and
+	mem.accId in (
+		select
+			mem.accId
+		from
+			GXD_Structure gs,
+			ACC_Accession acc,
+			MGI_EMAPS_Mapping mem
+		where
+			gs._Structure_key = acc._Object_key and
+			acc._MGIType_key = 38 and
+			mem.accId = acc.accId
+		group by
+			mem.accId
+		having
+			count(mem.accId) > 1
+	)
+go
+
+print ''
+print 'Check 8'
+print 'AD Stage and EMAPS Stage that are not the same'
+print ''
+
+select
+	substring(mem.accId, 1, 15) as "AD ID",
+	convert(varchar(5), gts.stage) "AD TS",
+	substring(gs.printName, 1,35) as "AD Printname",
+	substring(mem.emapsId, 1, 15) as "EMAPS ID",
+	substring(vte.stage, 1, 5) as "EMAPS TS",
+	substring(voct.term, 1,35) as "EMAPS Term Name"
+from
+	GXD_Structure gs,
+	GXD_TheilerStage gts,
+	GXD_StructureName gsn,
+	ACC_Accession aa1,
+	ACC_MGIType ty1,
+	MGI_EMAPS_Mapping mem,
+	ACC_Accession aa2,
+	ACC_MGIType ty2,
+	VOC_Term voct,
+	VOC_Term_EMAPS vte
+where
+	gs._Structure_key = aa1._Object_key and
+	gs._StructureName_key = gsn._StructureName_key and
+	gs._Stage_key = gts._Stage_key and
+	aa1.accId = mem.accID and
+	aa1._MGIType_key = ty1._MGIType_key and
+	ty1.tableName = 'GXD_Structure' and
+	mem.emapsID = aa2.accId and
+	aa2._MGIType_key = ty2._MGIType_key and
+	ty2.tableName = 'VOC_Term' and
+	aa2._Object_key = voct._Term_key and
+	voct._Term_key = vte._Term_key and
+	(case when convert(INT, vte.stage) = gts.stage then 1 else 0 end) = 0
+go
+
