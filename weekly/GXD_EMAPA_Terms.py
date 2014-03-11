f79053e8cd4474c8d231e4f4a6d965ad04c9c64e
CVS 2 Git Conversion <mgiadmin@jax.org>
2014-03-11 14:47:04 +0000

    This commit was manufactured by cvs2svn to create tag 'qcreports_db-5-1-8-1'.
    Sprout from master 2014-03-06 17:15:41 UTC MGI Admin <mgiadmin@jax.org> 'TR11515/allele stuff'
    Cherrypick from master 2014-03-11 14:47:03 UTC MGI Admin <mgiadmin@jax.org> 'TR11597/GXD reports':
        weekly/GXD_EMAPA_Terms.py
        weekly/GXD_EMAPS_Terms.py

---
+++ weekly/GXD_EMAPA_Terms.py
@@ -0,0 +1,82 @@
+#!/usr/local/bin/python
+
+'''
+#
+# Report:
+#       See TR11578 
+#
+# History:
+#
+# lec	03/11/2014
+#	- TR11597/make this a weekly report
+#
+# sc	01/28/2014
+#	- created
+#
+'''
+ 
+import sys 
+import os
+import db
+import reportlib
+
+CRT = reportlib.CRT
+SPACE = reportlib.SPACE
+TAB = reportlib.TAB
+PAGE = reportlib.PAGE
+
+#
+# Main
+#
+
+db.useOneConnection(1)
+
+fp = reportlib.init(sys.argv[0], 'EMAPA Terms', os.environ['QCOUTPUTDIR'], printHeading = None)
+
+emapaSynDict = {} # {termKey:[list of synonyms], ...}
+# EMAPA
+# EMAPA synonyms
+results = db.sql('''select t._Term_key, s.synonym
+    from VOC_Term t, MGI_Synonym s
+    where t._Vocab_key = 90
+    and t._Term_key = s._Object_key
+    and s._MGIType_key = 13''', 'auto')
+
+for r in results:
+    termKey = r['_Term_key']
+    synonym = r['synonym']
+    if not emapaSynDict.has_key(termKey):
+	emapaSynDict[termKey] = []
+    emapaSynDict[termKey].append(synonym)
+
+# get EMAPA ID, TS and term name 
+results = db.sql('''select t._Term_key, t.term, a.accid as emapaID, 
+	emapa.startStage, emapa.endStage
+    from VOC_Term t, ACC_Accession a, VOC_Term_EMAPA emapa
+    where t._Vocab_key = 90
+    and t._Term_key = a._Object_key
+    and a._MGIType_key = 13
+    and a._LogicalDB_key = 169
+    and t._Term_key = emapa._Term_key
+    order by t.term''', 'auto')
+
+numResults = len(results)
+# write report
+fp.write('EMAPA ID%sTerm%sStage Range%sSynonyms%s' % (TAB, TAB, TAB, CRT))
+for r in results:
+    termKey = r['_Term_key']
+    term = r['term']
+    emapaId = r['emapaID']
+    start = r['startStage']
+    end = r['endStage']
+    range = '%s, %s' % (start, end)
+    synList = []
+    if emapaSynDict.has_key(termKey):
+	synList =  emapaSynDict[termKey]
+    synString = '|'.join(synList)
+    fp.write('%s%s%s%s%s%s%s%s' % (emapaId, TAB, term, TAB, range, TAB, synString, CRT))
+fp.write('%sTotal results: %s' % (CRT, numResults))
+
+reportlib.finish_nonps(fp)	# non-postscript file
+db.useOneConnection(0)
+
