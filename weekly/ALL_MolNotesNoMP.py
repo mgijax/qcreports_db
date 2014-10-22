45bff69389bd5fe702571af618267e77b1958b9a
MGI Admin <mgiadmin@jax.org>
2014-10-22 16:26:23 +0000

    TR11750/postres complient

--- weekly/ALL_MolNotesNoMP.py
+++ weekly/ALL_MolNotesNoMP.py
@@ -27,6 +27,9 @@
 #
 # History:
 #
+# lec   10/22/2014
+#       - TR11750/postres complient
+#
 # 07/17/2014	lec
 #	- TR11544/exclude J:204812
 #
@@ -127,7 +130,7 @@ db.sql('''
 db.sql('create index idx1 on #alleles(_Allele_key)', None)
 
 results = db.sql('''
-	select distinct a._Allele_key, a1.accID 
+	select distinct a._Allele_key, a1.accID , a1.numericPart
     	from #alleles a, MGI_Reference_Assoc r, ACC_Accession a1 
     	where a._Allele_key = r._Object_key 
     	and r._MGIType_key = 11 
