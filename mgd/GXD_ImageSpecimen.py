ea846a8f081463f9b2e71c94fd65bb65f8d1d761
MGI Admin <mgiadmin@jax.org>
2014-10-23 15:59:58 +0000

    TR11750/postres complient

--- mgd/GXD_ImageSpecimen.py
+++ mgd/GXD_ImageSpecimen.py
@@ -16,6 +16,9 @@
 #
 # History:
 #
+# lec   10/23/2014
+#       - TR11750/postres complient
+#
 # lec   03/11/2014
 #       - TR11597/sort by mgiID desc
 #
@@ -48,8 +51,7 @@ PAGE = reportlib.PAGE
 
 fp = reportlib.init(sys.argv[0], outputdir = os.environ['QCOUTPUTDIR'])
 
-cmds = []
-cmds.append('''
+db.sql('''
     select distinct s._Assay_key,
                     s._Specimen_key,
                     iri._ImagePane_key,
@@ -71,20 +73,31 @@ cmds.append('''
           r._Specimen_key = s._Specimen_key and
           s._Assay_key = a._Assay_key and
           a._AssayType_key in (1,2,3,4,5,6,8,9) and
-          a._Refs_key not in (92242)''')
+          a._Refs_key not in (92242)
+	  ''', None)
 
-cmds.append('''
+db.sql('create index idx1_assay on #assays(_Assay_key)', None)
+
+db.sql('''
     select *
     into #final
-    from #assays
-    group by _Assay_key, _ImagePane_key
-    having count(*) > 1''')
+    from #assays a1
+    where exists (select 1 from #assays a2
+	where a2._assay_key = a1._assay_key and a2._imagepane_key = a1._imagepane_key
+		and a2._specimen_key != a1._specimen_key
+	) 
+    ''', None)
 
-cmds.append('''
+db.sql('create index idx1_final on #final(_Image_key)', None)
+db.sql('create index idx2_final on #final(_Assay_key)', None)
+db.sql('create index idx3_final on #final(_Refs_key)', None)
+
+results = db.sql('''
     select distinct assayID = a.accID,
            imageID = i.accID,
            jNumber = r.accID,
-           label = substring(f.figureLabel + f.paneLabel, 1, 50)
+           figureLabel = f.figureLabel,
+	   paneLabel = f.paneLabel
     from #final f,
          ACC_Accession i,
          ACC_Accession a,
@@ -104,11 +117,10 @@ cmds.append('''
           r._LogicalDB_key = 1 and
           r.prefixPart = "J:" and
           r.preferred = 1
-    order by imageID desc''')
+    order by imageID desc
+    ''', 'auto')
 
-results = db.sql(cmds, 'auto')
-
-fp.write('(%d rows affected)' % (len(results[2])))
+fp.write('(%d rows affected)' % (len(results)))
 fp.write(2*CRT)
 
 fp.write('Displayed Image Panes annotated to >1 specimen in the same assay.')
@@ -133,14 +145,25 @@ fp.write(SPACE)
 fp.write(50*'-')
 fp.write(CRT)
 
-for r in results[2]:
+def xstr(s):
+    """
+    convert s to '' if it is NoneType
+    """
+    if s is None:
+        return ''
+    else:
+        return str(s)
+
+for r in results:
     fp.write(string.ljust(r['assayID'],30))
     fp.write(SPACE)
     fp.write(string.ljust(r['imageID'],30))
     fp.write(SPACE)
     fp.write(string.ljust(r['jNumber'],30))
     fp.write(SPACE)
-    fp.write(string.ljust(r['label'],50))
+    figureLabel = xstr(r['figureLabel'])
+    paneLabel = xstr(r['paneLabel'])
+    fp.write(string.ljust((figureLabel + paneLabel)[:50],50))
     fp.write(CRT)
 
 reportlib.finish_nonps(fp)
