606908a17ae92ad69b2b89c1fa14a1cc887467bd
MGI Admin <mgiadmin@jax.org>
2014-12-08 21:35:26 +0000

    qcreports_db-5-2-1-1

--- weekly/PRB_StrainJAX4.sql
+++ weekly/PRB_StrainJAX4.sql
@@ -9,11 +9,12 @@ print ''
 set nocount off
 go
 
-select a.accID
-from ACC_Accession a
+select substring(a.accID,1,8) as accID, substring(s.strain,1,70) as strain
+from ACC_Accession a, PRB_Strain s
 where a._MGIType_key = 10
 and a._LogicalDB_key = 22
 and a.creation_date between dateadd(day, -7, getdate()) and getdate()
+and a._Object_key = s._Strain_key
 order by a.accID
 go
 
