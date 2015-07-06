3e643b6e46efdf650518ab9b950e0b67754f9d77
MGI Admin <mgiadmin@jax.org>
2015-07-06 11:23:43 +0000

    TR11750/postgres

--- mgd/GXD_EMAPS_MappingNameChecks.py
+++ mgd/GXD_EMAPS_MappingNameChecks.py
@@ -64,18 +64,16 @@ results = db.sql('''select mem.accID,
 	    vte.stage as emapsStage,
 	    voct.term as emapsName
 	from GXD_Structure gs, GXD_TheilerStage gts, GXD_StructureName gsn, 
-	    ACC_Accession aa1, ACC_MGIType ty1, MGI_EMAPS_Mapping mem, 
-	    ACC_Accession aa2, ACC_MGIType ty2, VOC_Term voct, 
+	    ACC_Accession aa1, MGI_EMAPS_Mapping mem, 
+	    ACC_Accession aa2, VOC_Term voct, 
 	    VOC_Term_EMAPS vte 
 	where gs._Structure_key = aa1._Object_key 
 	and gs._StructureName_key = gsn._StructureName_key 
 	and gs._Stage_key = gts._Stage_key 
 	and aa1.accId = mem.accID 
-	and aa1._MGIType_key = ty1._MGIType_key 
-	and ty1.tableName = 'GXD_Structure' 
+	and aa1._MGIType_key = 38
 	and mem.emapsID = aa2.accId 
-	and aa2._MGIType_key = ty2._MGIType_key 
-	and ty2.tableName = 'VOC_Term' 
+	and aa2._MGIType_key = 13
 	and aa2._Object_key = voct._Term_key 
 	and voct._Term_key = vte._Term_key 
 	and (case when voct.term = gsn.structure then 1 else 0 end) = 0
