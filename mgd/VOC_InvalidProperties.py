3215e6125d52f4a3cb0112116f8a518303d1bcf9
Lori Corbani <Lori.Corbani@jax.org>
2015-09-21 17:24:54 +0000

    TR12070/VOC_InvalidProperties.py

--- mgd/VOC_InvalidProperties.py
+++ mgd/VOC_InvalidProperties.py
@@ -40,17 +40,30 @@ fp = reportlib.init(sys.argv[0], outputdir = os.environ['QCOUTPUTDIR'])
 fp.write('\nInvalid "Properties" Values in GO Annotations' + 2 * reportlib.CRT)
 rows = 0
 
+#
 # read in all term ids (13) of interest
 # this is the list of valid accession ids
+#
+# 4 : GO
+# 6 : Adult Mouse Anatomy
+# 77 : Protein Ontology (PR)
+# 102 : CL
+#
+# and MGI: ids for markers
+#
 
 mgiLookup = []
 results = db.sql('''
+(
 select a.accID from ACC_Accession a, VOC_Term t
 where a._MGIType_key = 13 
 and a._Object_key = t._Term_key
 and t.isObsolete = 0
-and t._Vocab_key in (102)
-order by a.accID
+and t._Vocab_key in (4,6,77,102)
+union all
+select a.accID from ACC_Accession a where a._MGIType_key = 2 and a.prefixPart = 'MGI:' and a.preferred = 1
+)
+order by accID
 ''', 'auto')
 for r in results:
     mgiLookup.append(r['accID'])
@@ -60,13 +73,21 @@ for r in results:
 db.sql('''
 	select distinct p.value, aa.accID, m.symbol
 	into temporary table annotations 
-	from VOC_Annot a, VOC_Evidence e, VOC_Evidence_Property p,
+	from VOC_Annot a, VOC_Evidence e, VOC_Evidence_Property p, VOC_Term t,
 		ACC_Accession aa, MRK_Marker m
 	where a._AnnotType_key = 1000 
 	and a._Annot_key = e._Annot_key 
 	and e._AnnotEvidence_key = p._AnnotEvidence_key
+	and p._PropertyTerm_key = t._Term_key
+	and t._Vocab_key = 82
+	and t.term not in ('anatomy', 'cell type', 'dual-taxon ID', 'evidence', 'external ref', 'gene product', 'modification', 'target', 'text')
 	and p.value is not null 
-	and p.value like '%CL:%'
+	and (
+	     p.value like '% CL:%'
+	     or p.value like '% GO:%'
+	     or p.value like '% MA:%'
+	     or p.value like '% PR:%'
+	    )
         and a._Term_key = aa._Object_key 
         and aa._MGIType_key = 13  
         and aa.preferred = 1 
@@ -78,19 +99,16 @@ results = db.sql('select * from annotations', 'auto')
 
 for r in results:
     ids = r['value']
-    ids = ids.replace('%', '|')
-    ids = ids.replace('!', '|')
-    ids = ids.replace(';', '|')
-    ids = ids.replace(',', '|')
-    ids = ids.replace('(', '|')
-    ids = ids.replace(')', '|')
-    ids = ids.replace(' ', '')
+    ids = ids.replace(' ', ';')
     ids = ids.upper()
-    delimiter = '|'
+    delimiter = ';'
     idList = string.split(ids, delimiter)
 
     for id in idList:
-	if string.find(id, 'CL:') >= 0:
+	if string.find(id, 'CL:') >= 0 \
+	   or string.find(id, 'GO:') >= 0 \
+	   or string.find(id, 'MA:') >= 0 \
+	   or string.find(id, 'PR:') >= 0 :
 		if id not in mgiLookup:
         		fp.write(r['accID'] + reportlib.TAB + \
 				r['symbol'] + reportlib.TAB + \
