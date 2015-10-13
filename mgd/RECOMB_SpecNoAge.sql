881af94511fe2e2657372373f5904c00974ba078
Lori Corbani <Lori.Corbani@jax.org>
2015-10-13 15:21:55 +0000

    qcreports_db-6-0-1-1/TR12153/mgd/RECOMB_SpecNoAge.sql

--- mgd/RECOMB_SpecNoAge.sql
+++ mgd/RECOMB_SpecNoAge.sql
@@ -4,7 +4,16 @@ from GXD_Specimen s
 where (s.age like 'Not Applicable%' or s.age like 'Not Specified%')
 ;
 
-create index spec1_idx on spec1(_Assay_key)
+select s._Assay_key, substring(s.laneLabel, 1, 50) as laneLabel
+INTO TEMPORARY TABLE spec2
+from GXD_GelLane s
+where s._GelControl_key = 1
+and (s.age like 'Not Applicable%' or s.age like 'Not Specified%')
+;
+
+create index spec1_idx1 on spec1(_Assay_key)
+;
+create index spec2_idx1 on spec2(_Assay_key)
 ;
 
 \echo ''
@@ -15,13 +24,33 @@ select a1.accID as mgiID, a2.accID as jnumID, s.specimenLabel
 from spec1 s, GXD_Assay ga, ACC_Accession a1, ACC_Accession a2
 where s._Assay_key = ga._Assay_key
 and ga._AssayType_key in (10,11)
-and ga._Marker_key = a1._Object_key
-and a1._MGIType_key = 2
+and ga._Assay_key = a1._Object_key
+and a1._MGIType_key = 8
 and a1._LogicalDB_key = 1
 and a1.prefixPart = 'MGI:'
 and a1.preferred = 1
 and ga._Refs_key = a2._Object_key
-and a2._MGIType_key = 2
+and a2._MGIType_key = 1
+and a2._LogicalDB_key = 1
+and a2.prefixPart = 'J:'
+and a2.preferred = 1
+;
+
+\echo ''
+\echo 'Gel Lane Specimens with Not Applicable, Not Specified'
+\echo ''
+
+select a1.accID as mgiID, a2.accID as jnumID, s.laneLabel
+from spec2 s, GXD_Assay ga, ACC_Accession a1, ACC_Accession a2
+where s._Assay_key = ga._Assay_key
+and ga._AssayType_key in (10,11)
+and ga._Assay_key = a1._Object_key
+and a1._MGIType_key = 8
+and a1._LogicalDB_key = 1
+and a1.prefixPart = 'MGI:'
+and a1.preferred = 1
+and ga._Refs_key = a2._Object_key
+and a2._MGIType_key = 1
 and a2._LogicalDB_key = 1
 and a2.prefixPart = 'J:'
 and a2.preferred = 1
@@ -35,17 +64,73 @@ and s._Specimen_key = i._Specimen_key
 and i._Result_key = r._Result_key
 and r._Structure_key = c._Structure_key
 and c._Stage_key = t._Stage_key
-and t.stage != 28 
-and t.stage != 27
+and t.stage not in (27, 28)
+;
+
+select distinct i._GelLane_key
+INTO TEMPORARY TABLE temp2
+from GXD_GelLane i, GXD_GelLaneStructure r, GXD_Structure s, GXD_TheilerStage t
+where i.age like 'postnatal%'
+and i._GelLane_key = r._GelLane_key
+and r._Structure_key = s._Structure_key
+and s._Stage_key = t._Stage_key
+and t.stage not in (27, 28)
 ;
 
 \echo ''
-\echo 'TS 27 and TS 28 InSitu Specimens annotated to embryonic structures'
+\echo 'Postnatal Age InSitu Specimens annotated to embryonic structures'
 \echo ''
 
 select a.mgiID, a.jnumID, substring(s.specimenLabel, 1, 50) as specimenLabel
 from temp1 t, GXD_Specimen s, GXD_Assay_View a
 where t._Specimen_key = s._Specimen_key
+and s._Assay_key = a._Assay_key
+and a._AssayType_key in (10,11)
+;
+
+\echo ''
+\echo 'Postnatal Age Gel Lanes annotated to embryonic structures'
+\echo ''
+
+select a.mgiID, a.jnumID, substring(s.laneLabel, 1, 50) as laneLabel
+from temp2 t, GXD_GelLane s, GXD_Assay_View a
+where t._GelLane_key = s._GelLane_key
+and s._Assay_key = a._Assay_key
+and a._AssayType_key in (10,11)
+;
+
+\echo ''
+\echo 'InSitu Specimens with Age either ''postnatal'', ''postnatal adult'', ''postnatal newborn'' but age range entered'
+\echo ''
+
+select s.age, a.mgiID, a.jnumID, substring(s.specimenLabel, 1, 50) as specimenLabel
+from GXD_Specimen s, GXD_Assay_View a
+where
+(
+s.age ~ 'postnatal [0-9]'
+or
+s.age ~ 'postnatal adult [0-9]'
+or
+s.age ~ 'postnatal newborn [0-9]'
+)
+and s._Assay_key = a._Assay_key
+and a._AssayType_key in (10,11)
+;
+
+\echo ''
+\echo 'Gel Lane Specimens with Age either ''postnatal'', ''postnatal adult'', ''postnatal newborn'' but age range entered'
+\echo ''
+
+select s.age, a.mgiID, a.jnumID, substring(s.laneLabel, 1, 50) as laneLabel
+from GXD_GelLane s, GXD_Assay_View a
+where
+(
+s.age ~ 'postnatal [0-9]'
+or
+s.age ~ 'postnatal adult [0-9]'
+or
+s.age ~ 'postnatal newborn [0-9]'
+)
 and s._Assay_key = a._Assay_key
 and a._AssayType_key in (10,11)
 ;
@@ -82,7 +167,45 @@ s.age = 'postnatal day 3.5' or
 s.age like 'postnatal newborn%'
 )
 and s._Assay_key = a._Assay_key
-and a._AssayType_key in (10, 11)
+and a._AssayType_key in (10,11)
 and not exists (select 1 from temp3 t
 where s._Specimen_key = t._Specimen_key)
+;
+
+\echo ''
+\echo 'Gel Lane Specimens with postnatal age in (''day 0'', ''day 0.5'', ''day 1'', ''day 1.5'', ''day 2'', ''day 2.5'', ''day 3'', ''day 3.5'', ''newborn''), but not TS27'
+\echo ''
+
+select distinct i._GelLane_key
+INTO TEMPORARY TABLE temp4
+from GXD_GelLane i, GXD_GelLaneStructure r, GXD_Structure s, GXD_TheilerStage t
+where i._GelControl_key = 1
+and i._GelLane_key = r._GelLane_key
+and r._Structure_key = s._Structure_key
+and s._Stage_key = t._Stage_key
+and t.stage = 27
+;
+
+create index temp4_idx on temp4(_GelLane_key )
+;
+
+select s.age, a.mgiID, a.jnumID, substring(s.laneLabel, 1, 50) as laneLabel
+from GXD_GelLane s, GXD_Assay_View a
+where
+(
+s.age = 'postnatal day 0' or
+s.age = 'postnatal day 0.5' or
+s.age = 'postnatal day 1' or 
+s.age = 'postnatal day 1.5' or
+s.age = 'postnatal day 2' or
+s.age = 'postnatal day 2.5' or
+s.age = 'postnatal day 3' or
+s.age = 'postnatal day 3.5' or
+s.age like 'postnatal newborn%'
+)
+and s._GelControl_key = 1
+and s._Assay_key = a._Assay_key
+and a._AssayType_key in (10,11)
+and not exists (select 1 from temp4 t
+where s._GelLane_key = t._GelLane_key)
 ;
