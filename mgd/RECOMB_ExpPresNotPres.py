72a9371c0d5fa758e748dff76078322aa2372a3e
Lori Corbani <Lori.Corbani@jax.org>
2016-01-13 13:56:26 +0000

    TR12223/gxd anatomy II

--- mgd/RECOMB_ExpPresNotPres.py
+++ mgd/RECOMB_ExpPresNotPres.py
@@ -69,42 +69,71 @@ fp.write('----------------------------------------------------------------------
 
 #
 # structures of male/female reproductive systems are to be excluded
-# TS16;urogenital system;gonadal component [1787]
-# TS17;urogenital system;gonadal component [2378]
-# TS18;gonad primordium [3000]
-# TS19;reproductive system [3715]
-# TS20;reproductive system [4402]
-# TS21;reproductive system [5261]
-# TS22;reproductive system [6327]
-# TS23;reproductive system [7648]
-# TS24;reproductive system [7649]
-# TS25;reproductive system [7650]
-# TS26;reproductive system [7651]
-# TS28;reproductive system [6950]
+# TS16;urogenital system;gonadal component 
+# TS17;urogenital system;gonadal component
+# TS18;gonad primordium
+# TS19;reproductive system
+# TS20;reproductive system
+# TS21;reproductive system
+# TS22;reproductive system
+# TS23;reproductive system
+# TS24;reproductive system
+# TS25;reproductive system
+# TS26;reproductive system
+# TS28;reproductive system
 
 db.sql('''
-	select _Descendent_key as _Structure_key
-	into temporary table excludeStructs 
-	from GXD_StructureClosure 
-	where _Structure_key in (1787,2378,3000,3715,4402,5261,6327,7648,7649,7650,7651,6950)
+    SELECT DISTINCT c._DescendentObject_key as _Term_key, s._Stage_key
+    INTO TEMPORARY TABLE excludeStructs
+    FROM VOC_Term t, DAG_Closure c, VOC_Term tt, VOC_Term_EMAPS s
+    WHERE t._Vocab_key = 90
+      AND t._Term_key = c._AncestorObject_key
+      AND c._MGIType_key = 13
+      AND c._DescendentObject_key = tt._Term_key
+      AND tt._Term_key = s._EMAPA_Term_key
+      AND
+      (
+      (t.term = 'reproductive system' AND s._Stage_key in (19,20,21,22,23,24,25,26,28))
+      OR
+      (t.term = 'urogenital system' AND s._Stage_key in (16,17))
+      OR
+      (t.term = 'gonad primordium' AND s._Stage_key in (18))
+      )
+    UNION 
+    SELECT DISTINCT t._Term_key, s._Stage_key
+    FROM VOC_TERM t, VOC_Term_EMAPS s
+    WHERE t._Vocab_key = 90
+      AND t._Term_key = s._EMAPA_Term_key
+      AND
+      (
+      (t.term = 'reproductive system' AND s._Stage_key in (19,20,21,22,23,24,25,26,28))
+      OR
+      (t.term = 'urogenital system' AND s._Stage_key in (16,17))
+      OR
+      (t.term = 'gonad primordium' AND s._Stage_key in (18))
+      )
 	''', None)
-db.sql('create index excludeStruct_idx1 on excludeStructs(_Structure_key)', None)
+
+db.sql('create index excludeStructs_idx1 on excludeStructs(_Term_key)', None)
+db.sql('create index excludeStructs_idx2 on excludeStructs(_Stage_key)', None)
 
 #
 # assays with expression
 #
 db.sql('''
-	select distinct e._Assay_key, e._Refs_key, e._Structure_key, e._Genotype_key, e.age 
+	select distinct e._Assay_key, e._Refs_key, e._EMAPA_Term_key, e._Stage_key, e._Genotype_key, e.age 
 	into temporary table expressed 
 	from GXD_Expression e 
 	where e.isForGXD = 0 
 	and e.expressed = 1 
-	and not exists (select 1 from excludeStructs r where e._Structure_key = r._Structure_key) 
+	and not exists (select 1 from excludeStructs r 
+		where e._EMAPA_Term_key = r._Term_key and e._Stage_key = r._Stage_key) 
 	''', None)
-db.sql('create index expressd_idx1 on expressed(_Assay_key)', None)
-db.sql('create index expressd_idx2 on expressed(_Structure_key)', None)
-db.sql('create index expressd_idx3 on expressed(_Genotype_key)', None)
-db.sql('create index expressd_idx4 on expressed(age)', None)
+db.sql('create index expressed_idx1 on expressed(_Assay_key)', None)
+db.sql('create index expressed_idx2 on expressed(_EMAPA_Term_key)', None)
+db.sql('create index expressed_idx3 on expressed(_Stage_key)', None)
+db.sql('create index expressed_idx4 on expressed(_Genotype_key)', None)
+db.sql('create index expressed_idx5 on expressed(age)', None)
 
 #
 # compare expressed/not expressed by assay, structure, genotype, age
@@ -115,14 +144,16 @@ db.sql('''
 	from expressed e, GXD_Expression n 
 	where e._Assay_key = n._Assay_key 
 	and n.isForGXD = 0 
-	and e._Structure_key = n._Structure_key 
+	and e._EMAPA_Term_key = n._EMAPA_Term_key 
+	and e._Stage_key = n._Stage_key
 	and e._Genotype_key = n._Genotype_key 
 	and e.age = n.age 
 	and n.expressed = 0 
 	''', None)
 db.sql('create index results_idx1 on results(_Assay_key)', None)
-db.sql('create index results_idx2 on results(_Structure_key)', None)
-db.sql('create index results_idx3 on results(_Refs_key)', None)
+db.sql('create index results_idx2 on results(_EMAPA_Term_key)', None)
+db.sql('create index results_idx3 on results(_Stage_key)', None)
+db.sql('create index results_idx4 on results(_Refs_key)', None)
 
 #
 # final results
@@ -130,10 +161,10 @@ db.sql('create index results_idx3 on results(_Refs_key)', None)
 results = db.sql('''
 	select ac1.accID as jnumID, 
 	       ac2.accID as mgiID, 
-	       t.stage::text || ':' || s.printName as structure
-         from results r, GXD_Structure s, GXD_TheilerStage t, ACC_Accession ac1, ACC_Accession ac2 
-         where r._Structure_key = s._Structure_key 
-         and s._Stage_key = t._Stage_key 
+	       t.stage::text || ':' || s.term as term
+         from results r, VOC_Term s, GXD_TheilerStage t, ACC_Accession ac1, ACC_Accession ac2 
+         where r._EMAPA_Term_key = s._Term_key 
+         and r._Stage_key = t._Stage_key 
          and r._Refs_key = ac1._Object_key 
          and ac1._LogicalDB_key = 1 
          and ac1._MGIType_key = 1 
@@ -142,7 +173,7 @@ results = db.sql('''
          and ac2._LogicalDB_key = 1 
          and ac2._MGIType_key = 8 
          and ac2.prefixPart = 'MGI:' 
-         order by mgiID
+	 order by mgiID, term
 	 ''', 'auto')
 
 #
@@ -151,6 +182,6 @@ results = db.sql('''
 
 fp.write('\n(%d rows affected)\n\n' % (len(results)))
 for r in results:
-    fp.write("%-9s  %-12s  %-100s\n" % (r['jnumID'],r['mgiID'],r['structure']))
+    fp.write("%-9s  %-12s  %-100s\n" % (r['jnumID'],r['mgiID'],r['term']))
 fp.write('\n(%d rows affected)\n' % (len(results)))
 
