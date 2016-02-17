0b04f886d89ea414c63ff6d9212627559d565170
MGI Admin <mgiadmin@jax.org>
2016-02-17 13:27:37 +0000

    qcreports_db-6-0-2-8

--- mgd/GXD_DupGenotype.py
+++ mgd/GXD_DupGenotype.py
@@ -32,33 +32,38 @@ TAB = reportlib.TAB
 fp = reportlib.init(sys.argv[0], 'Duplicate Genotypes', os.environ['QCOUTPUTDIR'])
 
 
-db.sql('''select v.*, a1.creation_date as genoCreateDate, a1.accid as genoAccid, c1.cellLine as mcl1, g.isConditional
+db.sql('''select v.*,  a1.creation_date as genoCreateDate, a1.accid as genoAccid, g.isConditional
     into temporary table allelePair1
-    from GXD_AllelePair_View v, ACC_Accession a1, ALL_CellLine c1, GXD_Genotype g
-    where v._MutantCellLine_key_1 = c1._CellLine_key
-    and v._Genotype_key = a1._Object_key
+    from GXD_AllelePair_View v, ACC_Accession a1, GXD_Genotype g
+    where v._Genotype_key = a1._Object_key
     and a1._MGIType_key = 12
     and a1._LogicalDB_key = 1
     and v._Genotype_key = g._Genotype_key''', None)
 
-db.sql('''create index idx1 on allelePair1(_MutantCellLine_key_2)''', None)
+db.sql('''create index idx1 on allelePair1(_MutantCellLine_key_1)''', None)
+db.sql('''create index idx2 on allelePair1(_MutantCellLine_key_2)''', None)
 
-db.sql('''select a.*, c2.cellLine as mcl2
+db.sql('''select a.*, c1.cellLine as mcl1
     into temporary table allelePair2
     from allelePair1 a
+    left outer join ALL_CellLIne c1 on (a._MutantCellLine_key_1 = c1._CellLine_key)''', None)
+
+db.sql('''select a.*, c2.cellLine as mcl2
+    into temporary table allelePair3
+    from allelePair2 a
     left outer join ALL_CellLIne c2 on (a._MutantCellLine_key_2 = c2._CellLine_key)''', None)
 
-db.sql('''create index idx2 on allelePair2(_Genotype_key)''', None)
+db.sql('''create index idx3 on allelePair3(_Genotype_key)''', None)
 
 results = db.sql('''select distinct a.*, s._Strain_key, s.strain
-    from allelePair2 a, GXD_Genotype g, PRB_Strain s
+    from allelePair3 a, GXD_Genotype g, PRB_Strain s
     where a._Genotype_key = g._Genotype_key
     and g._Strain_key = s._Strain_key''', 'auto')
 
 #{genoAccid:[result1, ...], ...}
 genoDict = {}
 
-fp.write('genotypeIds%ssymbol%sallele1%sallele2%salleleState%smcl1%smcl2%sstrain%sisConditional%s' % (TAB, TAB, TAB, TAB, TAB, TAB, TAB, TAB, CRT))
+fp.write('genotypeIds%ssymbol%schr%sallele1%sallele2%salleleState%smcl1%smcl2%sstrain%sisConditional%s' % (TAB, TAB, TAB, TAB, TAB, TAB, TAB, TAB, TAB, CRT))
 
 # map each genotype ID to its set of allele pairs
 for r in results:
@@ -77,6 +82,7 @@ for gId in genoDict:
     resultsList = genoDict[gId]
     for r in resultsList:
 	symbol = r['symbol']
+	chr = r['chromosome']
 	allele1 = r['allele1']
 	allele2 = r['allele2']
 	alleleState = r['alleleState']
@@ -88,7 +94,7 @@ for gId in genoDict:
 	isConditional = r['isConditional']
 
 	# delimit the attributes of each allele pair with '|'
-	k = '%s|%s|%s|%s|%s|%s|%s|%s' %(symbol, allele1, allele2, alleleState, mcl1, mcl2, strain, isConditional)
+	k = '%s|%s|%s|%s|%s|%s|%s|%s|%s' %(symbol, chr, allele1, allele2, alleleState, mcl1, mcl2, strain, isConditional)
 	keyList.append(k)
     # Delimit the allele pairs in the key with 'comma'
     key = string.join(keyList, ',')
