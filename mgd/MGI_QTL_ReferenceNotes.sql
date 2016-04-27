9eecf087c5d7d2dd381e89838d905b962c10bb28
Lori Corbani <Lori.Corbani@jax.org>
2016-04-27 16:36:09 +0000

    TR12289/MGI_QTL_ReferenceNotes.sql

--- mgd/MGI_QTL_ReferenceNotes.sql
+++ mgd/MGI_QTL_ReferenceNotes.sql
@@ -7,7 +7,7 @@ e.jnumID as Reference_Note_Jnumber,
 substring(n.note,1,100) as Reference_Note
 from MLD_Expt_View e, MLD_Notes n
 where e.exptType in ('TEXT-QTL') 
-and e._Refs_key and = n._Refs_key
+and e._Refs_key = n._Refs_key
 and n.note ilike '%' 
 order by e.exptType, e.jnumID
 ;
