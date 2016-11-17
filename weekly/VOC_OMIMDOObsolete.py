5efaa700697afe77cd15731ca5635c03d0420402
Lori Corbani <lori.corbani@jax.org>
2016-11-17 13:10:57 -0500

    TR12427/Disase Ontology (DO)

--- weekly/VOC_OMIMDOObsolete.py
+++ weekly/VOC_OMIMDOObsolete.py
@@ -36,15 +36,36 @@ CRT = reportlib.CRT
 fp = reportlib.init(sys.argv[0], 'Obsolete OMIM terms in DO as xrefs', outputdir = os.environ['QCOUTPUTDIR'])
 
 results = db.sql('''
+	(
 	select a1.accID as doid, a2.accID as omimid
-	from ACC_Accession a1, ACC_Accession a2, VOC_Term t
+	from ACC_Accession a1, ACC_Accession a2
 	where a1._MGIType_key = 13
 	and a1._LogicalDB_key = 191 
 	and a1._Object_key = a2._Object_key
 	and a1.preferred = 1 
 	and a2._LogicalDB_key = 15
-	and a2._Object_key = t._Term_key
-	and t.isObsolete = 1
+	and not exists (select 1 from ACC_Accession a3, VOC_Term t
+		where a2.accID = a3.accID
+		and a3._MGIType_key = 13
+		and a3._LogicalDB_key = 15
+		and a3._Object_key = t._Term_key
+		and t._Vocab_key = 44
+	union
+	select a1.accID as doid, a2.accID as omimid
+	from ACC_Accession a1, ACC_Accession a2
+	where a1._MGIType_key = 13
+	and a1._LogicalDB_key = 191 
+	and a1._Object_key = a2._Object_key
+	and a1.preferred = 1 
+	and a2._LogicalDB_key = 15
+	and exists (select 1 from ACC_Accession a3, VOC_Term t
+		where a2.accID = a3.accID
+		and a3._MGIType_key = 13
+		and a3._LogicalDB_key = 15
+		and a3._Object_key = t._Term_key
+		and t._Vocab_key = 44
+		and t.isObsolete = 1
+	)
 	order by a1.accID
 	''', 'auto')
 
