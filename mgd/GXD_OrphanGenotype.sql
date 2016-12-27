15dcf597954a21d741ee5f8ad6225bead8d69667
Lori Corbani <lori.corbani@jax.org>
2016-12-27 07:52:16 -0500

    TR12453/mgd/GXD_OrphanGenotype.sql/HT sample

--- mgd/GXD_OrphanGenotype.sql
+++ mgd/GXD_OrphanGenotype.sql
@@ -12,6 +12,7 @@ and not exists (select 1 from GXD_Specimen a where g._Genotype_key = a._Genotype
 and not exists (select 1 from VOC_Annot a where g._Genotype_key = a._Object_key and a._AnnotType_key in (1001,1002, 1005))
 and not exists (select 1 from PRB_Strain_Genotype a where g._Genotype_key = a._Genotype_key)
 and not exists (select 1 from IMG_ImagePane_Assoc a where g._Genotype_key = a._Object_key and a._MGIType_key = 12)
+and not exists (select 1 from GXD_HTSample a where g._Genotype_key = a._Genotype_key)
 and g._Strain_key = s._Strain_key
 and g._Genotype_key = a._Object_key
 and a._MGIType_key = 12
