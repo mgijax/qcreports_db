90a1da1c6afcc2d8f26291eea1c42c32dd00b24f
Sharon Cousins Giannatto <sharon.giannatto@jax.org>
2017-05-04 07:14:12 -0400

    added mgd/BIB_DupPM_DOI.sql

---
+++ mgd/BIB_DupPM_DOI.sql
@@ -0,0 +1,23 @@
+select _logicaldb_key, accid, count(distinct _object_key) as dupCount
+   into temporary table dups
+   from acc_accession
+   where _logicaldb_key in (29,65)
+   and _mgitype_key = 1
+   group by _logicaldb_key, accid
+   having count(*) > 1
+   order by _logicaldb_key, accid
+;
+
+create index idx1 on dups(_LogicalDB_key)
+;
+
+\echo ''
+\echo 'References with Duplicate PubMed and DOI IDs'
+\echo 'Col1: Source Col2: accID Col3: Dup Count'
+\echo ''
+
+select ldb.name, d.accid, d.dupCount
+from ACC_LogicalDB ldb, dups d
+where d._LogicalDB_key = ldb._LogicalDB_key
+;
+ 
