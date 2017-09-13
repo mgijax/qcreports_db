c462259fa099acc544c5568753f7a06bc4ae22d5
Lori Corbani <lori.corbani@jax.org>
2017-09-13 08:22:28 -0400

    TR12250/Lit Triage

--- mgd/MRK_GOUnknown.sql
+++ mgd/MRK_GOUnknown.sql
@@ -1,8 +1,8 @@
 
-/* GO Annotations to unknown terms */
-/* only include non-FANTOM references */
-/* and J: creation date >= Annotation creation date */
-/* ('GO:0008150', 'GO:0003674', 'GO:0005575') */
+-- GO Annotations to unknown terms 
+-- only include non-FANTOM references 
+-- and J: creation date >= Annotation creation date 
+-- ('GO:0008150', 'GO:0003674', 'GO:0005575')
 
 select distinct substring(m.symbol,1,25) as symbol, m._Marker_key, r._Refs_key,
 substring(r.jnumID,1,20) as jnumID,
@@ -26,7 +26,7 @@ create index temp1_idx1 on temp1(_Marker_key)
 create index temp1_idx2 on temp1(_Refs_key)
 ;
 
-/* grab the marker accession ids */
+-- grab the marker accession ids
 
 select t._Marker_key, t.symbol, t._Refs_key, t.jnumID, t.pubmedID, jnumDate, annotDate, substring(ma.accID,1,20) as mgiID
 INTO TEMPORARY TABLE temp2
@@ -43,7 +43,7 @@ create index temp2_idx1 on temp2(_Marker_key)
 create index temp2_idx2 on temp2(_Refs_key)
 ;
 
-/* set DO yes/no flag */
+-- set DO yes/no flag
 
 select distinct t._Marker_key, t.symbol
 INTO TEMPORARY TABLE diseaseontology
@@ -63,84 +63,152 @@ and a.isWildType != 1
 create index diseaseontology_idx1 on diseaseontology(_Marker_key)
 ;
 
-/* grab the pubmed ids */
-/* tag 1: for those with GO annotations only */
-/* tag 2: for those with GO and A&P annotations only */
-/* tag 3: any GO annotations */
-/* for those without GO annotations */
+-- grab the pubmed ids
+-- tag 1: for those with GO annotations only
+-- tag 2: for those with GO and A&P annotations only 
+-- tag 3: any GO annotations
+-- for those without GO annotations
 
-/* tag 1: for those with GO annotations only */
+-- tag 1: for those with GO annotations only
+-- group GO, Routed/Chosen
+
+--select t._Marker_key, t.symbol, t.mgiID, t.jnumID, t.pubmedID, t.jnumDate, t.annotDate, '1'::text as tag
+--INTO TEMPORARY TABLE temp3
+--from temp2 t
+--where exists (select 1 from BIB_DataSet_Assoc a, BIB_DataSet d
+--where t._Refs_key = a._Refs_key
+--and a._DataSet_key = d._DataSet_key
+--and d.abbreviation = 'GO'
+--and a.isNeverUsed = 0)
+--and not exists (select 1 from BIB_DataSet_Assoc a, BIB_DataSet d
+--where t._Refs_key = a._Refs_key
+--and a._DataSet_key = d._DataSet_key
+--and d.abbreviation != 'GO'
+--and a.isNeverUsed = 0)
+--and not exists (select 1 from VOC_Annot va, VOC_Evidence e
+--where va._AnnotType_key = 1000
+--and va._Annot_key = e._Annot_key
+--and e._Refs_key = t._Refs_key)
+--;
 select t._Marker_key, t.symbol, t.mgiID, t.jnumID, t.pubmedID, t.jnumDate, t.annotDate, '1'::text as tag
 INTO TEMPORARY TABLE temp3
 from temp2 t
-where exists (select 1 from BIB_DataSet_Assoc a, BIB_DataSet d
-where t._Refs_key = a._Refs_key
-and a._DataSet_key = d._DataSet_key
-and d.abbreviation = 'GO'
-and a.isNeverUsed = 0)
-and not exists (select 1 from BIB_DataSet_Assoc a, BIB_DataSet d
-where t._Refs_key = a._Refs_key
-and a._DataSet_key = d._DataSet_key
-and d.abbreviation != 'GO'
-and a.isNeverUsed = 0)
+where exists (select 1 from BIB_Workflow_Status s
+	where t._Refs_key = s._Refs_key
+	and s._Group_key = 31576666
+	and s._Status_key in (31576670,31576671)
+	and s.isCurrent = 1)
+and not exists (select 1 from BIB_Workflow_Status s
+	where t._Refs_key = s._Refs_key
+	and s._Group_key != 31576666
+	and s._Status_key in (31576670,31576671)
+	and s.isCurrent = 1)
 and not exists (select 1 from VOC_Annot va, VOC_Evidence e
-where va._AnnotType_key = 1000
-and va._Annot_key = e._Annot_key
-and e._Refs_key = t._Refs_key)
+	where va._AnnotType_key = 1000
+	and va._Annot_key = e._Annot_key
+	and e._Refs_key = t._Refs_key)
 ;
 
-/* tag 2: for those with GO and A&P annotations only */
-/* and not in tag 1 */
+-- tag 2: for those with GO and A&P annotations only
+-- group GO, Routed/Chosen
+-- group AP, Routed/Chosen
+-- and not in tag 1
+
+--INSERT INTO temp3
+--select t._Marker_key, t.symbol, t.mgiID, t.jnumID, t.pubmedID, t.jnumDate, t.annotDate, '2'::text as tag
+--from temp2 t
+--where not exists (select 1 from temp3 t3 
+--where t.symbol = t3.symbol
+--and t.pubmedID = t3.pubmedID)
+--and exists (select 1 from BIB_DataSet_Assoc a, BIB_DataSet d
+--where t._Refs_key = a._Refs_key
+--and a._DataSet_key = d._DataSet_key
+--and d.abbreviation = 'GO'
+--and a.isNeverUsed = 0)
+--and exists (select 1 from BIB_DataSet_Assoc a, BIB_DataSet d
+--where t._Refs_key = a._Refs_key
+--and a._DataSet_key = d._DataSet_key
+--and d.abbreviation = 'Allele/Pheno'
+--and a.isNeverUsed = 0)
+--and not exists (select 1 from BIB_DataSet_Assoc a, BIB_DataSet d
+--where t._Refs_key = a._Refs_key
+--and a._DataSet_key = d._DataSet_key
+--and d.abbreviation not in ('GO', 'Allele/Pheno')
+--and a.isNeverUsed = 0)
+--and not exists (select 1 from VOC_Annot va, VOC_Evidence e
+--where va._AnnotType_key = 1000
+--and va._Annot_key = e._Annot_key
+--and e._Refs_key = t._Refs_key)
+--;
+
 INSERT INTO temp3
 select t._Marker_key, t.symbol, t.mgiID, t.jnumID, t.pubmedID, t.jnumDate, t.annotDate, '2'::text as tag
 from temp2 t
 where not exists (select 1 from temp3 t3 
-where t.symbol = t3.symbol
-and t.pubmedID = t3.pubmedID)
-and exists (select 1 from BIB_DataSet_Assoc a, BIB_DataSet d
-where t._Refs_key = a._Refs_key
-and a._DataSet_key = d._DataSet_key
-and d.abbreviation = 'GO'
-and a.isNeverUsed = 0)
-and exists (select 1 from BIB_DataSet_Assoc a, BIB_DataSet d
-where t._Refs_key = a._Refs_key
-and a._DataSet_key = d._DataSet_key
-and d.abbreviation = 'Allele/Pheno'
-and a.isNeverUsed = 0)
-and not exists (select 1 from BIB_DataSet_Assoc a, BIB_DataSet d
-where t._Refs_key = a._Refs_key
-and a._DataSet_key = d._DataSet_key
-and d.abbreviation not in ('GO', 'Allele/Pheno')
-and a.isNeverUsed = 0)
+	where t.symbol = t3.symbol
+	and t.pubmedID = t3.pubmedID)
+and exists (select 1 from BIB_Workflow_Status s
+	where t._Refs_key = s._Refs_key
+	and s._Group_key = 31576666
+	and s._Status_key in (31576670,31576671)
+	and s.isCurrent = 1)
+and exists (select 1 from BIB_Workflow_Status s
+	where t._Refs_key = s._Refs_key
+	and s._Group_key = 31576664
+	and s._Status_key in (31576670,31576671)
+	and s.isCurrent = 1)
+and not exists (select 1 from BIB_Workflow_Status s
+	where t._Refs_key = s._Refs_key
+	and s._Group_key in (31576666, 31576664)
+	and s._Status_key in (31576670,31576671)
+	and s.isCurrent = 1)
 and not exists (select 1 from VOC_Annot va, VOC_Evidence e
-where va._AnnotType_key = 1000
-and va._Annot_key = e._Annot_key
-and e._Refs_key = t._Refs_key)
+	where va._AnnotType_key = 1000
+	and va._Annot_key = e._Annot_key
+	and e._Refs_key = t._Refs_key)
 ;
 
-/* tag 3: any GO annotations */
-/* and not in tag 1 or 2 */
+-- tag 3: any GO annotations 
+-- and not in tag 1 or 2
+
+--INSERT INTO temp3
+--select t._Marker_key, t.symbol, t.mgiID, t.jnumID, t.pubmedID, t.jnumDate, t.annotDate, '3'::text as tag
+--from temp2 t
+--where not exists (select 1 from temp3 t3 
+--where t.symbol = t3.symbol
+--and t.pubmedID = t3.pubmedID)
+--and exists (select 1 from BIB_DataSet_Assoc a, BIB_DataSet d
+--where t._Refs_key = a._Refs_key
+--and a._DataSet_key = d._DataSet_key
+--and d.abbreviation = 'GO'
+--and a.isNeverUsed = 0)
+--and not exists (select 1 from VOC_Annot va, VOC_Evidence e
+--where va._AnnotType_key = 1000
+--and va._Annot_key = e._Annot_key
+--and e._Refs_key = t._Refs_key)
+--;
+
 INSERT INTO temp3
 select t._Marker_key, t.symbol, t.mgiID, t.jnumID, t.pubmedID, t.jnumDate, t.annotDate, '3'::text as tag
 from temp2 t
 where not exists (select 1 from temp3 t3 
-where t.symbol = t3.symbol
-and t.pubmedID = t3.pubmedID)
-and exists (select 1 from BIB_DataSet_Assoc a, BIB_DataSet d
-where t._Refs_key = a._Refs_key
-and a._DataSet_key = d._DataSet_key
-and d.abbreviation = 'GO'
-and a.isNeverUsed = 0)
+	where t.symbol = t3.symbol
+	and t.pubmedID = t3.pubmedID)
+and exists (select 1 from BIB_Workflow_Status s
+	where t._Refs_key = s._Refs_key
+	and s._Group_key = 31576666
+	and s._Status_key in (31576670,31576671)
+	and s.isCurrent = 1)
 and not exists (select 1 from VOC_Annot va, VOC_Evidence e
-where va._AnnotType_key = 1000
-and va._Annot_key = e._Annot_key
-and e._Refs_key = t._Refs_key)
+	where va._AnnotType_key = 1000
+	and va._Annot_key = e._Annot_key
+	and e._Refs_key = t._Refs_key)
 ;
 
 create index temp3_idx1 on temp3(_Marker_key)
 ;
 
-/* set hasDO */
+-- set hasDO
 
 select t.*, 'Y'::text as hasDO
 INTO TEMPORARY TABLE temp4
