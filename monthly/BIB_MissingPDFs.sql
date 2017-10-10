2cfd4a1d35455081c0e89e9a5ba65c6ae337016f
Jonathan Beal <jsb@jax.org>
2017-10-10 13:29:51 -0400

    added report for J: numbers missing PDF files, to be updated monthly

---
+++ monthly/BIB_MissingPDFs.sql
@@ -0,0 +1,17 @@
+\echo ''
+\echo 'J#''s missing PDF''s'
+\echo ''
+
+with refs_without_pdfs as (
+select c.numericPart as jnum_numeric, c.jnumid, c.short_citation
+from bib_refs r, voc_term t, bib_workflow_data d, bib_citation_cache c
+where r._ReferenceType_key = t._Term_key
+	and t.term = 'Peer Reviewed Article'
+	and r._Refs_key = d._Refs_key
+	and r._Refs_key = c._Refs_key
+	and d.hasPDF = 0
+order by 1 desc
+)
+select jnumid, short_citation
+from refs_without_pdfs
+;
