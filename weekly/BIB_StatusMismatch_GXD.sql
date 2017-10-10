d9941696e63443e1be3c5dd6e26bc7c890a64c12
Jonathan Beal <jsb@jax.org>
2017-10-10 12:42:33 -0400

    added status mismatch report for GXD

---
+++ weekly/BIB_StatusMismatch_GXD.sql
@@ -0,0 +1,34 @@
+\echo ''
+\echo 'References with GXD Status not matching curation data'
+\echo ''
+
+with gxd_unused as (
+	select r._Refs_key, j.numericPart, j.accid, g.abbreviation as group, t.term as status
+	from bib_refs r, acc_accession j, bib_workflow_status s, voc_term g, voc_term t
+	where r._Refs_key = j._Object_key
+		and j._MGIType_key = 1
+		and j.prefixPart = 'J:'
+		and j._LogicalDB_key = 1
+		and r._Refs_key = s._Refs_key
+		and s.isCurrent = 1
+		and s._Group_key = g._Term_key
+		and g.abbreviation = 'GXD'
+		and s._Status_key = t._Term_key
+		and t.term not in ('Indexed', 'Full-coded')
+)
+select distinct u.accid, u.group, u.status,
+	case
+		when (a._Assay_key is not null) then 'yes'
+		else ''
+	end has_gxd_assays,
+	case
+		when (i._Index_key is not null) then 'yes'
+		else ''
+	end in_gxd_index
+from gxd_unused u
+left outer join gxd_assay a on (u._Refs_key = a._Refs_key)
+left outer join gxd_index i on (u._Refs_key = i._Refs_key)
+where a._Assay_key is not null
+	or i._Index_key is not null
+order by u.accid
+;
