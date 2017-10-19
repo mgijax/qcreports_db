49132a54e0a0de3dc7967dce3354176b17d30973
Jonathan Beal <jsb@jax.org>
2017-10-19 12:37:24 -0400

    adjusted to report matching text in a case insensitive manner; also did performance tweak

--- mgd/WF_AP_NewAlleleNomenTag.py
+++ mgd/WF_AP_NewAlleleNomenTag.py
@@ -74,6 +74,8 @@ searchTerms = [
 'gene-trap'
 ]
 
+searchTerms = map(lambda x : x.lower(), searchTerms)
+
 fp.write('''
  	The reference must be:
  	     group = AP, status = 'Routed' or 'Chosen'
@@ -89,33 +91,40 @@ byJText = {}
 
 searchSQL = ''
 for s in searchTerms:
-	searchSQL += ' lower(d.extractedText) like lower(\'%' + s + '%\') or'
+	searchSQL += ' d.extractedText like \'%' + s + '%\' or'
+#	searchSQL += ' lower(d.extractedText) like lower(\'%' + s + '%\') or'
 searchSQL = searchSQL[:-2]
 
 results = db.sql('''
-select r._Refs_key, c.jnumID, g.abbreviation as groupTerm, s.term as statusTerm, d.extractedText,
-	to_char(r.creation_date, 'MM/dd/yyyy') as cdate
-from BIB_Refs r, BIB_Citation_Cache c, BIB_Workflow_Status wfs, BIB_Workflow_Data d, 
-	VOC_Term g, VOC_Term s
-where r.isDiscard = 0
-and r._Refs_key = c._Refs_key
-and r._Refs_key = d._Refs_key
-and r._Refs_key = wfs._Refs_key
-and wfs._Group_key = g._Term_key
-and wfs._Status_key = s._Term_key
-and wfs.isCurrent = 1
-
-and exists (select wfso._Refs_key from BIB_Workflow_Status wfso
+-- working with references with AP status of Chosen or Routed, which do NOT have one of two special tags,
+-- and which are NOT discarded
+with refSet1 as (select wfso._Refs_key, r.creation_date
+    from BIB_Workflow_Status wfso, BIB_Refs r
 	where r._Refs_key = wfso._Refs_key
+	and r.isDiscard = 0
 	and wfso._Group_key in (31576664)
 	and wfso._Status_key in (31576670, 31576671)
 	and wfso.isCurrent = 1
+    and not exists (select wftag._Refs_key from BIB_Workflow_Tag wftag
+	    where r._Refs_key = wftag._Refs_key
+	    and wftag._Tag_key in (31576700, 31576702)
 	)
-
-and not exists (select wftag._Refs_key from BIB_Workflow_Tag wftag
-	where r._Refs_key = wftag._Refs_key
-	and wftag._Tag_key in (31576700, 31576702)
-	)
+),
+-- lowercase needed text fields (only once) into a temp table
+lowerText as (select d._Refs_key, lower(d.extractedText) as extractedText, r.creation_date
+    from BIB_Workflow_Data d, refSet1 as r
+    where d._Refs_key = r._Refs_key
+)
+select d._Refs_key, c.jnumID, g.abbreviation as groupTerm, s.term as statusTerm, bwd.extractedText,
+	to_char(d.creation_date, 'MM/dd/yyyy') as cdate
+from lowerText d, BIB_Citation_Cache c, BIB_Workflow_Status wfs, BIB_Workflow_Data bwd, 
+	VOC_Term g, VOC_Term s
+where d._Refs_key = c._Refs_key
+and d._Refs_key = bwd._Refs_key
+and d._Refs_key = wfs._Refs_key
+and wfs._Group_key = g._Term_key
+and wfs._Status_key = s._Term_key
+and wfs.isCurrent = 1
 
 and (%s)
 order by jnumID, groupTerm
@@ -146,7 +155,7 @@ for r in results:
 	extractedText = extractedText.replace('\n', ' ')
 	extractedText = extractedText.replace('\r', ' ')
 	for s in searchTerms:
-	    for match in re.finditer(s, extractedText):
+	    for match in re.finditer(s, extractedText, re.IGNORECASE):
 		subText = extractedText[match.start()-40:match.end()+40]
                 byJText[jnumID].append(subText)
 
