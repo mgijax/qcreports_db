624eeac825857f5af48281caf5644f6d9c008e46
Lori Corbani <lori.corbani@jax.org>
2017-10-20 11:19:36 -0400

    TR12250/Lit Triage

--- weekly/GXD_References.sql
+++ weekly/GXD_References.sql
@@ -5,8 +5,9 @@
 \echo ''
 
 (
-select r.jnumID, wft.term as wfterm
-from BIB_Citation_Cache r, BIB_Workflow_Status s, BIB_Workflow_Tag t, VOC_Term wft
+select r.jnumID, wft.term as wfterm, wfs.term as supplemental
+from BIB_Citation_Cache r, BIB_Workflow_Status s, BIB_Workflow_Tag t, VOC_Term wft,
+	BIB_Workflow_Data d, VOC_Term wfs
 where r._Refs_key = s._Refs_key
 and s.isCurrent = 1
 and s._Group_key = 31576665
@@ -14,13 +15,18 @@ and s._Status_key in (31576670, 31576671)
 and r._Refs_key = t._Refs_key
 and t._Tag_key = wft._Term_key
 and wft.term like 'GXD:%'
+and r._Refs_key = d._Refs_key
+and d._Supplemental_key = wfs._Term_key
 union
-select r.jnumID, null as wfterm
-from BIB_Citation_Cache r, BIB_Workflow_Status s
+select r.jnumID, null as wfterm, wfs.term as supplemental
+from BIB_Citation_Cache r, BIB_Workflow_Status s,
+	BIB_Workflow_Data d, VOC_Term wfs
 where r._Refs_key = s._Refs_key
 and s.isCurrent = 1
 and s._Group_key = 31576665
 and s._Status_key in (31576670, 31576671)
+and r._Refs_key = d._Refs_key
+and d._Supplemental_key = wfs._Term_key
 and not exists (select 1 from BIB_Workflow_Tag t, VOC_Term wft
 	where r._Refs_key = t._Refs_key
 	and t._Tag_key = wft._Term_key
