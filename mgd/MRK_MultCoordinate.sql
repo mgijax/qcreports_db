c05abdc3ae46da1137188bd132a4a529cfb54e96
Lori Corbani <lori.corbani@jax.org>
2018-02-14 11:08:01 -0500

    TR12773/MRK_MultCoordinate.sql

--- mgd/MRK_MultCoordinate.sql
+++ mgd/MRK_MultCoordinate.sql
@@ -4,7 +4,7 @@ INTO TEMPORARY TABLE coord
 from SEQ_Marker_Cache s, SEQ_Coord_Cache c
 where s._Organism_key = 1
 and s._Sequence_key = c._Sequence_key
-and c.provider in ('NCBI Gene Model', 'Ensembl Gene Model', 'VEGA Gene Model')
+and c.provider in ('NCBI Gene Model', 'Ensembl Gene Model')
 ;
 
 create index coord_idx1 on coord(_Marker_key)
@@ -22,13 +22,17 @@ create index dups_idx1 on dups(_Marker_key)
 ;
 
 \echo ''
-\echo 'Symbols w/ > 1 Ensembl, VEGA or NCBI Gene Model Association'
+\echo 'Symbols w/ > 1 Ensembl, NCBI Gene Model Association'
 \echo ''
 
-select distinct m.symbol, c.accID, c.provider
-from dups s, coord c, MRK_Marker m
+select distinct a.accID, m.symbol, c.accID, c.provider
+from dups s, coord c, MRK_Marker m, ACC_Accession a
 where s._Marker_key = c._Marker_key
 and s._Marker_key = m._Marker_key
+and m._Marker_key = a._Object_key
+and a._MGIType_key = 2
+and a._LogicalDB_key = 1
+and a.preferred = 1
 order by c.provider, m.symbol
 ;
 
