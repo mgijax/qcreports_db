d2c9a5483b14a4762c0632e8f22ad3c4c1208586
Lori Corbani <lori.corbani@jax.org>
2019-05-07 13:33:28 -0400

    TR13007/RNA Sequence/ageMin/ageMax

--- mgd/GXD_HTSampleWrongAge.sql
+++ mgd/GXD_HTSampleWrongAge.sql
@@ -1,43 +1,65 @@
 \echo ''
-\echo 'Samples where relevance = Yes and Age either ''postnatal'', ''postnatal adult'', ''postnatal newborn'' but age range entered'
+\echo 'Sample with Not Applicable, Not Specified'
+\echo 'Relevance = Yes'
 \echo ''
 
-select a.accID as exptId, s.name, s.age
-from GXD_HTSample s, ACC_Accession a
-    where s._Relevance_key = 20475450
-    and (
-          s.age ~ 'postnatal [0-9]'
-          or
-          s.age ~ 'postnatal adult [0-9]'
-          or
-          s.age ~ 'postnatal newborn [0-9]'
-    )
-    and s._Experiment_key = a._Object_key
-    and a._MGIType_key = 42
-    and a._LogicalDB_key = 189
-    and a.preferred = 1
-;
-
-\echo ''
-\echo 'Samples where relevance = Yes and postnatal age in (''day 0'', ''day 0.5'', ''day 1'', ''day 1.5'', ''day 2'', ''day 2.5'', ''day 3'', ''day 3.5'', ''newborn''), but not TS27'
-\echo ''
-select _Experiment_key
-into temporary table temp1
-from GXD_HTSample 
-where _Stage_key = 27
-;
-
-create index idx1 on temp1(_Experiment_key)
-;
-
-select a.accid as exptId, s.name, s.age, s._Stage_key
-from GXD_HTSample s, ACC_Accession a
+select a1.accID as exptID, s.name
+from GXD_HTSample s, ACC_Accession a1
 where s._Relevance_key = 20475450
-and s._Experiment_key = a._Object_key
-and a._MGIType_key = 42
-and a._LogicalDB_key = 189
-and a.preferred = 1
-and 
+and (s.age like 'Not Applicable%' or s.age like 'Not Specified%')
+and s._Sample_key = a1._Object_key
+and a1._MGIType_key = 42
+and a1._LogicalDB_key = 189
+;
+
+\echo ''
+\echo 'Postnatal Age annotated to embryonic structures'
+\echo 'Relevance = Yes'
+\echo ''
+
+select a1.accID as exptID, s.name
+from GXD_HTSample s, GXD_TheilerStage t, ACC_Accession a1
+where s._Relevance_key = 20475450
+and s.age like 'postnatal%'
+and s._Stage_key = t._Stage_key
+and t.stage not in (27, 28)
+and s._Sample_key = a1._Object_key
+and a1._MGIType_key = 42
+and a1._LogicalDB_key = 189
+;
+
+\echo ''
+\echo 'Age either ''postnatal'', ''postnatal adult'', ''postnatal newborn'' but age range entered'
+\echo 'Relevance = Yes'
+\echo ''
+
+select s.age, a1.accID as exptID, s.name
+from GXD_HTSample s, ACC_Accession a1
+where s._Relevance_key = 20475450
+and
+(
+s.age ~ 'postnatal [0-9]'
+or
+s.age ~ 'postnatal adult [0-9]'
+or
+s.age ~ 'postnatal newborn [0-9]'
+)
+and s._Sample_key = a1._Object_key
+and a1._MGIType_key = 42
+and a1._LogicalDB_key = 189
+;
+
+\echo ''
+\echo 'postnatal age in (''day 0'', ''day 0.5'', ''day 1'', ''day 1.5'', ''day 2'', ''day 2.5'', ''day 3'', ''day 3.5'', ''newborn''), but not TS27'
+\echo 'Relevance = Yes'
+\echo ''
+
+select s.age, a1.accID as exptID, s.name
+from GXD_HTSample s, GXD_TheilerStage t, ACC_Accession a1
+where s._Relevance_key = 20475450
+and s._Stage_key = t._Stage_key
+and t.stage not in (27)
+and
 (
 s.age = 'postnatal day 0' or
 s.age = 'postnatal day 0.5' or
@@ -49,8 +71,46 @@ s.age = 'postnatal day 3' or
 s.age = 'postnatal day 3.5' or
 s.age like 'postnatal newborn%'
 )
-and not exists (select 1
-from temp1 t
-where s._Experiment_key = t._Experiment_key)
-order by a.accid, s.name
+and s._Sample_key = a1._Object_key
+and a1._MGIType_key = 42
+and a1._LogicalDB_key = 189
 ;
+
+\echo ''
+\echo 'Theiler = 28 and ageMin < 21.01'
+\echo 'Relevance = Yes'
+\echo 'excludes placenta,decidua,decidua basalis,decidua capsularis, cumulus oophorus, uterus'
+\echo ''
+
+select s.age, s.ageMin, a1.accID as exptID, s.name
+from GXD_HTSample s, GXD_TheilerStage t, VOC_Term vt, ACC_Accession a1
+where s._Relevance_key = 20475450
+and s._Stage_key = t._Stage_key
+and t.stage = 28
+and s.ageMin < 21.01
+and s._emapa_key = vt._Term_key
+and vt.term not in ('placenta','decidua','decidua basalis','decidua capsularis', 'cumulus oophorus', 'uterus')
+and s._Sample_key = a1._Object_key
+and a1._MGIType_key = 42
+and a1._LogicalDB_key = 189
+;
+
+\echo ''
+\echo 'Theiler = 27 and ageMin < 21.01' or ageMin not > 28.01
+\echo 'Relevance = Yes'
+\echo 'excludes (placenta,decidua,decidua basalis,decidua capsularis, cumulus oophorus'
+\echo ''
+
+select s.age, s.ageMin, a1.accID as exptID, s.name
+from GXD_HTSample s, GXD_TheilerStage t, VOC_Term vt, ACC_Accession a1
+where s._Relevance_key = 20475450
+and s._Stage_key = t._Stage_key
+and t.stage = 27
+and (s.ageMin < 21.01 or s.ageMax > 28.01)
+and s._emapa_key = vt._Term_key
+and vt.term not in ('placenta','decidua','decidua basalis','decidua capsularis', 'cumulus oophorus')
+and s._Sample_key = a1._Object_key
+and a1._MGIType_key = 42
+and a1._LogicalDB_key = 189
+;
+
