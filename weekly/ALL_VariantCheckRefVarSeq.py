185a5d3f33585006eebdaf85ea8e09a247c8e428
Sharon Cousins Giannatto <sharon.giannatto@jax.org>
2020-05-15 12:42:52 -0400

    curator notes instead of hgsv

--- weekly/ALL_VariantCheckRefVarSeq.py
+++ weekly/ALL_VariantCheckRefVarSeq.py
@@ -42,8 +42,9 @@ TAB = reportlib.TAB
 
 fp  = reportlib.init(sys.argv[0], title = 'Variants where reference and variant sequence are the same', outputdir = os.environ['QCOUTPUTDIR'], printHeading = None)
 fp.write('AlleleID%sAlleleSymbol%sCuratedGenomicReferenceAllele%sCuratedGenomicVariantAllele%sCuratorNotes%s' % (TAB, TAB, TAB, TAB, CRT))
-results = db.sql('''select aa.accid, a.symbol, vs.referencesequence, 
-        vs.variantsequence, v.description as hgvs
+db.sql('''select aa.accid, a.symbol, vs.referencesequence, 
+        vs.variantsequence, vs._variant_key
+    into temporary table variants
     from acc_accession aa, all_variant v, all_variant_sequence vs, all_allele a
     where v._sourceVariant_key is not null --curated
     and v._variant_key = vs._variant_key
@@ -54,10 +55,25 @@ results = db.sql('''select aa.accid, a.symbol, vs.referencesequence,
     and aa._mgitype_key = 11
     and aa._Logicaldb_key = 1
     and aa.preferred = 1
-    and aa.prefixpart = 'MGI:' ''', 'auto')
+    and aa.prefixpart = 'MGI:' ''', None)
+
+db.sql('''create index idx1 on variants(_variant_key)''')
+
+db.sql('''select n._object_key, nc.note
+    into temporary table variantNotes
+    from MGI_Note n, MGI_NoteChunk nc
+    where n._NoteType_key = 1050
+    and n._note_key = nc._note_key''', None)
+
+db.sql('''create index idx2 on variantNotes(_object_key)''', None)
+
+results = db.sql('''select v.*, vn.note
+    from variants v
+    left outer join variantNotes vn on (v._variant_key = vn._object_key)''', 'auto')
+
 for r in results:
-    hgvs = r['hgvs']
-    if hgvs == None:
-        hgvs = ''
-    fp.write('%s%s%s%s%s%s%s%s%s%s' % (r['accid'], TAB, r['symbol'], TAB, r['referencesequence'], TAB, r['variantsequence'], TAB, hgvs, CRT) )
+    curNote = r['note']
+    if curNote == None:
+        curNote = ''
+    fp.write('%s%s%s%s%s%s%s%s%s%s' % (r['accid'], TAB, r['symbol'], TAB, r['referencesequence'], TAB, r['variantsequence'], TAB, curNote, CRT) )
 reportlib.finish_nonps(fp)	# non-postscript file
