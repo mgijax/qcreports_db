5e7f4e18d40103fa046819a074e6b1b8e026cd7f
Lori Corbani <lori.corbani@jax.org>
2021-06-17 11:14:32 -0400

    yaks-39/Identify curator contacts for all loads

--- mgd/MGI_GenoNoMarker.sql
+++ mgd/MGI_GenoNoMarker.sql
@@ -6,11 +6,6 @@
 select distinct aa.symbol
         from GXD_AlleleGenotype g, ALL_Allele aa
         where g._Allele_key = aa._Allele_key
-        and not exists (select 1 from ACC_Accession ac
-        where g._Marker_key = ac._Object_key 
-        and ac._MGIType_key = 2 
-        and ac._LogicalDB_key = 1 
-        and ac.prefixPart = 'MGI:' 
-        and ac.preferred = 1)
+        and aa._Marker_key is null
 ;
 
