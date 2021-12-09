60cdbeea9b6f6e8d9562a0e1e37ff49b490a2a5c
Sharon Cousins Giannatto <sharon.giannatto@jax.org>
2021-12-09 16:24:05 -0500

    add cell type term

--- mgd/GXD_SameStructure.py
+++ mgd/GXD_SameStructure.py
@@ -11,6 +11,9 @@
 #		J number of paper
 #		MGI id of assay
 #		Name of offending specimen/gel lane
+#               EMAPA Term
+#               Stage
+#               Cell Type Term
 #
 #	Sort by: J number
 #
@@ -26,6 +29,9 @@
 #	- all private SQL reports require the header
 #
 # History:
+#
+# sc    12/09/2021
+#       - YAKS project; added cell type term column
 #
 # lec	05/01/2008
 #	- TR 8775; on select GXD assay types
@@ -61,50 +67,63 @@ fp.write(str.ljust('J:', 15))
 fp.write(str.ljust('Assay ID', 15))
 fp.write(str.ljust('Label', 35))
 fp.write(str.ljust('EMAPA-Term', 50))
-fp.write(str.ljust('Stage', 5))
+fp.write(str.ljust('Stage', 10))
+fp.write(str.ljust('CellType-Term', 50))
 fp.write(CRT)
 fp.write(str.ljust('----------', 15))
 fp.write(str.ljust('----------', 15))
 fp.write(str.ljust('----------', 35))
 fp.write(str.ljust('----------', 50))
-fp.write(str.ljust('-----', 5))
+fp.write(str.ljust('-----', 10))
+fp.write(str.ljust('----------', 50))
 fp.write(2*CRT)
 
 # get the all specimen/structure pairs
 
 db.sql('''
-    select gr._Specimen_key, grs._EMAPA_Term_key , grs._Stage_key
-    into temporary table specstructs 
-    from GXD_Assay a, GXD_Specimen s, GXD_InSituResult gr, GXD_ISResultStructure grs 
-    where a._AssayType_key in (1,2,3,4,5,6,8,9) 
-    and a._Assay_key = s._Assay_key 
-    and s._Specimen_key = gr._Specimen_key 
+    select gr._Specimen_key, grs._EMAPA_Term_key , grs._Stage_key, gr._result_key
+    into temporary table specstructs
+    from GXD_Assay a, GXD_Specimen s, GXD_InSituResult gr, GXD_ISResultStructure grs
+    where a._AssayType_key in (1,2,3,4,5,6,8,9)
+    and a._Assay_key = s._Assay_key
+    and s._Specimen_key = gr._Specimen_key
     and gr._Result_key = grs._Result_key
     ''', None)
 
-db.sql('create index specs_idx1 on specstructs (_Specimen_key)', None)
-db.sql('create index specs_idx2 on specstructs (_EMAPA_Term_key)', None)
+db.sql('create index specs_idx1 on specstructs (_result_key)', None)
+
+db.sql('''
+    select ct._celltype_term_key, s._Specimen_key, s._EMAPA_Term_key , s._Stage_key
+    into temporary table specstructs_ct
+    from specstructs s
+    left outer join GXD_ISResultCelltype ct on (s._result_key = ct._result_key)
+    ''', None)
+
+db.sql('create index specs_ct_idx1 on specstructs_ct (_Specimen_key)', None)
+db.sql('create index specs_ct_idx2 on specstructs_ct (_EMAPA_Term_key)', None)
 
 # get the duplicates
 
 db.sql('''
     select * into temporary table dupspecimens 
-    from specstructs 
-    group by _Specimen_key, _EMAPA_Term_key, _Stage_key having count(*) > 1
+    from specstructs_ct 
+    group by _Specimen_key, _EMAPA_Term_key, _Stage_key, _celltype_term_key  having count(*) > 1
     ''', None)
 
 db.sql('create index dupspec_idx1 on dupspecimens (_Specimen_key)', None)
 db.sql('create index dupspec_idx2 on dupspecimens (_EMAPA_Term_key)', None)
+db.sql('create index dupspec_idx3 on dupspecimens (_celltype_term_key)', None)
 
 # get the specimen label _Refs_key and  _Assay_key, and structure for each dup
 
 db.sql('''
-    select distinct ss.*, gs.specimenLabel, ga._Refs_key, gs._Assay_key, s.term, t.stage
-    into temporary table specimens 
-    from dupspecimens ss, GXD_Specimen gs, GXD_Assay ga, VOC_Term s, GXD_TheilerStage t
-    where ss._Specimen_key= gs._Specimen_key 
-    and gs._Assay_key = ga._Assay_key 
-    and ss._EMAPA_Term_key = s._Term_key 
+    select distinct ss.*, gs.specimenLabel, ga._Refs_key, gs._Assay_key, s1.term as emapaTerm, s2.term as celltypeTerm, t.stage
+    into temporary table specimens
+    from dupspecimens ss, GXD_Specimen gs, GXD_Assay ga, VOC_Term s1, VOC_Term s2, GXD_TheilerStage t
+    where ss._Specimen_key= gs._Specimen_key
+    and gs._Assay_key = ga._Assay_key
+    and ss._EMAPA_Term_key = s1._Term_key
+    and ss._celltype_term_key = s2._term_key
     and ss._Stage_key = t._Stage_key
     ''', None)
 
@@ -136,19 +155,19 @@ db.sql('create index dupgel_idx2 on dupgels (_EMAPA_Term_key)', None)
 # get the gel label _Refs_key and  _Assay_key, and structure for each dup
 
 db.sql('''
-    select distinct ss.*, gs.laneLabel, ga._Refs_key, gs._Assay_key, s.term, t.stage
-    into temporary table gels 
+    select distinct ss.*, gs.laneLabel, ga._Refs_key, gs._Assay_key, s.term as emapaTerm, '' as celltypeTerm, t.stage
+    into temporary table gels
     from dupgels ss, GXD_GelLane gs, GXD_Assay ga, VOC_Term s, GXD_TheilerStage t
-    where ss._GelLane_key= gs._GelLane_key 
-    and gs._Assay_key = ga._Assay_key 
-    and ss._EMAPA_Term_key = s._Term_key 
+    where ss._GelLane_key= gs._GelLane_key
+    and gs._Assay_key = ga._Assay_key
+    and ss._EMAPA_Term_key = s._Term_key
     and ss._Stage_key = t._Stage_key
     ''', None)
 
 # get the MGI and Jnum ids
 
 db.sql('''
-    select distinct ss.specimenLabel, ss.term, ss.stage,
+    select distinct ss.specimenLabel, ss.emapaTerm, ss.celltypeTerm, ss.stage,
            a1.accid as mgiID, a2.accid as jnumID, a2.numericPart 
     into temporary table finalSpecimen 
     from specimens ss, ACC_Accession a1, ACC_Accession a2 
@@ -164,12 +183,12 @@ db.sql('''
     ''', None)
 
 db.sql('''
-    select distinct ss.laneLabel, ss.term, ss.stage,
+    select distinct ss.laneLabel, ss.emapaTerm, ss.celltypeTerm, ss.stage,
            a1.accid as mgiID, a2.accid as jnumID, a2.numericPart 
     into temporary table finalGel 
     from gels ss, ACC_Accession a1, ACC_Accession a2 
     where ss._Assay_key = a1._Object_key 
-    and a1._MGIType_key = 8 
+    as.celltypeTermd a1._MGIType_key = 8 
     and a1._LogicalDB_key = 1 
     and a1.prefixPart = 'MGI:' 
     and ss._Refs_key = a2._Object_key 
@@ -179,24 +198,29 @@ db.sql('''
     order by a2.numericPart, ss.laneLabel
     ''', None)
 
+finalDict = {}
 results = db.sql('''
-        (
-        select specimenLabel, term, stage, mgiID, jnumID, numericPart 
-        from finalSpecimen 
-        union 
-        select laneLabel, term, stage, mgiID, jnumID, numericPart from finalGel 
-        )
-        order by numericPart
-        ''', 'auto')
-
+        select specimenLabel, emapaTerm, celltypeTerm, stage, mgiID, jnumID, numericPart 
+        from finalSpecimen''', 'auto')
 for r in results:
+    finalDict[r['numericPart']] = r
+
+results = db.sql('''select laneLabel, emapaTerm, celltypeTerm, stage, mgiID, jnumID, numericPart 
+        from finalGel''', 'auto')
+for r in results:
+    finalDict[r['numericPart']] = r
+
+sortOrder = sorted(finalDict.keys())
+for k in sortOrder:
+    r = finalDict[k]
     fp.write(str.ljust(r['jnumID'], 15))
     fp.write(str.ljust(r['mgiID'], 15))
     fp.write(str.ljust(r['specimenLabel'], 35))
-    fp.write(str.ljust(r['term'], 50))
-    fp.write(str.ljust(str(r['stage']), 5))
+    fp.write(str.ljust(r['emapaTerm'], 50))
+    fp.write(str.ljust(str(r['stage']), 10))
+    fp.write(str.ljust(str(r['celltypeTerm']), 50))
     fp.write(CRT)
 
-fp.write('\n(%d rows affected)\n' % (len(results)))
+fp.write('\n(%d rows affected)\n' % (len(finalDict)))
 
 reportlib.finish_nonps(fp)	# non-postscript file
--- mgd/RECOMB_SameStructure.py
+++ mgd/RECOMB_SameStructure.py
@@ -11,6 +11,9 @@
 #		J number of paper
 #		MGI id of assay
 #		Name of offending specimen/gel lane
+#               EMAPA Term
+#               Stage
+#               Cell Type Term
 #
 #	Sort by: J number
 #
@@ -26,6 +29,9 @@
 #	- all private SQL reports require the header
 #
 # History:
+#
+# sc    12/09/2021
+#       - YAKS project; added cell type term column
 #
 # lec	01/08/2016
 #	- TR12223/gxd anatomy II
@@ -58,19 +64,21 @@ fp.write(str.ljust('J:', 35))
 fp.write(str.ljust('Assay ID', 35))
 fp.write(str.ljust('Label', 45))
 fp.write(str.ljust('EMAPA-Term', 50))
-fp.write(str.ljust('Stage', 5))
+fp.write(str.ljust('Stage', 10))
+fp.write(str.ljust('CellType-Term', 50))
 fp.write(CRT)
 fp.write(str.ljust('----------', 35))
 fp.write(str.ljust('----------', 35))
 fp.write(str.ljust('----------', 45))
 fp.write(str.ljust('----------', 50))
+fp.write(str.ljust('----------', 10))
 fp.write(str.ljust('-----', 50))
 fp.write(2*CRT)
 
 # get the all specimen/structure pairs
 
 db.sql('''
-        select gr._Specimen_key, grs._EMAPA_Term_key, grs._Stage_key
+        select gr._Specimen_key, grs._EMAPA_Term_key, grs._Stage_key, gr._result_key
         into temporary table specstructs 
         from GXD_Assay a, GXD_Specimen s, GXD_InSituResult gr, GXD_ISResultStructure grs 
         where a._AssayType_key in (10,11) 
@@ -79,29 +87,40 @@ db.sql('''
         and gr._Result_key = grs._Result_key
         ''', None)
 
-db.sql('create index specs_idx1 on specstructs (_Specimen_key)', None)
-db.sql('create index specs_idx2 on specstructs (_EMAPA_Term_key)', None)
+db.sql('create index specs_idx1 on specstructs (_result_key)', None)
+
+db.sql('''
+    select ct._celltype_term_key, s._Specimen_key, s._EMAPA_Term_key , s._Stage_key
+    into temporary table specstructs_ct
+    from specstructs s
+    left outer join GXD_ISResultCelltype ct on (s._result_key = ct._result_key)
+    ''', None)
+
+db.sql('create index specs_ct_idx1 on specstructs (_Specimen_key)', None)
+db.sql('create index specs_ct_idx2 on specstructs (_EMAPA_Term_key)', None)
 
 # get the duplicates
 
 db.sql('''
         select * into temporary table dupspecimens 
-        from specstructs 
-        group by _Specimen_key, _EMAPA_Term_key, _Stage_key having count(*) > 1
+        from specstructs_ct
+        group by _Specimen_key, _EMAPA_Term_key, _Stage_key, _celltype_term_key having count(*) > 1
         ''', None)
 
 db.sql('create index dups_idx1 on dupspecimens (_Specimen_key)', None)
 db.sql('create index dups_idx2 on dupspecimens (_EMAPA_Term_key)', None)
+db.sql('create index dupspec_idx3 on dupspecimens (_celltype_term_key)', None)
 
 # get the specimen label _Refs_key and  _Assay_key, and structure for each dup
 
 db.sql('''
-        select distinct ss.*, gs.specimenLabel, ga._Refs_key, gs._Assay_key, s.term, t.stage
+        select distinct ss.*, gs.specimenLabel, ga._Refs_key, gs._Assay_key, s1.term as emapaTerm, s2.term as celltypeTerm, t.stage
         into temporary table specimens 
-        from dupspecimens ss, GXD_Specimen gs, GXD_Assay ga, VOC_Term s, GXD_TheilerStage t
+        from dupspecimens ss, GXD_Specimen gs, GXD_Assay ga, VOC_Term s1, VOC_Term s2, GXD_TheilerStage t
         where ss._Specimen_key= gs._Specimen_key 
         and gs._Assay_key = ga._Assay_key 
-        and ss._EMAPA_Term_key = s._Term_key 
+        and ss._EMAPA_Term_key = s1._Term_key 
+        and ss._celltype_term_key = s2._Term_key
         and ss._Stage_key = t._Stage_key
         ''', None)
 
@@ -133,7 +152,7 @@ db.sql('create index dupgel_idx2 on dupgels (_EMAPA_Term_key)', None)
 # get the gel label _Refs_key and  _Assay_key, and structure for each dup
 
 db.sql('''
-        select distinct ss.*, gs.laneLabel, ga._Refs_key, gs._Assay_key, s.term, t.stage
+        select distinct ss.*, gs.laneLabel, ga._Refs_key, gs._Assay_key, s.term as emapaTerm, '' as celltypeTerm, t.stage
         into temporary table gels 
         from dupgels ss, GXD_GelLane gs, GXD_Assay ga, VOC_Term s, GXD_TheilerStage t
         where ss._GelLane_key= gs._GelLane_key 
@@ -145,7 +164,7 @@ db.sql('''
 # get the MGI and Jnum ids
 
 db.sql('''
-        select distinct ss.specimenLabel, ss.term, ss.stage, a1.accid as mgiID, a2.accid as jnumID, a2.numericPart 
+        select distinct ss.specimenLabel, ss.emapaTerm, ss.celltypeTerm, ss.stage, a1.accid as mgiID, a2.accid as jnumID, a2.numericPart 
         into temporary table finalSpecimen 
         from specimens ss, ACC_Accession a1, ACC_Accession a2 
         where ss._Assay_key = a1._Object_key 
@@ -160,7 +179,7 @@ db.sql('''
         ''', None)
 
 db.sql('''
-        select distinct ss.laneLabel, ss.term, ss.stage, a1.accid as mgiID, a2.accid as jnumID, a2.numericPart 
+        select distinct ss.laneLabel, ss.emapaTerm, ss.celltypeTerm, ss.stage, a1.accid as mgiID, a2.accid as jnumID, a2.numericPart 
         into temporary table finalGel 
         from gels ss, ACC_Accession a1, ACC_Accession a2 
         where ss._Assay_key = a1._Object_key 
@@ -174,23 +193,29 @@ db.sql('''
         order by a2.numericPart, ss.laneLabel
         ''', None)
 
+finalDict = {}
 results = db.sql('''
-        (
-        select specimenLabel, term, stage, mgiID, jnumID, numericPart from finalSpecimen 
-        union 
-        select laneLabel, term, stage, mgiID, jnumID, numericPart from finalGel 
-        )
-        order by numericPart
-        ''', 'auto')
-
+        select specimenLabel, emapaTerm, celltypeTerm, stage, mgiID, jnumID, numericPart
+        from finalSpecimen''', 'auto')
 for r in results:
+    finalDict[r['numericPart']] = r
+
+results = db.sql('''select laneLabel, emapaTerm, celltypeTerm, stage, mgiID, jnumID, numericPart
+        from finalGel''', 'auto')
+for r in results:
+    finalDict[r['numericPart']] = r
+
+sortOrder = sorted(finalDict.keys())
+for k in sortOrder:
+    r = finalDict[k]
     fp.write(str.ljust(r['jnumID'], 35))
     fp.write(str.ljust(r['mgiID'], 35))
     fp.write(str.ljust(r['specimenLabel'], 45))
-    fp.write(str.ljust(r['term'], 50))
-    fp.write(str.ljust(str(r['stage']), 5))
+    fp.write(str.ljust(r['emapaTerm'], 50))
+    fp.write(str.ljust(str(r['stage']), 10))
+    fp.write(str.ljust(str(r['celltypeTerm']), 50))
     fp.write(CRT)
 
-fp.write('\n(%d rows affected)\n' % (len(results)))
+fp.write('\n(%d rows affected)\n' % (len(finalDict)))
 
 reportlib.finish_nonps(fp)	# non-postscript file
