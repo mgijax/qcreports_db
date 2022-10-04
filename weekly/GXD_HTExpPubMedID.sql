03cbc23f3f771bc15ca13e22c3348288c086b1fa
Lori Corbani <lori.corbani@jax.org>
2022-10-04 06:57:26 -0400

    wts2-992/identify HT index experiments with associated PMID used by GXD in MasterBib

--- weekly/GXD_HTExpPubMedID.sql
+++ weekly/GXD_HTExpPubMedID.sql
@@ -1,9 +1,11 @@
 \echo ''
 \echo 'HT Experiments with PMIDs used by GXD'
+\echo 'sort:  Experiment Type, Evaluation State (desc), Curation State (desc), Experiment ID'
 \echo ''
 
-select a.accid as ExperimentID, c.jnumid, p.value, t.term as evaluationstate, t2.term as experimenttype, e.name
-from gxd_htexperiment e, voc_term t, voc_term t2, acc_accession a, mgi_property p, bib_citation_cache c
+select a.accid as ExperimentID, c.jnumid, p.value, t.term as evaluationstate, t3.term as curationstate, t2.term as experimenttype, e.name
+from gxd_htexperiment e, voc_term t, voc_term t2, voc_term t3, acc_accession a, mgi_property p, 
+        bib_citation_cache c, bib_workflow_relevance wr
 where e._evaluationstate_key = t._term_key
 and e._experimenttype_key = t2._term_key
 and e._experiment_key = a._object_key
@@ -19,5 +21,8 @@ and exists (select 1 from bib_workflow_status s
         and s._group_key = 31576665
         and s._status_key in (31576671, 31576674, 31576673)
         )
-order by t2.term, t.term desc, a.accid
+and c._refs_key = wr._refs_key
+and wr.isCurrent = 1
+and wr._relevance_key = t3._term_key
+order by t2.term, t.term desc, t3.term asc, a.accid
 
