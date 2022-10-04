e85d4b8ce92791614d6dd45983f7066af917d351
Lori Corbani <lori.corbani@jax.org>
2022-10-04 06:46:58 -0400

    wts2-992/identify HT index experiments with associated PMID used by GXD in MasterBib

---
+++ weekly/GXD_HTExpPubMedID.sql
@@ -0,0 +1,23 @@
+\echo ''
+\echo 'HT Experiments with PMIDs used by GXD'
+\echo ''
+
+select a.accid as ExperimentID, c.jnumid, p.value, t.term as evaluationstate, t2.term as experimenttype, e.name
+from gxd_htexperiment e, voc_term t, voc_term t2, acc_accession a, mgi_property p, bib_citation_cache c
+where e._evaluationstate_key = t._term_key
+and e._experimenttype_key = t2._term_key
+and e._experiment_key = a._object_key
+and a._mgitype_key = 42
+and a._logicaldb_key in (189,190)
+and a.preferred = 1
+and p._propertytype_key = 1002
+and e._experiment_key = p._object_key
+and p.value = c.pubmedid
+and exists (select 1 from bib_workflow_status s
+        where c._refs_key = s._refs_key
+        and s.isCurrent = 1
+        and s._group_key = 31576665
+        and s._status_key in (31576671, 31576674, 31576673)
+        )
+order by t2.term, t.term desc, a.accid
+
