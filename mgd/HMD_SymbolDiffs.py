27df79b02ef2cfccc9be05550f89f99730e11660
Lori Corbani <lori.corbani@jax.org>
2022-11-08 10:54:20 -0500

    wts2-1031/Divide data in nomen QC reports/HMD_SymbolDiffs.py

--- mgd/HMD_SymbolDiffs.py
+++ mgd/HMD_SymbolDiffs.py
@@ -16,6 +16,9 @@
 #
 # History:
 #
+# lec   11/8/2022
+#       wts2-1031/Divide data in nomen QC reports
+#
 # sc    02/11/2021
 #       TR13349 - B39 project. Update to use alliance direct homology
 #
@@ -32,7 +35,6 @@
  
 import sys
 import os
-import string
 import mgi_utils
 import reportlib
 import db
@@ -100,28 +102,90 @@ def runQueries(includeRiken):
     for r in results:
         mgiID[r['m_Marker_key']] = r['accID']
 
+    db.sql('drop table if exists results_onetoone', None)
+    db.sql('drop table if exists results_onetomany', None)
+    db.sql('drop table if exists results_manytomany', None)
     db.sql('drop table if exists results', None)
 
+    # one-to-one
     db.sql('''
-        select h.*, e.status as hstatus
-        into temporary table results 
-        from homology h, radar.DP_EntrezGene_Info e 
+        WITH onemarker AS (
+        select h.msymbol
+        from homology h, radar.DP_EntrezGene_Info e
+        where h.hsymbol = e.symbol and e.taxID = 9606
+        group by h.msymbol having count(*) = 1
+        )
+        select h.hsymbol
+        into temporary table results_onetoone
+        from onemarker m, homology h, radar.DP_EntrezGene_Info e 
         where h.hsymbol = e.symbol and e.taxID = 9606 
+        and m.msymbol = h.msymbol
+        group by h.hsymbol having count(*) = 1
         union 
-        select h.*, '?' as hstatus
-        from homology h 
-        where not exists (select 1 from radar.DP_EntrezGene_Info e 
-        where h.hsymbol = e.symbol and e.taxID = 9606)
+        select h.hsymbol
+        from onemarker m, homology h 
+        where not exists (select 1 from radar.DP_EntrezGene_Info e where h.hsymbol = e.symbol and e.taxID = 9606)
+        and m.msymbol = h.msymbol
+        group by h.hsymbol having count(*) = 1
         ''', None)
 
-def report1(fp, includeRiken = 0):
+    # one-to-many
+    db.sql('''
+        WITH onemarker AS (
+        select h.msymbol
+        from homology h, radar.DP_EntrezGene_Info e
+        where h.hsymbol = e.symbol and e.taxID = 9606
+        group by h.msymbol having count(*) = 1
+        )
+        select h.hsymbol
+        into temporary table results_onetomany
+        from onemarker m, homology h, radar.DP_EntrezGene_Info e 
+        where h.hsymbol = e.symbol and e.taxID = 9606 
+        and m.msymbol = h.msymbol
+        group by h.hsymbol having count(*) > 1
+        union 
+        select h.hsymbol
+        from onemarker m, homology h 
+        where not exists (select 1 from radar.DP_EntrezGene_Info e where h.hsymbol = e.symbol and e.taxID = 9606)
+        and m.msymbol = h.msymbol
+        group by h.hsymbol having count(*) > 1
+        ''', None)
+
+    # many-to-many : everything not in the results_onetoone
+    db.sql('''
+        select h.hsymbol
+        into temporary table results_manytomany
+        from homology h, radar.DP_EntrezGene_Info e 
+        where h.hsymbol = e.symbol and e.taxID = 9606 
+        and not exists (select 1 from results_onetoone r where r.hsymbol = h.hsymbol)
+        and not exists (select 1 from results_onetomany r where r.hsymbol = h.hsymbol)
+        union 
+        select h.hsymbol
+        from homology h 
+        where not exists (select 1 from radar.DP_EntrezGene_Info e where h.hsymbol = e.symbol and e.taxID = 9606)
+        and not exists (select 1 from results_onetoone r where r.hsymbol = h.hsymbol)
+        and not exists (select 1 from results_onetomany r where r.hsymbol = h.hsymbol)
+        union
+        select h.hsymbol 
+        from results_onetomany r, homology h
+        where r.hsymbol = h.hsymbol
+        ''', None)
+
+
+def report1(fp, includeRiken, isOneToOne):
 
     if includeRiken:
         riken = 'RIKEN and Gene Models only'
     else:
         riken = 'excludes RIKEN and Gene Models'
 
-    fp.write('MGI Symbols differing from Human Ortholog Symbols, ' + riken + ' (#1)' + CRT)
+    if isOneToOne == 1:
+        other_results = 'results_onetoone'
+        fp.write('\nMGI Symbols differing from Human Ortholog Symbols, ' + riken + ';one-to-one' + CRT)
+    else:
+        other_results = 'results_manytomany'
+        fp.write('\nMGI Symbols differing from Human Ortholog Symbols, ' + riken + ';many-to-many or one-to-many' + CRT)
+
     fp.write('(sorted by modification date of human symbol, symbol status, mouse symbol)' + 2*CRT)
 
     fp.write(str.ljust('MGI Symbol', 32))
@@ -139,6 +203,18 @@ def report1(fp, includeRiken = 0):
     fp.write(str.ljust('-' * 30, 32))
     fp.write(CRT)
 
+    db.sql('''
+        select h.*, e.status as hstatus
+        into temporary table results 
+        from homology h, radar.DP_EntrezGene_Info e 
+        where h.hsymbol = e.symbol and e.taxID = 9606 
+        and exists (select 1 from %s r where r.hsymbol = h.hsymbol)
+        union 
+        select h.*, '?' as hstatus
+        from homology h 
+        where not exists (select 1 from radar.DP_EntrezGene_Info e where h.hsymbol = e.symbol and e.taxID = 9606)
+        and exists (select 1 from %s r where r.hsymbol = h.hsymbol)
+        ''' % (other_results, other_results), None)
 
     results = db.sql('select * from results order by modification_date desc, hstatus desc, msymbol', 'auto')
 
@@ -163,7 +239,7 @@ def report2(fp, includeRiken):
     else:
         riken = 'excludes RIKEN and Gene Models'
 
-    fp.write('MGI Symbols differing from Human Ortholog Symbols, ' + riken + ' (#2)' + CRT)
+    fp.write('MGI Symbols differing from Human Ortholog Symbols, ' + riken + CRT)
     fp.write('(sorted by human status, mouse status, mouse symbol)' + 2*CRT)
 
     fp.write(str.ljust('MGI Symbol', 32))
@@ -177,6 +253,16 @@ def report2(fp, includeRiken):
     fp.write(str.ljust('-' * 12, 15))
     fp.write(CRT)
 
+    db.sql('''
+        select h.*, e.status as hstatus
+        into temporary table results 
+        from homology h, radar.DP_EntrezGene_Info e 
+        where h.hsymbol = e.symbol and e.taxID = 9606 
+        union 
+        select h.*, '?' as hstatus
+        from homology h 
+        where not exists (select 1 from radar.DP_EntrezGene_Info e where h.hsymbol = e.symbol and e.taxID = 9606)
+        ''', None)
 
     results = db.sql('select * from results order by hstatus desc, mstatus desc, msymbol', 'auto')
 
@@ -198,10 +284,15 @@ def report2(fp, includeRiken):
 
 fp1 = reportlib.init('HMD_SymbolDiffs1', outputdir = os.environ['QCOUTPUTDIR'])
 runQueries(includeRiken = 0)
-report1(fp1, includeRiken = 0)
+report1(fp1, includeRiken = 0, isOneToOne = 1)
+reportlib.finish_nonps(fp1)
+
+fp2 = reportlib.init('HMD_SymbolDiffs2', outputdir = os.environ['QCOUTPUTDIR'])
+runQueries(includeRiken = 0)
+report1(fp2, includeRiken = 0, isOneToOne = 0)
 reportlib.finish_nonps(fp1)
 
 fp3 = reportlib.init('HMD_SymbolDiffs3', outputdir = os.environ['QCOUTPUTDIR'])
 runQueries(includeRiken = 1)
-report1(fp3, includeRiken = 1)
+report2(fp3, includeRiken = 1)
 reportlib.finish_nonps(fp3)
