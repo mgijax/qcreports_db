4c1e74010487f0beae87a7c7a76a7e9978670458
Lori Corbani <lori.corbani@jax.org>
2022-11-30 13:55:03 -0500

    fl2/gxd secondary triage

--- weekly/WF_GXD_secondary.py
+++ weekly/WF_GXD_secondary.py
@@ -17,6 +17,9 @@
 #       Section 2
 #       Relevanve = discard
 #
+#       Section 3
+#       <=500 characters in body extracted text
+#
 #       In each section:
 #       mgiid
 #       pubmedid
@@ -157,6 +160,44 @@ process(sql % (70594667))
 
 fp.write('\nDiscard - papers routed to GXD\n')
 process(sql % (70594666))
+
+#
+# gxd workflow status = Not Routed
+# extractedText (body) <= 500 characters
+# relevance = Keep, Discard
+# reference type = Peer Reviewed Article
+# confidence >= -2.75
+#
+fp.write('\n<=500 characters in body extracted text\n')
+fp.write('MGI ID, pmid, Relevance, Confidance\n')
+results = db.sql('''
+select c.mgiid, c.pubmedid, c.relevanceterm, v.confidence
+from bib_citation_cache c, bib_workflow_relevance v
+where c.relevanceterm in ('keep', 'discard')
+and c.referencetype = 'Peer Reviewed Article'
+and c._refs_key = v._refs_key
+and v.isCurrent = 1
+and v.confidence >= -2.75
+and exists (select 1 from bib_workflow_status s 
+        where c._refs_key = s._refs_key
+        and s.isCurrent = 1
+        and s._group_key = 31576665
+        and s._status_key = 31576669
+        )
+and exists (select 1 from bib_workflow_data d 
+        where c._refs_key = d._refs_key
+        and d._extractedtext_key = 48804490
+        and char_length(d.extractedText) <= 500
+        )
+order by c.mgiid
+''', 'auto')
+for r in results:
+        fp.write(r['mgiid'] + TAB)
+        fp.write(str(r['pubmedid']) + TAB)
+        fp.write(r['relevanceterm'] + TAB)
+        fp.write(str(r['confidence']) + CRT)
+
+fp.write('\n(%d rows affected)\n' % len(results))
 
 reportlib.finish_nonps(fp)
 
