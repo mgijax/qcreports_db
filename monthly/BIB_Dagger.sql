37949448e902c0b618d2d4fe1c14dc35ccb47243
Lori Corbani <lori.corbani@jax.org>
2022-12-14 12:00:49 -0500

    wts2-1073/Titles come in from pubmed that say 'dagger'

---
+++ monthly/BIB_Dagger.sql
@@ -0,0 +1,17 @@
+\echo ''
+\echo 'Dagger character in title'
+\echo ''
+\echo 'Directions:'
+\echo ''
+\echo 'If the word "dagger" or the phrase "double dagger" is at the end of the title, it is usually because there is an icon at the end of the title that Pubmed changes to the word "dagger".'
+\echo ''
+\echo 'Check the pdf - if the word dagger is not part of the title, then remove it from the title field in the database. There may also be parentheses that should be removed.'
+\echo ''
+
+select c.mgiid, r.title
+from bib_refs r, bib_citation_cache c
+where
+(r.title like '%double dagger%' or r.title like '%dagger.' or r.title like '%(dagger).')
+and r._refs_key = c._refs_key
+order by c.mgiid
+
