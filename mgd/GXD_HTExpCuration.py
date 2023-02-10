50ee5eb8dfb257b8f5f1130840f61672ee0f1a50
Lori Corbani <lori.corbani@jax.org>
2023-02-10 10:04:16 -0500

    wts2-1097/fl2-203/add Experiments where Curation State=Done and Evaluation !=Yes

--- mgd/GXD_HTExpCuration.py
+++ mgd/GXD_HTExpCuration.py
@@ -1,12 +1,17 @@
-
 '''
 #
 # TR 12370
 #
 # Report:
 #
-# List of experiments whose Curation State = Done but Study Type or Experimental
-#	 Variables = Not Curated or Experiment Type = Not Resolved
+# List of experiments whose 
+#       (Curation State = Done)
+#       AND (
+#               Study Type=Not Curated 
+#               OR Experimental Variables=Not Curated 
+#               OR Experiment Type=Not Resolved
+#               OR Evaluation != Yes
+#       )
 #
 # Usage:
 #       GXD_HTExpCuration.py
@@ -15,14 +20,16 @@
 #
 # History:
 #
-# sc   11/04/2061
+# lec   02/09/2023
+#       - wts2-1097/fl2-203/add Experiments where Curation State=Done and Evaluation !=Yes
+#
+# sc   11/04/2016
 #       - TR12370 created
 #
 '''
 
 import sys
 import os
-import string
 import reportlib
 import db
 
@@ -33,46 +40,33 @@ SPACE = reportlib.SPACE
 TAB = reportlib.TAB
 PAGE = reportlib.PAGE
 
-notCuratedStudyTypeList = []
-notCuratedVarList = []
-expTypeNRList = []
-doneCurState = 20475421
+fp = reportlib.init(sys.argv[0], '\nExperiments where Curation State=Done and (Study Type=Not Curated OR Experimental Variables=Not Curated OR Experiment Type=Not Resolved OR Evaluation!= Yes)', os.environ['QCOUTPUTDIR'])
 
-fp = reportlib.init(sys.argv[0], 'Experiments where Curation State=Done and (Study Type=Not Curated OR Experimental Variables=Not Curated OR Experiment Type=Not Resolved)', os.environ['QCOUTPUTDIR'])
-
-results = db.sql('''select _Experiment_key
-        from GXD_HTExperiment
-        where _StudyType_key = 20475461''', 'auto') # Not Curated
-for r in results:
-    notCuratedStudyTypeList.append(r['_Experiment_key'])
-
-results = db.sql('''select _Experiment_key from GXD_HTExperimentVariable
-        where _Term_key = 20475439''', 'auto') # Not Curated
-for r in results:
-        notCuratedVarList.append(r['_Experiment_key'])
-
-
-results = db.sql('''select _Experiment_key 
-        from GXD_HTExperiment
-        where _ExperimentType_key = 20475438''', 'auto') # Not Resolved
-for r in results:
-    expTypeNRList.append(r['_Experiment_key'])
-
-
-results = db.sql('''select a.accid, hte._Experiment_key
-        from ACC_Accession a, GXD_HTExperiment hte
+results = db.sql('''
+        -- curation state = Done
+        select distinct a.accid, hte._Experiment_key
+        from ACC_Accession a, GXD_HTExperiment hte, GXD_HTExperimentVariable hv
         where hte._CurationState_key = 20475421
+        and hte._Experiment_key = hv._Experiment_key
         and hte._Experiment_key = a._Object_key
         and a._MGIType_key = 42
         and a._LogicalDB_key in (189, 190)
-        and a.preferred = 1''', 'auto') # Done
+        and a.preferred = 1
+        and (
+                -- study type =  Not Curated
+                hte._StudyType_key = 20475461
+                -- experiment type = Not Resolved
+                or hte._ExperimentType_key = 20475438
+                -- evaluation state != yes
+                or hte._evaluationstate_key != 20225942
+                --  experimental variables = Not Curated
+                or hv._Term_key = 20475439
+        )
+        ''', 'auto')
 
-ct = 0
 for r in results:
     exptKey = r['_Experiment_key']
     accid = r['accid']
-    if exptKey in notCuratedStudyTypeList or exptKey in notCuratedVarList or exptKey in expTypeNRList:
-        ct += 1
-        fp.write('%s%s' % (accid, CRT))
+    fp.write('%s%s' % (accid, CRT))
 
-fp.write('%sTotal:%s%s' % (CRT, ct, CRT))
+fp.write('%sTotal:%s%s' % (CRT, len(results), CRT))
