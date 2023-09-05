6faf938edb232a88ec9ed2a26e3d179a62304ffc
Lori Corbani <lori.corbani@jax.org>
2023-09-05 11:24:14 -0400

    wts2-1268/fl2503/2 new GXD QC reports for fullcoding priority

--- monthly/GXD_FullCodeableLargeScale.py
+++ monthly/GXD_FullCodeableLargeScale.py
@@ -7,7 +7,7 @@
 #
 #       J# for large scale screens: see below
 #
-#       List all markers in these J# 
+#       List all markers with only full-coded is from large scale J# 
 #
 #       MGI ID of marker gene symbol
 #
@@ -39,7 +39,8 @@ fp.write('''
         J:101679 J:122989 J:140465 J:141291 J:143778 J:153498 J:157819 J:162220 J:171409
         J:215487 J:226028 J:228563 J:279207 J:46439 J:80501 J:80502 J:85124 J:91257 J:93300
 
-        For each marker list the number of papers: indexed, fullcoded
+        index records by marker = # of all references from gxd_index
+        full-coded by marker = # of large scale references from gxd_expression_cache
 
 ''')
 
@@ -54,54 +55,53 @@ fp.write(SPACE)
 fp.write(CRT*2)
 
 #
-# count: all genes in the GXD index
+# count: all genes in the GXD index by marker
 #
-db.sql('''
-        select g._Marker_key, count(*) as idx_count 
-        into temporary table indexcount
-        from GXD_Index g, BIB_Citation_Cache c
-        where c.jnumid in (
-        'J:101679', 'J:122989', 'J:140465', 'J:141291', 'J:143778', 'J:153498', 'J:157819', 'J:162220', 'J:171409', 
-        'J:215487', 'J:226028', 'J:228563', 'J:279207', 'J:46439', 'J:80501', 'J:80502', 'J:85124', 'J:91257', 'J:93300'
-        )
-        and c._refs_key = g._refs_key
-        group by g._Marker_key
-        ''', None)
-db.sql('create index indexed_idx1 on indexcount(_Marker_key)', None)
-
-#
-# all genes in the GXD index
-#
-
-db.sql('''
-        select distinct g._Marker_key, m.symbol, a.accID, g.idx_count
-        into temporary table allgenes
-        from indexcount g, MRK_Marker m, ACC_Accession a
-        where g._Marker_key = m._Marker_key
-        and g._Marker_key = a._Object_key
-        and a._MGIType_key = 2
-        and a._LogicalDB_key = 1
-        and a.preferred = 1
-        ''', None)
-
-db.sql('create index allgenes_idx1 on allgenes(_Marker_key)', None)
-
-#
-# count of total index records that have been full-coded
-#
-
-fullcodedrecords = {}
+indexcount = {}
 results = db.sql('''
-        select distinct g._Marker_key, g._Refs_key 
-        from GXD_Assay g
-        where _AssayType_key not in (10,11)
-        ''', 'auto')
+select g._Marker_key, count(*) as idx_count 
+from GXD_Index g
+group by g._Marker_key
+''', 'auto')
 for r in results:
-    key = r['_Marker_key']
-    value = r['_Refs_key']
-    if key not in fullcodedrecords:
-        fullcodedrecords[key] = []
-    fullcodedrecords[key].append(value)
+        key = r['_marker_key']
+        value = r['idx_count']
+        indexcount[key] = value
+
+#
+# count:  all full-coded genes in large scale references
+# group by marker, reference
+#
+db.sql('''
+select a._marker_key, a._refs_key, count(*) as idx_count
+into temp table refcount
+from GXD_Assay a, BIB_Citation_Cache c
+where c.jnumid in (
+'J:101679', 'J:122989', 'J:140465', 'J:141291', 'J:143778', 'J:153498', 'J:157819', 'J:162220', 'J:171409', 
+'J:215487', 'J:226028', 'J:228563', 'J:279207', 'J:46439', 'J:80501', 'J:80502', 'J:85124', 'J:91257', 'J:93300'
+)
+and c._refs_key = a._refs_key
+and exists (select 1 from GXD_Expression g where a._Assay_key = g._Assay_key)
+and a._assaytype_key not in (10,11)
+group by a._marker_key, a._refs_key
+''', None)
+db.sql('create index indexed_idx1 on refcount(_marker_key)', None)
+
+#
+# group by marker only
+#
+db.sql('''
+select g._marker_key, m.symbol, a.accID, count(g.idx_count) as idx_count
+into temporary table allgenes
+from refcount g, MRK_Marker m, ACC_Accession a
+where g._marker_key = m._marker_key
+and g._marker_key = a._object_key
+and a._mgitype_key = 2
+and a._logicaldb_key = 1
+and a.preferred = 1
+group by g._marker_key, m.symbol, a.accID
+''', None)
+db.sql('create index allgenes_idx1 on allgenes(_marker_key)', None)
 
 #
 # to print...
@@ -116,15 +116,9 @@ for r in results:
     fp.write(SPACE)
     fp.write(str.ljust(r['symbol'], 35))
     fp.write(SPACE)
-    fp.write(str.ljust(str(r['idx_count']), 20))
+    fp.write(str.ljust(str(indexcount[key]), 20))
     fp.write(SPACE)
-
-    if key in fullcodedrecords:
-        fp.write(str.ljust(str(len(fullcodedrecords[key])), 20))
-    else:
-        fp.write(str.ljust('0', 20))
-
-    fp.write(CRT)
+    fp.write(str.ljust(str(r['idx_count']), 20) + CRT)
         
 fp.write('\n(%d rows affected)\n' % (len(results)))
 reportlib.finish_nonps(fp)
