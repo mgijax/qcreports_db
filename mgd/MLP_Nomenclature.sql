8a8a10cd2a7b5b521b976acf63a89bad20763d42
Lori Corbani <lori.corbani@jax.org>
2023-09-20 14:54:46 -0400

    wts2-1281/fl2-544/Add a column to the results of Strains report #14

--- mgd/MLP_Nomenclature.sql
+++ mgd/MLP_Nomenclature.sql
@@ -8,6 +8,7 @@
 select substring(s.strain,1,60) as strain, 
        substring(m.symbol,1,25) as marker, 
        substring(a.symbol,1,25) as allele,
+       array_to_string(array_agg(distinct aa.accid),',') as externalAccId,
        s.modification_date
 from PRB_Strain s
      INNER JOIN PRB_Strain_NeedsReview_View n on (s._Strain_key = n._Object_key
@@ -15,6 +16,8 @@ from PRB_Strain s
      LEFT OUTER JOIN PRB_Strain_Marker sm on (s._Strain_key = sm._Strain_key)
      LEFT OUTER JOIN MRK_Marker m on (sm._Marker_key = m._Marker_key)
      LEFT OUTER JOIN ALL_Allele a on (sm._Allele_key = a._Allele_key)
+     LEFT OUTER JOIN ACC_Accession aa on (s._Strain_key = aa._Object_key and aa._MGIType_key = 10 and aa._LogicalDB_key != 1)
+group by s.strain, m.symbol, a.symbol, s.modification_date
 order by s.modification_date desc, strain
 ;
 
