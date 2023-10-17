3af340bc3a32f498f18fba8cef2814c9c78bf32c
Sharon Cousins Giannatto <sharon.giannatto@jax.org>
2023-10-17 11:32:13 -0400

    fixed bug in _marker__s_status_key

--- mgd/MRK_Withdrawn.sql
+++ mgd/MRK_Withdrawn.sql
@@ -8,7 +8,7 @@ substring(symbol,1,30) as "New Symbol",
 substring(markerName,1,50) as "New Name",
 to_char(event_date, 'MM/dd/yyyy') as "Event Date"
 from MRK_History_View
-where _Marker_Event_key in (2,3,4,5,6)
+where _Marker_Event_key in (106563605, 106563606, 106563607, 106563608, 106563609)
 and event_date >= (now() + interval '-3 day') and event_date <= now()
 order by symbol
 ;
