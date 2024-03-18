8c1f0728427da372f90ad1fc1edac3ec23ceaf51
Lori Corbani <lori.corbani@jax.org>
2024-03-18 14:39:09 -0400

    wts2-1441/fl3-787/custom report listing HT Experiment Notes for experiments whose curation status = done

---
+++ pwi/GXD_HTExperimentCuratedNote.py
@@ -0,0 +1,48 @@
+
+'''
+#
+# GXD HT Experiment Notes for Curated Experiments
+#
+# This report would display 
+#
+# Report name: GXD HT Experiment Note
+#       Experiment ID
+#       Note
+#
+'''
+ 
+import sys
+import os
+import db
+import reportlib
+
+#db.setTrace()
+
+CRT = reportlib.CRT
+TAB = reportlib.TAB
+
+results = db.sql('''
+select e._experiment_key,
+a.accid as ExperimentID, 
+n.note
+from gxd_htexperiment e, acc_accession a, mgi_note n
+where e._experiment_key = a._object_key 
+and a._mgitype_key = 42 
+and a._logicaldb_key in (189,190) 
+and a.preferred = 1 
+and e._curationstate_key = 20475421
+and e._experiment_key = n._object_key 
+and n._notetype_key = 1047 
+order by ExperimentID
+'''
+
+sys.stdout.write('ExperimentID' + TAB)
+sys.stdout.write('note' + CRT)
+
+for r in results:
+        sys.stdout.write(r['ExperimentID'] + TAB)
+        note = r['note'].replace('\n', ' ').replace('\t', ' ')
+        sys.stdout.write(note + CRT)
+
+sys.stdout.flush()
+
