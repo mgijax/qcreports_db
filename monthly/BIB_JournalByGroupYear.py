7e5198c1eefb9022f8ac16dbde6435c3df372f0c
Lori Corbani <lori.corbani@jax.org>
2024-03-22 11:48:13 -0400

    wts2-1447/wts2-1448/wts2-1449/new monthly lit triage/curation metrics

---
+++ monthly/BIB_JournalByGroupYear.py
@@ -0,0 +1,148 @@
+#
+# Report: BIB_JournalByGroupYear.py
+#
+# Lit triage: journals x curatorial group
+#
+# The purpose of this report is to generate journal counts by group:
+#       AP, GXD, GO, and Tumor.
+# 
+# * one report per group
+#       Lit triage: journals x AP
+#       Lit triage: journals x GXD
+#       Lit triage: journals x GO
+#       Lit triage: journals x Tumor
+#
+# 1. Routed records (each year for group)
+# 2. Chosen records (each year for group)
+# 3. Indexed records (each year for group)
+# 4. Full-coded records (each year for group)
+# 5. Rejected records (each year for group)
+# 6. Not routed records (each year for group)
+# 
+
+import sys
+import os
+import copy
+import reportlib
+import db
+
+db.setTrace()
+
+CRT = reportlib.CRT
+TAB = reportlib.TAB
+
+def init():
+        global displayByStatus
+        global displayByYear
+
+        # 
+        # set of unique years
+        #
+        displayByStatus = {}
+        results = db.sql(''' select term from voc_term where _vocab_key = 128 ''', 'auto')
+        for r in results:
+                displayByStatus[r['term']] = 0
+
+        displayByYear = {}
+        results = db.sql('''
+                select distinct r.year
+                from BIB_Refs r
+                where r.year between date_part('year', CURRENT_DATE) - 5 and date_part('year', CURRENT_DATE) 
+                and r.journal is not null
+                order by year
+                ''', 'auto')
+        for r in results:
+                deepCopy = copy.deepcopy(displayByStatus)
+                displayByYear[r['year']] = deepCopy
+
+        #print(displayByYear)
+
+def printHeaders():
+        #
+        # create fp reports
+        # print year headers
+        #
+        global fp1, fp2, fp3, fp4
+
+        fp1 = reportlib.init('BIB_JournalByGroupYear_AP', 'AP - Group reference usage by journal reported by publication year', os.environ['QCOUTPUTDIR'])
+        fp2 = reportlib.init('BIB_JournalByGroupYear_GXD', 'GXD - Group reference usage by journal reported by publication year', os.environ['QCOUTPUTDIR'])
+        fp3 = reportlib.init('BIB_JournalByGroupYear_GO', 'GO - Group reference usage by journal reported by publication year', os.environ['QCOUTPUTDIR'])
+        fp4 = reportlib.init('BIB_JournalByGroupYear_Tumor', 'Tumor - Group reference usage by journal reported by publication year', os.environ['QCOUTPUTDIR'])
+
+        for fp in (fp1, fp2, fp3, fp4):
+                #
+                # write the year
+                #
+                fp.write(TAB)
+                for r in displayByYear:
+                        fp.write('Routed' + ' ' + str(r) + TAB)
+                        fp.write('Chosen' + ' ' + str(r) + TAB)
+                        fp.write('Indexed' + ' ' + str(r) + TAB)
+                        fp.write('Full-coded' + ' ' + str(r) + TAB)
+                        fp.write('Rejected' + ' ' + str(r) + TAB)
+                        fp.write('Not Routed' + ' ' + str(r) + TAB)
+                fp.write(CRT)
+
+def processGroup(fp, group):
+        #
+        # journal -> group -> year -> status
+        #
+        # 1. Routed records (each year for group)
+        # 2. Chosen records (each year for group)
+        # 3. Indexed records (each year for group)
+        # 4. Full-coded records (each year for group)
+        # 5. Rejected records (each year for group)
+        # 6. Not routed records (each year for group)
+        #
+
+        displayByGroup = {}
+
+        results = db.sql('''
+                select r.journal, t1.abbreviation as group, r.year, t2.term as status, count(*) as counter
+                from BIB_Refs r, BIB_Workflow_Status s, VOC_Term t1, VOC_Term t2
+                where r.year between date_part('year', CURRENT_DATE) - 5 and date_part('year', CURRENT_DATE) 
+                and r.journal is not null
+                and r._Refs_key = s._Refs_key
+                and s.isCurrent = 1
+                and s._Group_key = t1._Term_key
+                and t1.abbreviation = '%s'
+                and s._Status_key = t2._Term_key
+                group by r.journal, t1.abbreviation, r.year, t2.term
+                order by r.journal, t1.abbreviation, r.year, t2.term
+        ''' % (group), 'auto')
+
+        for r in results:
+                journal = r['journal']
+                group = r['group']
+                year = r['year']
+                status = r['status']
+                if journal not in displayByGroup:
+                        deepCopy = copy.deepcopy(displayByYear)
+                        displayByGroup[journal] = deepCopy
+                displayByGroup[journal][year][status] = r['counter']
+
+        for r in displayByGroup:
+                fp.write(r + TAB)
+                for y in displayByGroup[r]:
+                        fp.write(str(displayByGroup[r][y]['Routed']) + TAB)
+                        fp.write(str(displayByGroup[r][y]['Chosen']) + TAB)
+                        fp.write(str(displayByGroup[r][y]['Indexed']) + TAB)
+                        fp.write(str(displayByGroup[r][y]['Full-coded']) + TAB)
+                        fp.write(str(displayByGroup[r][y]['Rejected']) + TAB)
+                        fp.write(str(displayByGroup[r][y]['Not Routed']) + TAB)
+                fp.write(CRT)
+                
+#
+# main
+#
+init()
+printHeaders()
+processGroup(fp1, 'AP')
+processGroup(fp2, 'GXD')
+processGroup(fp3, 'GO')
+processGroup(fp4, 'Tumor')
+reportlib.finish_nonps(fp1)
+reportlib.finish_nonps(fp2)
+reportlib.finish_nonps(fp3)
+reportlib.finish_nonps(fp4)
+
---
+++ monthly/BIB_JournalByYearKD.py
@@ -0,0 +1,96 @@
+#
+# Report: BIB_JournalByYear.py
+#
+# WTS2-1449 Lit triage: journals' keep/discard numbers
+#
+# This purpose of this report is to generate journal counts by year where:
+#       . year is within the last 5 years of today's year
+#
+# x axis = year/total, year/keep, year/discard
+# y axis = journal
+#
+
+import sys
+import os
+import copy
+import reportlib
+import db
+
+db.setTrace()
+
+CRT = reportlib.CRT
+TAB = reportlib.TAB
+
+# 
+# set of unique years
+#
+displayByRelevance = {}
+results = db.sql(''' select term from voc_term where _vocab_key = 149 and term != 'Not Specified' order by term desc ''', 'auto')
+for r in results:
+        displayByRelevance[r['term']] = 0
+
+displayByYear = {}
+results = db.sql('''
+        select distinct r.year
+        from BIB_Refs r
+        where r.year between date_part('year', CURRENT_DATE) - 5 and date_part('year', CURRENT_DATE) 
+        and r.journal is not null
+        order by year
+        ''', 'auto')
+for r in results:
+        deepCopy = copy.deepcopy(displayByRelevance)
+        displayByYear[r['year']] = deepCopy
+#print(displayByYear)
+
+#
+# count grouped by year, journal
+#       . journal must have a J:
+#       . year is within the last 5 years of today's year
+#
+displayByJournal = {}
+results = db.sql('''
+    select r.journal, r.year, c.relevanceterm, count(*) as counter
+    from BIB_Refs r, BIB_Citation_Cache c
+    where r.year between date_part('year', CURRENT_DATE) - 5 and date_part('year', CURRENT_DATE) 
+    and r.journal is not null
+    and r._refs_key = c._refs_key 
+    group by r.journal, year, relevanceterm
+    order by r.journal, r.year, c.relevanceterm
+    ''', 'auto')
+for r in results:
+    journal = r['journal']
+    year = r['year']
+    relevanceterm = r['relevanceterm']
+    if journal not in displayByJournal:
+        deepCopy = copy.deepcopy(displayByYear)
+        displayByJournal[journal] = deepCopy
+    displayByJournal[journal][year][relevanceterm] = r['counter']
+
+fp = reportlib.init(sys.argv[0], 'For each publication year columns report the Total number of papers imported, classified as Keep, or classified as Discard from each journal.', os.environ['QCOUTPUTDIR'])
+
+#
+# write the year
+#
+fp.write(TAB)
+for r in displayByYear:
+        fp.write(str(r) + ' Total' + TAB)
+        for t in displayByYear[r]:
+                fp.write(str(r) + ' ' + t + TAB)
+fp.write(CRT)
+#
+# write the journals & counts
+#
+for r in displayByJournal:
+        fp.write(r + TAB)
+        for y in displayByJournal[r]:
+                totalCount = 0
+                for t in displayByYear[y]:
+                        totalCount += displayByJournal[r][y][t]
+                fp.write(str(totalCount) + TAB)
+        for y in displayByJournal[r]:
+                for t in displayByYear[y]:
+                        fp.write(str(displayByJournal[r][y][t]) + TAB)
+        fp.write(CRT)
+
+reportlib.finish_nonps(fp)
+
