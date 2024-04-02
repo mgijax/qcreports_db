d98ade3d6cd036067442b31655d49f2c285b1a23
Lori Corbani <lori.corbani@jax.org>
2024-04-02 05:57:58 -0400

    wts2-1453/fl2-811/add curation state to existing custom report

--- pwi/GXD_HTRawSampleByValue2.py
+++ pwi/GXD_HTRawSampleByValue2.py
@@ -4,13 +4,14 @@
 # GXD HT Raw Sample Value Search 2
 #
 # Search GXD HT Raw Sample Value 
-# -- sort by experment type, relevance, experiment id
+# -- sort by experment type, relevance, curation status, experiment id
 # -- results limited to 5000 
 # -- both key and value columns are aggregated
 #
-# 1: Experiment ID (tertiary sort)
-# 2: Experiment Type (primary sort)
-# 3: Relevance (secondary sort using the same custom ordering we use for the GXD HT Raw Sample Value Search).
+# 1: Experiment ID (sort 4)
+# 2: Experiment Type (sort 1)
+# 3: Relevance (sort 2 using the same custom ordering we use for the GXD HT Raw Sample Value Search).
+# 4: Curator Status (sort 3 using the same custom ordering we use for the GXD HT Raw Sample Value Search).
 #
 '''
  
@@ -30,12 +31,14 @@ results = db.sql('''
 select distinct a.accid as ExperimentID, 
 t2.term as experimenttype, 
 t.term as relevance,
+t3.term as curationStatus,
 e.name
 from gxd_htrawsample rs, 
      mgi_keyvalue kv, 
      gxd_htexperiment e, 
      voc_term t, 
      voc_term t2, 
+     voc_term t3,
      acc_accession a 
 where kv.value ilike '%s' 
 and kv._mgitype_key = 47 -- raw sample 
@@ -43,6 +46,7 @@ and kv._object_key = rs._rawsample_key
 and rs._experiment_key = e._experiment_key 
 and e._evaluationstate_key = t._term_key 
 and e._experimenttype_key = t2._term_key 
+and e._curationstate_key = t3._term_key 
 and e._experiment_key = a._object_key 
 and a._mgitype_key = 42 -- experiment 
 and a._logicaldb_key = 190 
@@ -60,6 +64,7 @@ for r in results:
         sys.stdout.write(r['ExperimentID'] + TAB)
         sys.stdout.write(r['experimenttype'] + TAB)
         sys.stdout.write(r['relevance'] + TAB)
+        sys.stdout.write(r['curationStatus'] + TAB)
         sys.stdout.write(r['name'] + CRT)
 
 sys.stdout.flush()
