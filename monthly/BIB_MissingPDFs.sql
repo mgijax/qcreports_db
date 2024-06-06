be749bf319e4ab9b3a8f50cc90cc73eca1eee1bd
Lori Corbani <lori.corbani@jax.org>
2024-06-06 09:31:36 -0400

    wts2-1483/fl2-894/more exclusions for References QC 5. J#s missing PDF

--- monthly/BIB_MissingPDFs.sql
+++ monthly/BIB_MissingPDFs.sql
@@ -10,11 +10,17 @@ with refs_without_pdfs as (
 select c.numericPart as jnum_numeric, c.jnumid, c.short_citation
 from bib_refs r, voc_term t, bib_workflow_data d, bib_citation_cache c
 where r._ReferenceType_key = t._Term_key
-	and t.term = 'Peer Reviewed Article'
-	and r._Refs_key = d._Refs_key
-	and r._Refs_key = c._Refs_key
-	and d.hasPDF = 0
-	and d._ExtractedText_key = 48804490
+and t.term = 'Peer Reviewed Article'
+and r._Refs_key = d._Refs_key
+and r._Refs_key = c._Refs_key
+and d.hasPDF = 0
+and d._ExtractedText_key = 48804490
+and c._relevance_key != 70594666
+and not exists (select 1 from bib_workflow_tag tg , voc_term tgv
+        where r._refs_key = tg._refs_key
+        and tg._tag_key = tgv._term_key
+        and tgv.term in ('MGI:hard_copy_avail', 'MGI:no_pdf_avail')
+        )
 order by 1 desc
 )
 select jnumid, short_citation
