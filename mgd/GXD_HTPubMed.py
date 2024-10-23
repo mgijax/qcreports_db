a5e14c694aa40190b63878c390ee40ecdad30d76
Lori Corbani <lori.corbani@jax.org>
2024-10-23 07:36:20 -0400

    wts2-1565/e4g-39/GXD HT: Missing PMIDs

--- mgd/GXD_HTPubMed.py
+++ mgd/GXD_HTPubMed.py
@@ -34,7 +34,7 @@ TAB = reportlib.TAB
 fp = reportlib.init(sys.argv[0], 'GXD HT Experiments with PMIDs that do not exist in Lit Triage', os.environ['QCOUTPUTDIR'])
 
 results = db.sql('''
-    select distinct p.value, a.accid as exptId
+    select distinct p.value, array_to_string(array_agg(distinct a.accid),'|') as exptId
     from GXD_HTExperiment e, ACC_Accession a, MGI_Property p
     where e._curationstate_key = 20475421 /* Done */
     and e._experiment_key = a._object_key
@@ -44,6 +44,7 @@ results = db.sql('''
     and p._mgitype_key = 42
     and p._propertyterm_key = 20475430
     and not exists (select 1 from BIB_Citation_Cache c where p.value = c.pubmedid)
+    group by p.value
     order by p.value
     ''', 'auto') 
 
