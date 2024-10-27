ab7f68e934877ee8e315c6fd37fc231ebc90c9d2
Lori Corbani <lori.corbani@jax.org>
2024-10-27 11:07:34 -0400

    wts2-1565/e4g-39/GXD HT: Missing PMIDs

--- mgd/GXD_HTPubMed.py
+++ mgd/GXD_HTPubMed.py
@@ -34,6 +34,7 @@ TAB = reportlib.TAB
 fp = reportlib.init(sys.argv[0], 'Curated GXD HT Experiments with PMIDs that do not exist in Lit Triage', os.environ['QCOUTPUTDIR'])
 
 results = db.sql('''
+    (
     select distinct p.value, array_to_string(array_agg(distinct a.accid),'|') as exptId
     from GXD_HTExperiment e, ACC_Accession a, MGI_Property p
     where e._curationstate_key = 20475421 /* Done */
@@ -45,7 +46,20 @@ results = db.sql('''
     and p._propertyterm_key = 20475430
     and not exists (select 1 from BIB_Citation_Cache c where p.value = c.pubmedid)
     group by p.value
-    order by p.value
+    union
+    select distinct p.value, array_to_string(array_agg(distinct a.accid),'|') as exptId
+    from GXD_HTExperiment e, ACC_Accession a, MGI_Property p
+    where e._curationstate_key = 20475421 /* Done */
+    and e._experiment_key = a._object_key
+    and a._mgitype_key = 42 /* ht experiment type */
+    and a.preferred = 1
+    and e._experiment_key = p._object_key
+    and p._mgitype_key = 42
+    and p._propertyterm_key = 20475430
+    and exists (select 1 from BIB_Citation_Cache c where p.value = c.pubmedid and c.jnumid is null)
+    group by p.value
+    )
+    order by value
     ''', 'auto') 
 
 for r in results:
