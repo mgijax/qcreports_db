d66577eb076d7555ee5cbb34b59d77a4a50f4f9b
Lori Corbani <lori.corbani@jax.org>
2024-11-01 08:10:31 -0400

    wts2-1539/e4g-57/GXD RNA-Seq Type/Schema/Migration

---
+++ mgd/GXD_HTSampleNoRNAType.py
@@ -0,0 +1,50 @@
+
+'''
+#
+# Report:
+#
+# GXD HT Experiments #8
+# RNA-Seq Samples missing RNA-Seq Type
+#
+# Experiment type = RNA Seq
+# GXD relevance = Yes
+# RNA-Seq Type value = “Not Specified”
+# 
+# Columns - 1: Experiment ID  2: Sample Name
+# Sort order – Experiment ID
+#
+# History:
+#
+# lec   11/01/2024
+#   wts2-1539/e4g-57/GXD RNA-Seq Type/Schema/Migration
+#
+'''
+
+import sys
+import os
+import reportlib
+import db
+
+db.setTrace()
+
+CRT = reportlib.CRT
+TAB = reportlib.TAB
+
+fp = reportlib.init(sys.argv[0], 'RNA-Seq Samples missing RNA-Seq Type', os.environ['QCOUTPUTDIR'])
+
+results = db.sql('''
+    select a.accid, s.name
+    from GXD_HTExperiment e, ACC_Accession a, GXD_HTSample s
+    where e._experimenttype_key = 20475450
+    and e._experiment_key = s._experiment_key
+    and s._rnaseqtype_key = 114866227
+    and e._experiment_key = a._object_key
+    and a._logicaldb_key in (189,190)
+    order by accid
+    ''', 'auto') 
+for r in results:
+    fp.write(r['accid'] + TAB)
+    fp.write(r['name'] + CRT)
+fp.write(CRT + '(%d rows affected)' % (len(results)) + CRT)
+
+reportlib.finish_nonps(fp)
