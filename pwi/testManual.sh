9201b5c00c2cc18ee048279695d10c21b8944eca
Lori Corbani <lori.corbani@jax.org>
2024-11-21 06:30:03 -0500

    added example to testManual.sh

--- pwi/testManual.sh
+++ pwi/testManual.sh
@@ -6,6 +6,11 @@
 # . call pwireports.cgi.
 #
 # In addition to ‘arg’ there are two other parameters, ‘cmd=run’ and ‘rpt=PRB_StrainRepoCheck’.
+#
+# examples
+#
+# testManual.sh PRB_StrainNomenCheck "C57BL/6-1700009J07Rik<em1Smoc>/Smoc"
+#
 
 export RPTNAME=$1
 export RPTARG=$2
