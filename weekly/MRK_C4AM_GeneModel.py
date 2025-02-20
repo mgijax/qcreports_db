840493b56d9cc7908c0e185b0c33360200665076
Lori Corbani <lori.corbani@jax.org>
2025-02-20 12:50:54 -0500

    wts2-1600/e4g-115/Marker Coordinate Load changes

--- weekly/MRK_C4AM_GeneModel.py
+++ weekly/MRK_C4AM_GeneModel.py
@@ -4,8 +4,14 @@
 #	MRK_C4AM_GeneModel.py
 #
 # Report:
-#       Weekly report of markers with curated coordinates associated 
-#	  with gene models
+#       Weekly report of markers with curated coordinates associated with gene models
+#
+# Collection = 131,256,257
+# AND
+# Gene Model association exists for:  Ensembl Reg Gene Model, NCBI Gene Model, VISTA Gene Model, Ensembl Gene Model
+#
+# -- what is the difference between colleciton 131/MGI & 256/MGI Curation
+# -- obsolete: where mcf._CreatedBy_key = 1523 /*mrk_coordload*/
 #
 # Usage:
 #       MRK_C4AM_GeneModel.py
@@ -35,52 +41,33 @@ import db
 CRT = reportlib.CRT
 TAB = reportlib.TAB
 
-# Organize result set by marker asmultiple gene models per marker
-# {mgiID:[line], ...}
-reportDict = {}
-
 fp = reportlib.init(sys.argv[0], 'Markers with C4AM Coordinates and Gene Models', os.environ['QCOUTPUTDIR'])
+fp.write('Marker ID%sCollection%sMarker Symbol%sGene Model IDs%s' % (TAB, TAB, TAB, CRT))
 
 # curated via mrkcoordload
-db.sql('''select mcf._Object_key as markerKey, a.accid as mgiID, m.symbol
-    into temporary table curated
-    from  MAP_Coord_Feature mcf, MAP_Coordinate mc, MAP_Coord_Collection mcc, 
-        ACC_Accession a, MRK_Marker m
-    where mcf._CreatedBy_key = 1523 /*mrk_coordload*/
-    and mcf._Map_key = mc._Map_key
+results = db.sql('''
+    select mcf._Object_key as markerKey, a.accid as mgiID, m.symbol, mcc.name, gm.accid as gmID
+    from MAP_Coord_Feature mcf, MAP_Coordinate mc, MAP_Coord_Collection mcc, ACC_Accession a, MRK_Marker m, ACC_Accession gm
+    where mcf._Map_key = mc._Map_key
     and mc._Collection_key = mcc._Collection_key
-    and mcc._Collection_key = 64 /* MGI */
     and mcf._Object_key = a._Object_key
     and a._MGIType_key = 2
     and a._LogicalDB_key = 1
     and a.preferred = 1
     and a.prefixPart = 'MGI:'
-    and mcf._Object_key = m._Marker_key''', None)
-
-db.sql('create index idx1 on curated(markerKey)', None)
-
-# get curated markers with gene models, NCBI, Ensembl
-results = db.sql('''select a.accid as genemodelID, c.* 
-from ACC_Accession a, curated c
-where a._Object_key = c.markerKey
-and a._MGIType_key = 2
-and a._LogicalDB_key in (59, 60)
-order by symbol''', 'auto') 
+    and mcf._Object_key = m._Marker_key
+    and a._Object_key = gm._Object_key
+    and gm._mgitype_key = 2
+    and gm._logicaldb_key in (223,222,60,59)
+    order by symbol
+    ''', 'auto')
 
 for r in results:
     mgiID = r['mgiID']
     symbol = r['symbol']
-    gmID = r['genemodelID']
-    key = '%s|%s' % (mgiID, symbol)
-    if key not in reportDict:
-        reportDict[key] = []
-    reportDict[key].append(gmID)
-
-fp.write('Marker ID%sMarker Symbol%sGene Model IDs%s' % (TAB, TAB, CRT))
-for key in reportDict:
-    gmIdList = reportDict[key]
-    mgiID, symbol = str.split(key, '|')
-    fp.write('%s%s%s%s%s%s' % (mgiID, TAB, symbol, TAB, ', '.join(gmIdList), CRT  ))
+    gmID = r['gmID']
+    collection = r['name']
+    fp.write('%s%s%s%s%s%s%s%s' % (mgiID, TAB, collection, TAB, symbol, TAB, gmID, CRT  ))
 
 fp.write(CRT)
 fp.write('Total: %s' % len(results))
