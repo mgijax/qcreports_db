cd227e4bd9c35439099b3cf2d9f53a7d9de9c217
Lori Corbani <lori.corbani@jax.org>
2025-06-03 06:32:05 -0400

    wts2-1673/how many alleles sold by Jax are used to annotated GXDHT_Samples

--- pwi/GXD_HTSampleJAX.py
+++ pwi/GXD_HTSampleJAX.py
@@ -28,29 +28,49 @@ def go (form) :
 
     # Write header
     sys.stdout.write('MGI id of allele'+ TAB)
-    sys.stdout.write('Allele Symbol' + CRT)
+    sys.stdout.write('Allele Symbol' + TAB)
+    sys.stdout.write('JR strain association yes/no' + CRT)
 
     results = db.sql('''
-        select a.accid, aa.symbol
-        from ALL_Allele aa, ACC_Accession a
-        where aa.iswildtype = 0
+        (
+        select distinct a.accid, aa.symbol, 'y' as hasJR
+        from GXD_HTSample ht, GXD_Genotype g, PRB_Strain s, GXD_AlleleGenotype ga, ALL_Allele aa, ACC_Accession a
+        where ht._genotype_key = g._genotype_key
+        and g._strain_key = s._strain_key
+        and g._genotype_key = ga._genotype_key
+        and ga._allele_key = aa._allele_key
+        and aa.iswildtype = 0
         and aa._allele_key = a._object_key
         and a._mgitype_key = 11 
         and a.preferred = 1
-        and exists (select 1 from PRB_Strain s, ACC_Accession sa, GXD_Genotype g, GXD_AlleleGenotype ga, GXD_HTSample ht
+        and exists (select 1 from ACC_Accession sa 
             where s._strain_key = sa._object_key
             and sa._mgitype_key = 10
             and sa._logicaldb_key = 22
-            and s._strain_key = g._strain_key
-            and g._genotype_key = ga._genotype_key
-            and ga._allele_key = aa._allele_key
-            and g._genotype_key = ht._genotype_key
+            )
+        union
+        select distinct a.accid, aa.symbol, 'n' as hasJR
+        from GXD_HTSample ht, GXD_Genotype g, PRB_Strain s, GXD_AlleleGenotype ga, ALL_Allele aa, ACC_Accession a
+        where ht._genotype_key = g._genotype_key
+        and g._strain_key = s._strain_key
+        and g._genotype_key = ga._genotype_key
+        and ga._allele_key = aa._allele_key
+        and aa.iswildtype = 0
+        and aa._allele_key = a._object_key
+        and a._mgitype_key = 11 
+        and a.preferred = 1
+        and not exists (select 1 from ACC_Accession sa 
+            where s._strain_key = sa._object_key
+            and sa._mgitype_key = 10
+            and sa._logicaldb_key = 22
+            )
         )
-        order by aa.symbol
+        order by symbol
     ''', 'auto')
 
     for r in results:
         sys.stdout.write(r['accid'] + TAB)
-        sys.stdout.write(r['symbol'] + CRT)
+        sys.stdout.write(r['symbol'] + TAB)
+        sys.stdout.write(r['hasJR'] + CRT)
 
     sys.stdout.flush()
