13818df3744132bf2aed78fb11961b2749d29ad8
Lori Corbani <lori.corbani@jax.org>
2025-06-03 12:17:11 -0400

    wts2-1673/how many alleles sold by Jax are used to annotated GXDHT_Samples

--- pwi/GXD_HTSampleAlleles.py
+++ pwi/GXD_HTSampleAlleles.py
@@ -38,8 +38,7 @@ def go (form) :
         select distinct a.accid, aa._allele_key, aa.symbol, 
             string_agg(distinct sa.accid, '|') as jaxids, 'yes' as hasJR,
             string_agg(distinct s.strain, '|') as strains
-        from GXD_HTSample ht, GXD_Genotype g, GXD_AlleleGenotype ga, ALL_Allele aa, ACC_Accession a,  
-            PRB_Strain s, ACC_Accession sa
+        from GXD_HTSample ht, GXD_Genotype g, GXD_AlleleGenotype ga, ALL_Allele aa, ACC_Accession a, PRB_Strain s, ACC_Accession sa
         where ht._genotype_key = g._genotype_key
         and g._genotype_key = ga._genotype_key
         and ga._allele_key = aa._allele_key
@@ -55,8 +54,9 @@ def go (form) :
         )
         select * from alleles
         union
-        select distinct a.accid, aa._allele_key, aa.symbol, null, 'no' as hasJR, null
-        from GXD_HTSample ht, GXD_Genotype g, GXD_AlleleGenotype ga, ALL_Allele aa, ACC_Accession a
+        select distinct a.accid, aa._allele_key, aa.symbol, null, 'no' as hasJR, 
+            string_agg(distinct s.strain, '|') as strains
+        from GXD_HTSample ht, GXD_Genotype g, GXD_AlleleGenotype ga, ALL_Allele aa, ACC_Accession a, PRB_Strain s
         where ht._genotype_key = g._genotype_key
         and g._genotype_key = ga._genotype_key
         and ga._allele_key = aa._allele_key
@@ -64,12 +64,14 @@ def go (form) :
         and aa._allele_key = a._object_key
         and a._mgitype_key = 11 
         and a._logicaldb_key = 1
+        and g._strain_key = s._strain_key
         and not exists (select 1 from ACC_Accession sa 
             where g._strain_key = sa._object_key
             and sa._mgitype_key = 10
             and sa._logicaldb_key = 22
             )
         and not exists (select 1 from alleles where aa._allele_key = alleles._allele_key)
+        group by 1,2,3
         order by hasJR desc, symbol
     ''', 'auto')
 
