08ece6409cf9eb9be93859684a319271fff5221a
Lori Corbani <lori.corbani@jax.org>
2025-06-03 12:06:48 -0400

    wts2-1673/how many alleles sold by Jax are used to annotated GXDHT_Samples

--- pwi/GXD_HTSampleAlleles.py
+++ pwi/GXD_HTSampleAlleles.py
@@ -29,12 +29,17 @@ def go (form) :
     # Write header
     sys.stdout.write('MGI id of allele' + TAB)
     sys.stdout.write('Allele Symbol' + TAB)
-    sys.stdout.write('JR strain association yes/no' + CRT)
+    sys.stdout.write('JR strain association yes/no' + TAB)
+    sys.stdout.write('JR ids' + TAB)
+    sys.stdout.write('Strains' + CRT)
 
     results = db.sql('''
         WITH alleles AS (
-        select distinct a.accid, aa._allele_key, aa.symbol, 'yes' as hasJR
-        from GXD_HTSample ht, GXD_Genotype g, GXD_AlleleGenotype ga, ALL_Allele aa, ACC_Accession a
+        select distinct a.accid, aa._allele_key, aa.symbol, 
+            string_agg(distinct sa.accid, '|') as jaxids, 'yes' as hasJR,
+            string_agg(distinct s.strain, '|') as strains
+        from GXD_HTSample ht, GXD_Genotype g, GXD_AlleleGenotype ga, ALL_Allele aa, ACC_Accession a,  
+            PRB_Strain s, ACC_Accession sa
         where ht._genotype_key = g._genotype_key
         and g._genotype_key = ga._genotype_key
         and ga._allele_key = aa._allele_key
@@ -42,15 +47,14 @@ def go (form) :
         and aa._allele_key = a._object_key
         and a._mgitype_key = 11 
         and a._logicaldb_key = 1
-        and exists (select 1 from ACC_Accession sa 
-            where g._strain_key = sa._object_key
-            and sa._mgitype_key = 10
-            and sa._logicaldb_key = 22
-            )
+        and g._strain_key = sa._object_key
+        and sa._mgitype_key = 10
+        and sa._logicaldb_key = 22
+        group by 1,2,3
         )
         select * from alleles
         union
-        select distinct a.accid, aa._allele_key, aa.symbol, 'no' as hasJR
+        select distinct a.accid, aa._allele_key, aa.symbol, null, 'no' as hasJR, null
         from GXD_HTSample ht, GXD_Genotype g, GXD_AlleleGenotype ga, ALL_Allele aa, ACC_Accession a
         where ht._genotype_key = g._genotype_key
         and g._genotype_key = ga._genotype_key
@@ -71,6 +75,8 @@ def go (form) :
     for r in results:
         sys.stdout.write(r['accid'] + TAB)
         sys.stdout.write(r['symbol'] + TAB)
-        sys.stdout.write(r['hasJR'] + CRT)
+        sys.stdout.write(r['hasJR'] + TAB)
+        sys.stdout.write(r['jaxids'] + TAB)
+        sys.stdout.write(r['strains'] + CRT)
 
     sys.stdout.flush()
